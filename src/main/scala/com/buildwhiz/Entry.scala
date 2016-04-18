528f3a09e1b29a11b0f767acfcacb1b1011fdd85
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-04-18 22:03:53 +0530

    Changes to support single entry point for all servlet based BAF service provider classes

--- src/main/scala/com/buildwhiz/Entry.scala
+++ src/main/scala/com/buildwhiz/Entry.scala
@@ -16,11 +16,11 @@ class Entry extends HttpServlet with Utils {
 
   private def handleRequest(request: HttpServletRequest, delegateTo: Entry.BWServlet => Unit): Unit = {
     val urlParts = request.getRequestURL.toString.split("/")
-    val bafOffset = urlParts.indexOf("baf")
-    val className = s"com.buildwhiz.baf.${urlParts(bafOffset + 1)}"
+    val bafOffset = urlParts.zipWithIndex.find(_._1.matches("baf|api")).head._2
+    val className = urlParts(bafOffset + 1)
     Entry.cache.get(className) match {
       case Some(httpServlet) => delegateTo(httpServlet)
-      case None => Try(Class.forName(className)) match {
+      case None => getClazz(className) match {
         case Success(clazz) =>
           Try(clazz.newInstance()) match {
             case Success(httpServlet: Entry.BWServlet @ unchecked) =>
@@ -56,13 +56,25 @@ class Entry extends HttpServlet with Utils {
     }
   }
 
+  override def doDelete(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    try {
+      handleRequest(request, servlet => servlet.doDelete(request, response))
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "doGet", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        t.printStackTrace()
+        throw t
+    }
+  }
+
 }
 
 object Entry {
   import scala.collection.mutable
 
   type BWServlet = {def doGet(req: HttpServletRequest, res: HttpServletResponse);
-    def doPost(req: HttpServletRequest, res: HttpServletResponse)}
+    def doPost(req: HttpServletRequest, res: HttpServletResponse);
+    def doDelete(req: HttpServletRequest, res: HttpServletResponse)}
 
   val cache = mutable.Map.empty[String, BWServlet]
 }

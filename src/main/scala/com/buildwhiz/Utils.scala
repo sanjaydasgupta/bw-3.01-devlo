95d4447eb4063dbaeb7fb00e99b8e5855aac0417
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-04-23 07:31:45 +0530

    Mail "send()" should be asynchronous to avoid blocking caller (fix for issue #7)

--- src/main/scala/com/buildwhiz/Utils.scala
+++ src/main/scala/com/buildwhiz/Utils.scala
@@ -12,10 +12,12 @@ import org.bson.types.ObjectId
 import scala.collection.JavaConversions._
 import scala.collection.mutable
 import scala.io.Source
-
 import java.util.Properties
 import javax.mail.{Address, Message, PasswordAuthentication, Session, Transport}
 import javax.mail.internet.{InternetAddress, MimeMessage}
+
+import scala.concurrent.Future
+import scala.util.Failure
 
 trait Utils {
 
@@ -33,16 +35,16 @@ trait Utils {
     props.put("mail.smtp.port", "465")
     props.put("mail.smtp.socketFactory.fallback", "false")
 
-    val session = Session.getInstance(props,
-      new javax.mail.Authenticator() {
-        override def getPasswordAuthentication = {
-          new PasswordAuthentication(username,
-            s"${username.substring(16, 21)}${password.length/2}${username.substring(21, 25)}")
-        }
-      }
-    )
-
     try {
+      val session = Session.getInstance(props,
+        new javax.mail.Authenticator() {
+          override def getPasswordAuthentication = {
+            new PasswordAuthentication(username,
+              s"${username.substring(16, 21)}${password.length/2}${username.substring(21, 25)}")
+          }
+        }
+      )
+
       val person: DynDoc = BWMongoDB3.persons.find(Map("_id" -> recipientOid)).head
       val email = person.emails[DocumentList].find(_.`type`[String] == "work").head.email[String]
       val (firstName, lastName) = (person.first_name[String], person.last_name[String])
@@ -54,7 +56,13 @@ trait Utils {
       message.setRecipients(Message.RecipientType.TO, recipients)
       message.setSubject(s"$subject for $email")
       message.setText(body)
-      Transport.send(message)
+      import scala.concurrent.ExecutionContext.Implicits.global
+      Future(Transport.send(message)).onComplete {
+        case Failure(t: Throwable) =>
+          BWLogger.log(getClass.getName, "sendMail", s"ERROR: ${t.getClass.getName}(${t.getMessage})")
+          t.printStackTrace()
+        case _ =>
+      }
       BWLogger.log(getClass.getName, "sendMail", s"EXIT-OK")
     } catch {
       case t: Throwable =>

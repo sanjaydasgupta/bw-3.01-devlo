d3cde72c16356b71dbf3ee5f6afe50b1ebbe1546
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-04-27 17:55:44 +0530

    Minor changes to utility program

--- src/main/scala/com/buildwhiz/etc/GenerateMongoDbIds.scala
+++ src/main/scala/com/buildwhiz/etc/GenerateMongoDbIds.scala
@@ -8,13 +8,13 @@ import scala.collection.JavaConversions._
 
 object GenerateMongoDbIds extends App {
   println(s"Count=${BWMongoDB3.id_generation.count()}")
-  for (i <- 1 to 10) {
+  for (i <- 1 to 20) {
     val tempObject: Document = Map("value" -> (900 + i))
     BWMongoDB3.id_generation.insertOne(tempObject)
     val tempDynObj: DynDoc = tempObject
   }
   val ids: Seq[DynDoc] = BWMongoDB3.id_generation.find().toList
-  println(ids.map(_._id[ObjectId]))
+  println(ids.map(obj => s""""${obj._id[ObjectId]}"""").mkString(", "))
   println(s"Count=${BWMongoDB3.id_generation.count()}")
   BWMongoDB3.id_generation.deleteMany(new Document)
   println(s"Count=${BWMongoDB3.id_generation.count()}")

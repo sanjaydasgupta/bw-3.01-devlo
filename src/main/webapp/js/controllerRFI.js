c6d2208e775279fabc3ae4f7e1736240b90a89a8
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-05-05 19:32:51 +0530

    RFI updates

--- src/main/webapp/js/controllerRFI.js
+++ src/main/webapp/js/controllerRFI.js
@@ -7,19 +7,28 @@
     self.selectedAction = null;
     self.isWaiting = false;
 
-    $http.get('baf/OwnedActionsAll?person_id=' + self.personId).then(
-        function (response) {
-            self.ownedActions = response.data;
-        }
-    )
+    self.setupRfiDisplay = function (action) {
+        self.selectedAction = null;
+        $http.get('baf/OwnedActionsAll?person_id=' + self.personId).then(
+            function (response) {
+                self.ownedActions = response.data;
+                self.selectedAction = action;
+            }
+        )
+    }
+
+    self.setupRfiDisplay(null);
 
     self.statusColor = function (action) {
-        switch (action.status) {
+        switch (action.display_status) {
         case "defined":
             return "yellow";
             break;
         case "waiting":
             return "magenta";
+            break;
+        case "waiting2":
+            return "brown";
             break;
         case "ended":
             return "gray";
@@ -56,8 +65,48 @@
         )
     }
 
-    self.btnRfiClick = function () {
-        alert("RFI");
+    self.rfiButtonClick = function () {
+        var isRequest = self.selectedAction.assignee_is_user;
+        self.isWaiting = true;
+        $http({
+            method: 'POST',
+            url: 'baf/RfiSubmit',
+            data: $.param({'project_id': self.selectedAction.project_id, 'action_name': self.selectedAction.name,
+                'activity_id': self.selectedAction.activity_id, 'rfi_text': self.newRfiText, is_request: isRequest}),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(
+            function() {
+                self.isWaiting = false;
+                self.newRfiText = '';
+                self.setupRfiDisplay(self.selectedAction);
+            },
+            function (responseError) {
+                self.isWaiting = false;
+                alert("RFI-Send Problem");
+            }
+        );
+    }
+
+    self.rfiButtonText = function () {
+        if (self.selectedAction.assignee_is_user) {
+            return "Request";
+        } else if (self.selectedAction.phase_manager_is_user) {
+            return "Respond";
+        }
+    }
+
+    self.selectedActionMayInput = function () {
+        if (self.selectedAction.assignee_is_user) {
+            return true;
+        } else if (self.selectedAction.phase_manager_is_user) {
+            var hasRequests = false;
+            self.selectedAction.inDocuments.forEach(function (d) {
+                if (d._id == "56fe4e6bd5d8ad3da60d5d38") {
+                    hasRequests = true;
+                }
+            })
+            return hasRequests;
+        }
     }
 
 });

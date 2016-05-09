dafd7d1e44beaaa42985b0c759a3fb1753848bbc
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-05-09 15:15:18 +0530

    Minor interface change

--- src/main/scala/com/buildwhiz/DateTimeUtils.scala
+++ src/main/scala/com/buildwhiz/DateTimeUtils.scala
@@ -5,7 +5,7 @@ import java.util.{Date, TimeZone}
 
 trait DateTimeUtils {
 
-  def dateTimeString(milliSeconds: Long, timeZoneCode: Option[String] = None): String = {
+  def dateTimeString(milliSeconds: Long, timeZoneCode: Option[String] = Some("GMT")): String = {
     val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm z")
     timeZoneCode.foreach(tzc => simpleDateFormat.setTimeZone(TimeZone.getTimeZone(tzc)))
     simpleDateFormat.format(new Date(milliSeconds))

6d6d4d45e93340cbd91e20f6c1f27cb4f6c304eb
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-05-14 17:52:57 +0530

    Fix for issue #23 "Skills and Roles Administration Screen"

---
+++ src/main/scala/com/buildwhiz/baf/UserSkillsSet.scala
@@ -0,0 +1,40 @@
+package com.buildwhiz.baf
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+import com.buildwhiz.infra.BWMongoDB3._
+import com.buildwhiz.infra.{BWLogger, BWMongoDB3}
+import com.buildwhiz.{CryptoUtils, HttpUtils}
+import org.bson.types.ObjectId
+
+class UserSkillsSet extends HttpServlet with HttpUtils with CryptoUtils {
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
+    try {
+      val personOid = new ObjectId(parameters("person_id"))
+      val setAdmin = parameters("BW-Admin").toBoolean match {
+        case false => "$pull" -> Map("omniclass34roles" -> "BW-Admin")
+        case true => "$addToSet" -> Map("omniclass34roles" -> "BW-Admin")
+      }
+      val setDemo = parameters("BW-Demo").toBoolean match {
+        case false => "$pull" -> Map("omniclass34roles" -> "BW-Demo")
+        case true => "$addToSet" -> Map("omniclass34roles" -> "BW-Demo")
+      }
+      val updateResult1 = BWMongoDB3.persons.updateOne(Map("_id" -> personOid), Map(setDemo))
+      if (updateResult1.getMatchedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult1")
+      val updateResult2 = BWMongoDB3.persons.updateOne(Map("_id" -> personOid), Map(setAdmin))
+      if (updateResult2.getMatchedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult2")
+      response.setStatus(HttpServletResponse.SC_OK)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        t.printStackTrace()
+        throw t
+    }
+    BWLogger.log(getClass.getName, "doPost", "EXIT-OK", request)
+  }
+}
---
+++ src/main/webapp/js/controllerUserRoles.js
@@ -0,0 +1,54 @@
+﻿app.controller("UserRolesCtrl", function ($log, $http) {
+
+    var self = this;
+    self.persons = [];
+    self.isWaiting = true;
+
+    self.initPage = function() {
+        $log.log('HTTP GET baf/Person')
+        $http.get('api/Person').then(
+            function (response) {
+                self.persons = response.data;
+                self.isWaiting = false;
+                for (var i = 0; i < self.persons.length; ++i) {
+                    var person = self.persons[i];
+                    if (person.first_name == "No" && person.last_name == "One") {
+                        self.persons.splice(i, 1);
+                        break;
+                    }
+                }
+                self.persons.forEach(function(person) {self.initPerson(person);});
+            },
+            function() {
+                alert('ERROR: HTTP GET api/Person');
+            }
+        );
+    }
+
+    self.initPerson = function(person) {
+        person.roleDemo = person.newRoleDemo = person.omniclass34roles.indexOf('BW-Demo') != -1;
+        person.roleAdmin = person.newRoleAdmin = person.omniclass34roles.indexOf('BW-Admin') != -1;
+    }
+
+    self.savePerson = function(person) {
+        var query = '?person_id=' + person._id + '&BW-Demo=' + person.newRoleDemo +
+                '&BW-Admin=' + person.newRoleAdmin;
+        $log.log('HTTP POST baf/UserSkillsSet/' + query)
+        $http.post('baf/UserSkillsSet/' + query).then(
+            function(response) {
+                //alert("Person record updated");
+                self.initPage();
+            },
+            function(response) {
+                alert('ERROR: api/Person/');
+            }
+        )
+    }
+
+    self.dataModified = function(person) {
+        return (person.roleDemo != person.newRoleDemo) || (person.roleAdmin != person.newRoleAdmin);
+    }
+
+    self.initPage();
+
+});

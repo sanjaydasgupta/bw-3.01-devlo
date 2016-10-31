c9af38d6a9ee7383db8253ff908dc2c6575c69c1
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-10-31 15:30:22 +0530

    Updates to tasks support

--- src/main/webapp/js/controllerHome.js
+++ src/main/webapp/js/controllerHome.js
@@ -2,9 +2,22 @@
 
 .controller("HomeCtrl", ['$log', '$http', 'AuthenticationService', function ($log, $http, AuthService) {
 
-    var self = this;
+  var self = this;
 
-    self.taskList = [];
-    self.projectList = [];
+  self.taskList = [];
+  self.projectList = [];
+
+  self.fetchActions = function() {
+    query = 'person_id=' + AuthService.data._id;
+    $log.log('HomeCtrl: GET baf/OwnedActionsAll?' + query);
+    $http.get('baf/OwnedActionsAll?' + query).then(
+      function(resp) {
+        self.taskList = resp.data;
+      },
+      function(errResponse) {alert("HomeCtrl: ERROR(collection-details): " + errResponse);}
+    );
+  }
+
+  self.fetchActions();
 
 }]);
--- src/main/webapp/js/controllerTasks.js
+++ src/main/webapp/js/controllerTasks.js
@@ -2,8 +2,22 @@
 
 .controller("TasksCtrl", ['$log', '$http', 'AuthenticationService', function ($log, $http, AuthService) {
 
-    var self = this;
+  var self = this;
 
-    self.taskList = [];
+  self.taskList = [];
+
+  self.fetchActions = function() {
+    query = 'person_id=' + AuthService.data._id;
+    $log.log('HomeCtrl: GET baf/OwnedActionsAll?' + query);
+    $http.get('baf/OwnedActionsAll?' + query).then(
+      function(resp) {
+        self.taskList = resp.data;
+        $log.log('OK-HomeCtrl: got ' + self.taskList.length + ' objects');
+      },
+      function(errResponse) {alert("HomeCtrl: ERROR(collection-details): " + errResponse);}
+    );
+  }
+
+  self.fetchActions();
 
 }]);

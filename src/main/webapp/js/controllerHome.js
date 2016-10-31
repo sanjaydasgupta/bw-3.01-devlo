17f307ff4516f459245f03c4293a79a4916a3fa3
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-10-31 17:43:27 +0530

    Updates for projects page

--- src/main/webapp/js/controllerHome.js
+++ src/main/webapp/js/controllerHome.js
@@ -13,11 +13,25 @@
     $http.get('baf/OwnedActionsAll?' + query).then(
       function(resp) {
         self.taskList = resp.data;
+        $log.log('OK-HomeCtrl-fetchActions: got ' + self.taskList.length + ' objects');
       },
       function(errResponse) {alert("HomeCtrl: ERROR(collection-details): " + errResponse);}
     );
   }
 
+  self.fetchProjects = function() {
+    query = 'person_id=' + AuthService.data._id;
+    $log.log('ProjectsCtrl: GET baf/OwnedProjects?' + query);
+    $http.get('baf/OwnedProjects?' + query).then(
+      function(resp) {
+        self.projectList = resp.data;
+        $log.log('OK-HomeCtrl-fetchProjects: got ' + self.projectList.length + ' objects');
+      },
+      function(errResponse) {alert("HomeCtrl: ERROR(collection-details): " + errResponse);}
+    );
+  }
+
+  self.fetchProjects();
   self.fetchActions();
 
 }]);
--- src/main/webapp/js/controllerProjects.js
+++ src/main/webapp/js/controllerProjects.js
@@ -2,8 +2,22 @@
 
 .controller("ProjectsCtrl", ['$log', '$http', 'AuthenticationService', function ($log, $http, AuthService) {
 
-    var self = this;
+  var self = this;
 
-    self.projectList = [];
+  self.projectList = [];
+
+  self.fetchProjects = function() {
+    query = 'person_id=' + AuthService.data._id;
+    $log.log('ProjectsCtrl: GET baf/OwnedProjects?' + query);
+    $http.get('baf/OwnedProjects?' + query).then(
+      function(resp) {
+        self.projectList = resp.data;
+        $log.log('OK-ProjectsCtrl: got ' + self.projectList.length + ' objects');
+      },
+      function(errResponse) {alert("ProjectsCtrl: ERROR(collection-details): " + errResponse);}
+    );
+  }
+
+  self.fetchProjects();
 
 }]);

1025ba3f6f3877708a78cebb164007b2240cf0c4
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-11-12 06:25:27 +0530

    Added service for posting progress reports

---
+++ src/main/scala/com/buildwhiz/baf/ProgressReportSubmit.scala
@@ -0,0 +1,23 @@
+package com.buildwhiz.baf
+
+import java.io.{File, FileOutputStream, InputStream}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+import com.buildwhiz.infra.BWMongoDB3._
+import com.buildwhiz.infra.{AmazonS3, BWLogger, BWMongoDB3}
+import com.buildwhiz.{HttpUtils, MailUtils}
+import org.bson.types.ObjectId
+
+import scala.annotation.tailrec
+import scala.collection.JavaConverters._
+
+class ProgressReportSubmit extends HttpServlet with HttpUtils with MailUtils {
+
+  private val rfiRequestOid = new ObjectId("56fe4e6bd5d8ad3da60d5d38")
+  private val rfiResponseOid = new ObjectId("56fe4e6bd5d8ad3da60d5d39")
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
+  }
+}

5f86077ea02beadc36ef9cba6193d68429dc9b81
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-11-12 11:28:18 +0530

    Updates for posting progress report with attachments

--- src/main/webapp/js/controllerTasks.js
+++ src/main/webapp/js/controllerTasks.js
@@ -9,9 +9,7 @@
   self.selectedTask = null;
   self.taskSelected = false;
   self.confirmingCompletion = false;
-  self.photoDescription = '';
   self.progressReportAttachments = [];
-  self.progressReportAttachmentNames = "";
 
   self.select = function(task) {
     if (task) {
@@ -38,15 +36,48 @@
     );
   }
 
+  self.submitProgressReport = function() {
+    var formData = new FormData();
+    angular.forEach(self.progressReportAttachments, function(file, index) {
+      formData.append(file.name, file, file.name);
+      $log.log('formData.append(' + file.name + ', ' + JSON.stringify(file) + ', file)');
+    });
+    var query = 'baf/ProgressReportSubmit?person_id=' + AuthService.data._id;
+    $http.post(query, formData, {transformRequest: angular.identity, headers: {'Content-Type': undefined}}).then(
+      function() {
+        $log.log('OK submitProgressReport')
+      },
+      function() {
+        $log.log('ERROR submitProgressReport')
+      }
+    )
+  }
+
+  self.handleFile = function(evt) {
+    var files = evt.target.files; // FileList of File objects
+    if (files.length > 0) {
+      var file = files[0];
+      var fileDescription = 'name: ' + file.name + ', type: ' + file.type + ', size: ' + file.size;
+      $log.log('File: ' + fileDescription);
+      self.progressReportAttachments.push(file);
+    }
+  }
+
+  self.captureFile = function() {
+      var fileButton = $window.document.getElementById('file-button');
+      $log.log('BEFORE File-Button addEventListener: ' + fileButton);
+      fileButton.addEventListener('change', self.handleFile, false);
+      fileButton.click();
+      $log.log('AFTER File-Button click: ' + fileButton);
+  }
+
   self.handlePhoto = function(evt) {
     var files = evt.target.files; // FileList of File objects
     if (files.length > 0) {
       var photo = files[0];
-      self.photoDescription = 'name: ' + photo.name + ', type: ' + photo.type + ', size: ' + photo.size;
-      $log.log('Photo ' + self.photoDescription);
+      var photoDescription = 'name: ' + photo.name + ', type: ' + photo.type + ', size: ' + photo.size;
+      $log.log('Photo: ' + photoDescription);
       self.progressReportAttachments.push(photo);
-      self.progressReportAttachmentNames =
-          self.progressReportAttachments.map(function(f) {return f.name;}).join('<br/>');
     }
   }
 

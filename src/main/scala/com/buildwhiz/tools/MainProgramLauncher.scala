4e2b884761914b3cdd6abec4f76828d1e1d4eaec
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-11-26 11:35:09 +0530

    Enable HTTP-GET operation in MainProgramLauncher

--- src/main/scala/com/buildwhiz/tools/MainProgramLauncher.scala
+++ src/main/scala/com/buildwhiz/tools/MainProgramLauncher.scala
@@ -7,16 +7,20 @@ import com.buildwhiz.infra.BWLogger
 
 class MainProgramLauncher extends HttpServlet with HttpUtils {
 
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    doPost(request, response)
+  }
+
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
     try {
       val parameters = getParameterMap(request)
       val clazz = Class.forName(s"""com.buildwhiz.infra.${parameters("program")}""")
       clazz.getMethod("main", classOf[Array[String]]).invoke(null, Array.empty[String])
-      BWLogger.log(getClass.getName, "doPost", s"EXIT-OK", request)
+      BWLogger.log(getClass.getName, "doPost/Get", s"EXIT-OK", request)
     } catch {
       case t: Throwable =>
-        BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        BWLogger.log(getClass.getName, "doPost/Get", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
         t.printStackTrace()
         throw t
     }

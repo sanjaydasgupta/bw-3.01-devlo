0055209ad142a0b3d83f6374a56b3f9de624fd83
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-12-08 18:26:39 +0530

    Periodic updates

--- src/main/webapp/js/controllerDocuments.js
+++ src/main/webapp/js/controllerDocuments.js
@@ -1,10 +1,24 @@
 ﻿angular.module('BuildWhizApp')
 
 .controller("DocumentsCtrl", ['$log', '$http', 'AuthenticationService', function ($log, $http, AuthService) {
-  $log.log('ENTRY DocumentsCtrl');
+
   var self = this;
 
   self.documentList = [];
+  self.currentFilterKey = 'All';
 
-  $log.log('EXIT DocumentsCtrl');
+  self.fetchDocuments = function(filter) {
+    self.currentFilterKey = filter ? filter : 'All';
+    var filterKey = filter ? filter : 'all';
+    var query = 'baf/OwnedDocumentsSummary?person_id=' + AuthService.data._id + '&filter_key=' + filter;
+    $log.log('HomeCtrl: GET ' + query);
+    $http.get(query).then(
+      function(resp) {
+        self.documentList = resp.data;
+        $log.log('OK-HomeCtrl: got ' + self.documentList.length + ' objects');
+      },
+      function(errResponse) {alert("HomeCtrl: ERROR(collection-details): " + errResponse);}
+    );
+  }
+
 }]);

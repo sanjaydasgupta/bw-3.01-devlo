6f9da912a9c75ebee76bcb3f95fac34575b78be8
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-12-16 11:26:57 +0530

    Minor updates to MongoDB View

--- src/main/webapp/js/controllerMongodb.js
+++ src/main/webapp/js/controllerMongodb.js
@@ -1,29 +1,35 @@
 ﻿angular.module('BuildWhizApp')
 
 .controller("MongodbCtrl", ['$http', '$log', function ($http, $log) {
-    var self = this;
-    self.collections = [];
-    self.details = [];
-    self.name = ""
+  var self = this;
+  self.collections = [];
+  self.details = [];
+  self.name = ""
 
-    $log.log('MongodbCtrl: calling GET baf/MongoDBView');
-    $http.get('baf/MongoDBView').then(
-        function(resp) {
-            self.collections = resp.data;
-        },
-        function(errResponse) {alert("MongodbCtrl: ERROR(collections): " + errResponse);}
+  $log.log('MongodbCtrl: calling GET baf/MongoDBView');
+  $http.get('baf/MongoDBView').then(
+    function(resp) {
+      var newCollections = [];
+      resp.data.forEach(function(coll) {
+        if (coll.name != 'trace_log') {
+          newCollections.push(coll);
+        }
+      })
+      self.collections = newCollections;
+    },
+    function(errResponse) {alert("MongodbCtrl: ERROR(collections): " + errResponse);}
+  );
+
+  self.displayCollection = function(name) {
+    query = '?collection_name=' + name;
+    $log.log('MongodbCtrl: GET baf/MongoDBView' + query);
+    $http.get('baf/MongoDBView' + query).then(
+      function(resp) {
+        self.details = resp.data;
+        self.name = name;
+      },
+      function(errResponse) {alert("MongodbCtrl: ERROR(collection-details): " + errResponse);}
     );
-
-    self.displayCollection = function(name) {
-        query = '?collection_name=' + name;
-        $log.log('MongodbCtrl: GET baf/MongoDBView' + query);
-        $http.get('baf/MongoDBView' + query).then(
-            function(resp) {
-                self.details = resp.data;
-                self.name = name;
-            },
-            function(errResponse) {alert("MongodbCtrl: ERROR(collection-details): " + errResponse);}
-        );
-    }
+  }
 }]);
 

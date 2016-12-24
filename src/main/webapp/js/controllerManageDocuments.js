2dbaef536ab56caca2642fada0d7a1527b609ed3
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-12-24 14:25:20 +0530

    Periodic updates

--- src/main/webapp/js/controllerManageDocuments.js
+++ src/main/webapp/js/controllerManageDocuments.js
@@ -1,11 +1,21 @@
 ﻿angular.module('BuildWhizApp')
 
-.controller("ManageDocumentsCtrl", ['$log', '$http', 'AuthenticationService', function ($log, $http, AuthService) {
+.controller("ManageDocumentsCtrl", ['$log', '$http', 'AuthenticationService', '$window',
+      function ($log, $http, AuthService, $window) {
 
   var self = this;
 
+  self.documentCategories = ['Architecture', 'Civil', 'Electrical Design', 'Interior Design', 'Landscape Design',
+      'Mechanical Design', 'Plumbing Design', 'Structure'];
+  self.documentSubcategories = [];
+
   self.documentList = [];
+  self.documentCount = 0;
   self.currentFilterKey = 'All';
+  self.currentAuthorKey = "Any";
+  self.currentContentKey = "Any";
+  self.currentSubcategoryKey = "Any";
+  self.currentCategoryKey = "Any";
 
   self.fetchDocuments = function(filter) {
     self.currentFilterKey = filter ? filter : 'All';
@@ -19,6 +29,70 @@
       },
       function(errResponse) {alert("HomeCtrl: ERROR(collection-details): " + errResponse);}
     );
+  }
+
+  self.fetchDocumentsByCategory = function(categoryKey) {
+    $log.log('Setting category=' + categoryKey);
+    self.currentCategoryKey = categoryKey;
+  }
+
+  self.fetchDocumentsBySubcategory = function(subcategoryKey) {
+    $log.log('Setting subcategory=' + subcategoryKey);
+    self.currentSubcategoryKey = subcategoryKey;
+  }
+
+  self.fetchDocumentsByContent = function(contentKey) {
+    $log.log('Setting content=' + contentKey);
+    self.currentContentKey = contentKey;
+  }
+
+  self.fetchDocumentsByAuthor = function(authorKey) {
+    $log.log('Setting author=' + authorKey);
+    self.currentAuthorKey = authorKey;
+  }
+
+  self.upload = function() {
+    $log.log('Called upload()');
+    var uploadButton = $window.document.getElementById('document-upload-button');
+    uploadButton.addEventListener('change', self.uploadOk, false);
+    uploadButton.click();
+    $log.log('Exiting upload()');
+  }
+
+  self.uploadOk = function(evt) {
+    $log.log('Called uploadOk()');
+    var uploadButton = $window.document.getElementById('document-upload-button');
+    uploadButton.removeEventListener('change', self.uploadOk, false);
+    var files = evt.target.files; // FileList of File objects
+    if (files.length > 0) {
+      var formData = new FormData();
+      angular.forEach(files, function(file, index) {
+        formData.append(file.name, file, file.name);
+        $log.log('formData.append(name: ' + file.name);
+      });
+      /*formData.append('end-marker', 'end-marker-content');
+      var query = 'baf/ProgressReportSubmit?person_id=' + AuthService.data._id +
+          '&activity_id=' + self.selectedTask.activity_id + '&action_name=' + self.selectedTask.name +
+          '&submission_title=' + self.submissionTitle + '&submission_type=' + self.submissionType +
+          '&submission_message=' + self.submissionMessage;
+      $http.post(query, formData, {transformRequest: angular.identity, headers: {'Content-Type': undefined}}).then(
+        function() {
+          self.submissionAttachments = [];
+          self.submissionType = 'Progress';
+          self.submissionTitle = '';
+          self.submissionMessage = '';
+          $log.log('OK submitProgressReport')
+        },
+        function() {
+          $log.log('ERROR submitProgressReport')
+        }
+      )*/
+    }
+    $log.log('Exiting uploadOk()');
+  }
+
+  self.listFiles = function() {
+    $log.log('Called listFiles()');
   }
 
 }]);

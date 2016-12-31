22fcb22215c903e5af5dbf9127b5027d4bc9c59a
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2016-12-31 19:01:27 +0530

    Prepopulate login username to facilitate testing

--- src/main/webapp/js/controllerMain.js
+++ src/main/webapp/js/controllerMain.js
@@ -1,11 +1,24 @@
 angular.module('BuildWhizApp')
 
-.controller('MainController', ['AuthenticationService', '$log', function (AuthService, $log) {
+.controller('MainController', ['AuthenticationService', '$http', '$log', function (AuthService, $http, $log) {
   var self = this;
 
   self.isNavCollapsed = true;
   self.loggedIn = false;
   self.tryAgain = false;
+  self.username = '';
+
+  $log.log('HTTP GET api/Environment');
+  $http.get('api/Environment').then(
+    function(response) {
+      var env = response.data;
+      $log.log('api/Environment -> ' + JSON.stringify(env));
+      if (env.timezone_raw_offset == 19800000) {
+        // to facilitate testing on local machine
+        self.username = "sanjay.dasgupta@buildwhiz.com";
+      }
+    }
+  );
 
   self.login = function() {
     AuthService.login(self.username, self.password, self);

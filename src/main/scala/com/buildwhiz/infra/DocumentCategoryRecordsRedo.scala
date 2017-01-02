5c585a9ac7559c06dd3a61a6f36d03982379aac2
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-01-02 10:04:46 +0530

    Document categories and subcategories

--- src/main/scala/com/buildwhiz/infra/DocumentCategoryRecordsRedo.scala
+++ src/main/scala/com/buildwhiz/infra/DocumentCategoryRecordsRedo.scala
@@ -5,42 +5,30 @@ import org.bson.types.ObjectId
 import BWMongoDB3._
 
 import scala.collection.JavaConverters._
-
 object DocumentCategoryRecordsRedo extends App {
 
-  val csv = """Document Category,Management,,,Design,,,,,,,,,,,,,Construction,,,,,,,,,,
-      |,Owner,Project Manager,Site Manager,Architect,Structural Engineer,Civil Engineer,Surveyor,Mechanical Engineer,Electrical Engineer,Plumbing Engineer,Landscape Designer,Building Scientist,Geotechnical Engineer,Interior Designer,Energy Conservationist,Arborist,Foundation,Framer,Plumber,Electrician,Mechanic,Civil Contractor,Concrete,Drywall,Roofer,Stucco,Siding
-      |Architecture,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X
-      |Structure,X,X,X,X,X,X,,X,X,X,,X,X,,X,,X,X,X,X,X,,X,,,,
-      |Interior Design,X,X,X,X,,,,X,X,X,,,,X,X,,,X,X,X,X,,,,,,
-      |Landscape Design,X,X,X,X,,X,,,X,X,X,X,,X,,,,,X,X,,X,,,,,
-      |Civil ,X,X,X,X,X,X,,,,,,X,X,,,,,,,,,X,X,,,,
-      |Mechanical Design,X,X,X,X,X,X,,X,X,X,,,,X,X,,,,,X,X,,,,,,
-      |Electrical Design,X,X,X,X,,,,,X,,,,,X,X,,,,X,X,,,,,,,
-      |Plumbing Design,X,X,X,X,,,,,,,,,,X,,,,,,,,,,,,,""".stripMargin
+  private val data =
+    """{"_id": ObjectId("5869d46692982d1c34f12d10"), "category": "ArchiCAD"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d11"), "category": "Architecture"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d12"), "category": "Architecture", "subCategory" : "CD"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d13"), "category": "Building Science"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d14"), "category": "Civil"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d15"), "category": "Contracts"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d16"), "category": "Electrical"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d17"), "category": "Elevator"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d18"), "category": "GeoTech Field Reports"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d19"), "category": "Interior"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d1a"), "category": "Material Specs"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d1b"), "category": "Mechanical"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d1c"), "category": "Permits"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d1d"), "category": "Plumbing"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d1e"), "category": "Reports"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d1f"), "category": "Revit"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d20"), "category": "Special Inspection Reports"}
+      |{"_id": ObjectId("5869d46692982d1c34f12d21"), "category": "Structure"}
+      |""".stripMargin
 
-  private def store(categories: Seq[(String, Seq[(String, String)])]): Unit = {
-    val withIds: Seq[(String, Seq[ObjectId])] = categories.map(c => {
-      (c._1, c._2.map(c2 => {
-        BWMongoDB3.roles_master.find(Map("category" -> c2._1, "name" -> c2._2)).asScala.head.y._id[ObjectId]}))
-    })
-    val docsWithIds = withIds.map(d => {
-      val doc = new Document("category", d._1)
-      doc.put("roleIds", d._2.asJava)
-      doc
-    })
+  private val documents = data.split("\n").map(line => Document.parse(line))
 
-    println(s"Original document_categories count: ${BWMongoDB3.roles_master.count()}")
-    BWMongoDB3.document_category_master.insertMany(docsWithIds.asJava)
-    println(s"Final document_categories count: ${BWMongoDB3.roles_master.count()}")
-  }
-
-  val lines: Seq[String] = csv.split("\n").map(_.trim)
-  val roleCategories: Seq[String] = lines.head.split(",", -1).tail.foldLeft(Nil: Seq[String])((seq, e) =>
-    if (e.isEmpty) seq.head +: seq else e +: seq).reverse
-  val roles: Seq[String] = lines.tail.head.split(",", -1).tail.toSeq
-  println(roles)
-  val documentDefinitions = lines.drop(2).map(_.split(",")).map(dd =>
-    dd.head.trim -> dd.tail.toSeq.zipWithIndex.filter(_._1.nonEmpty).map(t => (roleCategories(t._2), roles(t._2))))
-  store(documentDefinitions)
+  BWMongoDB3.document_category_master.insertMany(documents.toSeq.asJava)
 }

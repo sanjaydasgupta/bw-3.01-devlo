e2830f34e7744e801f1bd7b34f23e319d7001668
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-01-06 20:44:12 +0530

    Removed unused views and controllers, other associated changes

--- src/main/webapp/js/module.js
+++ src/main/webapp/js/module.js
@@ -7,8 +7,6 @@ angular.module('BuildWhizApp', ['ngRoute', 'ui.bootstrap'])
     }).when('/documents', {
         templateUrl: 'html/manage-documents.html',
         controller: 'ManageDocumentsCtrl as manageDocsCtrl'
-        //templateUrl: 'html/documents.html',
-        //controller: 'DocumentsCtrl as documentsCtrl'
     }).when('/projects', {
         templateUrl: 'html/projects.html',
         controller: 'ProjectsCtrl as projectsCtrl'
@@ -24,9 +22,6 @@ angular.module('BuildWhizApp', ['ngRoute', 'ui.bootstrap'])
     }).when('/mongodb', {
         templateUrl: 'html/mongodb.html',
         controller: 'MongodbCtrl as mongoCtrl'
-    }).when('/manageDocuments', {
-        templateUrl: 'html/manage-documents.html',
-        controller: 'ManageDocumentsCtrl as manageDocsCtrl'
     }).when('/manageUsers', {
         templateUrl: 'html/manage-users.html',
         controller: 'ManageUsersCtrl as usersCtrl'

e0e2793961ded3badc142792a72278dc90817923
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-01-08 06:49:03 +0530

    Added seconds field to date-time display

--- src/main/scala/com/buildwhiz/DateTimeUtils.scala
+++ src/main/scala/com/buildwhiz/DateTimeUtils.scala
@@ -6,7 +6,7 @@ import java.util.{Date, TimeZone}
 trait DateTimeUtils {
 
   def dateTimeString(milliSeconds: Long, timeZoneCode: Option[String] = Some("GMT")): String = {
-    val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm z")
+    val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z")
     timeZoneCode.foreach(tzc => simpleDateFormat.setTimeZone(TimeZone.getTimeZone(tzc)))
     simpleDateFormat.format(new Date(milliSeconds))
   }

60a5e7f8a772fc3919fcc63409a99d95d1707ddc
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-01-09 09:18:32 +0530

    System Monitoring update

---
+++ src/main/scala/com/buildwhiz/etc/SystemMonitor.scala
@@ -0,0 +1,36 @@
+package com.buildwhiz.etc
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+import com.buildwhiz.infra.BWLogger
+import com.buildwhiz.{CryptoUtils, HttpUtils}
+
+import scala.sys.process._
+
+class SystemMonitor extends HttpServlet with HttpUtils with CryptoUtils {
+
+  private def dfh(response: HttpServletResponse): Unit = {
+    val output: String = "df -h".!!
+    val lines = output.split("\n").map(_.trim).filter(_.nonEmpty)
+    val fieldedLines = lines.map(_.split("\\s+"))
+    val fieldedLines2 = fieldedLines.head.init +: fieldedLines.tail
+    val json = fieldedLines2.map(_.mkString("[\"", "\", \"", "\"]")).mkString("[", ", ", "]")
+    response.getWriter.print(json)
+    response.setContentType("application/json")
+  }
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
+    try {
+      dfh(response)
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        t.printStackTrace()
+        throw t
+    }
+  }
+}
--- src/main/webapp/js/controllerSystemMonitor.js
+++ src/main/webapp/js/controllerSystemMonitor.js
@@ -4,4 +4,18 @@
 
   var self = this;
 
+  self.dfh = [];
+
+  var dfhQuery = 'etc/SystemMonitor?command=df -h'
+  $log.log('Calling POST ' + dfhQuery)
+  $http.post(dfhQuery).then(
+    function(resp) {
+      self.dfh = resp.data;
+      $log.log('OK POST ' + dfhQuery + ', rows: ' + self.dfh.length)
+    },
+    function(resp) {
+      $log.log('ERROR POST ' + dfhQuery)
+    }
+  );
+
 }]);

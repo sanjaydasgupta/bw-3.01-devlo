f326ac6de7b69a0552a372edff6852ca599badb8
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-01-09 11:02:21 +0530

    System Monitor updates

--- src/main/scala/com/buildwhiz/etc/SystemMonitor.scala
+++ src/main/scala/com/buildwhiz/etc/SystemMonitor.scala
@@ -19,11 +19,24 @@ class SystemMonitor extends HttpServlet with HttpUtils with CryptoUtils {
     response.setContentType("application/json")
   }
 
+  private def topbn(response: HttpServletResponse): Unit = {
+    val output: String = "top -b -n 1".!!
+    val lines = output.split("\n").map(_.trim).filter(_.nonEmpty).dropWhile(line => !line.startsWith("PID"))
+    val fieldedLines = lines.map(_.split("\\s+"))
+    val json = fieldedLines.map(_.mkString("[\"", "\", \"", "\"]")).mkString("[", ", ", "]")
+    response.getWriter.print(json)
+    response.setContentType("application/json")
+  }
+
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
     try {
-      dfh(response)
+      parameters("command") match {
+        case "dfh" => dfh(response)
+        case "topbn" => topbn(response)
+        case _ =>
+      }
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)
     } catch {
--- src/main/webapp/js/controllerSystemMonitor.js
+++ src/main/webapp/js/controllerSystemMonitor.js
@@ -4,18 +4,33 @@
 
   var self = this;
 
-  self.dfh = [];
+  self.output = [];
 
-  var dfhQuery = 'etc/SystemMonitor?command=df -h'
-  $log.log('Calling POST ' + dfhQuery)
-  $http.post(dfhQuery).then(
-    function(resp) {
-      self.dfh = resp.data;
-      $log.log('OK POST ' + dfhQuery + ', rows: ' + self.dfh.length)
-    },
-    function(resp) {
-      $log.log('ERROR POST ' + dfhQuery)
-    }
-  );
+  self.monitor = function(command) {
+    var dfhQuery = 'etc/SystemMonitor?command=' + command;
+    $log.log('Calling POST ' + dfhQuery)
+    $http.post(dfhQuery).then(
+      function(resp) {
+        self.output = resp.data;
+        $log.log('OK POST ' + dfhQuery + ', rows: ' + self.output.length)
+      },
+      function(resp) {
+        $log.log('ERROR POST ' + dfhQuery)
+      }
+    );
+  }
+
+  self.bgColor = function(row) {
+    var color = 'white'
+    var patterns = ['/', '/home', 'mongodb', 'java'];
+    patterns.forEach(function(pattern) {
+      row.forEach(function(rowElement) {
+        if (rowElement == pattern) {
+          color = 'yellow';
+        }
+      })
+    });
+    return color;
+  }
 
 }]);

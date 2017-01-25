7eddb526f32460b25187259d1db08034a605aa6c
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-01-25 09:18:32 +0530

    RFIClose service created

---
+++ src/main/scala/com/buildwhiz/baf/RFIClose.scala
@@ -0,0 +1,29 @@
+package com.buildwhiz.baf
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+import com.buildwhiz.infra.{BWLogger, BWMongoDB3}
+import BWMongoDB3._
+import com.buildwhiz.HttpUtils
+import org.bson.types.ObjectId
+
+class RFIClose extends HttpServlet with HttpUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
+    try {
+      val rfiOid = new ObjectId(parameters("rfi_id"))
+      val updateResult = BWMongoDB3.rfi_messages.updateOne(Map("_id" -> rfiOid), Map("status" -> "closed"))
+      if (updateResult.getModifiedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB error: $updateResult")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        t.printStackTrace()
+        throw t
+    }
+  }
+}

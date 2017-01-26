b438325748f36d127c9db32b8a146cf3ce6b912a
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-01-26 13:35:26 +0530

    Added email notification for password-change

--- src/main/scala/com/buildwhiz/baf/UserPasswordSet.scala
+++ src/main/scala/com/buildwhiz/baf/UserPasswordSet.scala
@@ -2,12 +2,20 @@ package com.buildwhiz.baf
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
-import com.buildwhiz.{CryptoUtils, HttpUtils}
+import com.buildwhiz.{CryptoUtils, HttpUtils, MailUtils}
 import com.buildwhiz.infra.{BWLogger, BWMongoDB3}
 import BWMongoDB3._
 import org.bson.types.ObjectId
 
-class UserPasswordSet extends HttpServlet with HttpUtils with CryptoUtils {
+class UserPasswordSet extends HttpServlet with HttpUtils with CryptoUtils with MailUtils {
+
+  private val mailBody =
+    """Your password on '430forest.com' has just been changed.
+      |If you have not changed it, please notify your contact person.
+      |Please do not reply to this email, this mailbox is not monitored.
+      |
+      |430 Forest project
+    """.stripMargin
 
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     val parameters = getParameterMap(request)
@@ -20,6 +28,7 @@ class UserPasswordSet extends HttpServlet with HttpUtils with CryptoUtils {
         Map("$set" -> Map(s"password" -> md5(newPassword))))
       if (updateResult.getModifiedCount == 0)
         throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+      sendMail(personOid, "Password changed on 430forest.com", mailBody)
       response.setStatus(HttpServletResponse.SC_OK)
     } catch {
       case t: Throwable =>
@@ -29,4 +38,5 @@ class UserPasswordSet extends HttpServlet with HttpUtils with CryptoUtils {
     }
     BWLogger.log(getClass.getName, "doPost", "EXIT-OK", request)
   }
+
 }

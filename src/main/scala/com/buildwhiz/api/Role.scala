ba123d27b223257957967be87ca03ab71765d4b2
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-02-01 16:45:53 +0530

    Added sorter for Role records REST GET

--- src/main/scala/com/buildwhiz/api/Role.scala
+++ src/main/scala/com/buildwhiz/api/Role.scala
@@ -2,10 +2,20 @@ package com.buildwhiz.api
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
+import com.buildwhiz.infra.BWMongoDB3.DynDoc
+import org.bson.Document
+
 class Role extends HttpServlet with RestUtils {
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    handleRestGet(request, response, "Role", "roles_master")
+    def roleSorter = (a: Document, b: Document) => {
+      val a1: DynDoc = a
+      val b1: DynDoc = b
+      val first = s"${a1.category[String]}/${a1.name[String]}".toLowerCase
+      val second = s"${b1.category[String]}/${b1.name[String]}".toLowerCase
+      first < second
+    }
+    handleRestGet(request, response, "Role", "roles_master", sorter=Some(roleSorter))
   }
 
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {

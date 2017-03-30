eb4885104e23b23acdaea64a2107f91f77d0a552
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-03-30 17:22:45 +0530

    Added email enable/disable capability to user profile page

--- src/main/webapp/js/controllerUserProfile.js
+++ src/main/webapp/js/controllerUserProfile.js
@@ -2,29 +2,48 @@
 
 .controller("UserProfileCtrl", ['$log', '$http', 'AuthenticationService', function ($log, $http, AuthService) {
 
-    var self = this;
+  var self = this;
 
-    self.userPasswordSetDisabled = function() {
-        return !self.oldPassword || !self.newPassword || !self.cnfPassword || (self.newPassword != self.cnfPassword);
+  self.emailEnabled = false;
+  if (AuthService.data.email_enabled) {
+    self.emailEnabled = true;
+  }
+
+  self.userPasswordSetDisabled = function() {
+      return !self.oldPassword || !self.newPassword || !self.cnfPassword || (self.newPassword != self.cnfPassword);
+  }
+
+  self.userPasswordSet = function() {
+    var sPersonID = AuthService.data._id;
+    var postData = {old_password: self.oldPassword, new_password: self.newPassword, person_id: sPersonID};
+    $log.log('Calling /baf/UserPasswordSet' + postData);
+    $http.post('baf/UserPasswordSet', postData).then(
+      function() {
+        self.oldPassword = '';
+          self.newPassword = '';
+          self.cnfPassword = '';
+        },
+        function() {
+          //self.oldPassword = '';
+          //self.newPassword = '';
+          //self.cnfPassword = '';
+          alert('Failed to set password');
+        }
+      )
     }
 
-    self.userPasswordSet = function() {
-        var sPersonID = AuthService.data._id;
-        var postData = {old_password: self.oldPassword, new_password: self.newPassword, person_id: sPersonID};
-        $log.log('Calling /baf/UserPasswordSet' + postData);
-        $http.post('baf/UserPasswordSet', postData).then(
-            function() {
-                self.oldPassword = '';
-                self.newPassword = '';
-                self.cnfPassword = '';
-            },
-            function() {
-                //self.oldPassword = '';
-                //self.newPassword = '';
-                //self.cnfPassword = '';
-                alert('Failed to set password');
-            }
-        )
-    }
+  self.toggleEmail = function() {
+    var query = 'baf/UserPropertySet?person_id=' + AuthService.data._id + '&property=email_enabled&value=' +
+        self.emailEnabled;
+    $log.log('POST ' + query);
+    $http.post(query).then(
+      function(res) {
+        $log.log('OK POST ' + query);
+      },
+      function(res) {
+        alert('ERROR email setting failed');
+      }
+    )
+  }
 
 }]);

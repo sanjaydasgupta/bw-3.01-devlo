b83cac0c384c1623f47e6f3af2c23acdbb11e579
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-04-04 11:53:49 +0530

    Added tasks to home page

--- src/main/webapp/js/controller-home.js
+++ src/main/webapp/js/controller-home.js
@@ -8,6 +8,7 @@
   self.projectList = [];
 //  self.documentList = [];
   self.rfiStatus = '';
+  self.tasksStatus = '';
 
   self.fetchRfiStatus = function() {
     var query = 'baf/RFIFetchStatus?person_id=' + AuthService.data._id;
@@ -16,6 +17,22 @@
       function(resp) {
         var status = resp.data;
         self.rfiStatus = status.unread + ' / ' + status.total;
+        $log.log('OK-HomeCtrl: got ' + JSON.stringify(status));
+      },
+      function(errResponse) {alert("HomeCtrl: ERROR(collection-details): " + errResponse);}
+    );
+  }
+
+  self.fetchTasksStatus = function() {
+    var query = 'baf/OwnedTasksStatusSummary?person_id=' + AuthService.data._id;
+    $log.log('HomeCtrl: GET ' + query);
+    $http.get(query).then(
+      function(resp) {
+        var status = resp.data;
+        self.tasksStatus =
+            (status.hasOwnProperty('waiting') ? status.waiting : 0) + '/' +
+            (status.hasOwnProperty('defined') ? status.defined : 0) + '/' +
+            (status.hasOwnProperty('ended') ? status.ended : 0);
         $log.log('OK-HomeCtrl: got ' + JSON.stringify(status));
       },
       function(errResponse) {alert("HomeCtrl: ERROR(collection-details): " + errResponse);}
@@ -42,6 +59,7 @@
     self.fullName = AuthService.data.first_name + ' ' + AuthService.data.last_name;
     self.fetchRfiStatus();
     self.fetchProjects();
+    self.fetchTasksStatus();
   }
 
   self.refresh();

0c5cd7407b3c493357b7cca1e516e452ad5d9e17
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-05-15 19:40:04 +0530

    Added internal BPMN element references to XML

--- src/main/scala/com/buildwhiz/baf/PhaseBpmnXml.scala
+++ src/main/scala/com/buildwhiz/baf/PhaseBpmnXml.scala
@@ -3,17 +3,52 @@ package com.buildwhiz.baf
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 import com.buildwhiz.utils.{BWLogger, BpmnUtils, HttpUtils}
+import org.bson.types.ObjectId
+import org.bson.Document
+import com.buildwhiz.infra.BWMongoDB3
+import BWMongoDB3._
 
 import scala.collection.mutable
 
 class PhaseBpmnXml extends HttpServlet with HttpUtils with BpmnUtils {
+
+  private def getVariables(phase: DynDoc, processName: String): Seq[Document] = {
+    val variables: Seq[DynDoc] = phase.variables[Many[Document]].filter(_.bpmn_name[String] == processName)
+    variables.map(variable => {
+      variable.asDoc
+    })
+  }
+
+  private def getTimers(phase: DynDoc, processName: String): Seq[Document] = {
+    val timers: Seq[DynDoc] = phase.timers[Many[Document]].filter(_.bpmn_name[String] == processName)
+    timers.map(timer => {
+      timer.id = timer.bpmn_name[String]
+      timer.remove("bpmn_name")
+      timer.asDoc
+    })
+  }
+
+  private def getActivities(phase: DynDoc, processName: String): Seq[Document] = {
+    val activityOids: Seq[ObjectId] = phase.activity_ids[Many[ObjectId]]
+    val activities: Seq[DynDoc] = BWMongoDB3.activities.
+      find(Map("_id" -> Map("$in" -> activityOids), "bpmn_name" -> processName))
+    val returnActivities = activities.map(activity => {
+      val actions: Seq[DynDoc] = activity.actions[Many[Document]]
+      val tasks = actions.map(action => {
+        new Document("type", action.`type`[String]).append("name", action.name[String]).
+          append("status", action.status[String]).append("duration", action.duration[String])
+      })
+      new Document("id", activity.bpmn_id[String]).append("status", activity.status[String]).append("tasks", tasks)
+    })
+    returnActivities
+  }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, "doGet", "ENTRY", request)
     try {
       val bpmnFileName = parameters("bpmn_name").replaceAll(" ", "-")
-      val processDefinition = getProcessDefinition(bpmnFileName)
+      val phaseOid = new ObjectId(parameters("phase_id"))
       val processModelStream = getProcessModel(bpmnFileName)
       val blockBuffer = new Array[Byte](4096)
       val byteBuffer = mutable.Buffer.empty[Byte]
@@ -28,8 +63,14 @@ class PhaseBpmnXml extends HttpServlet with HttpUtils with BpmnUtils {
       }
       copyModelToOutput()
       val xml = new String(byteBuffer.toArray)
-      response.getWriter.println(xml)
-      response.setContentType("application/xml")
+      val phase: DynDoc = BWMongoDB3.phases.find(Map("_id" -> phaseOid)).head
+      val processVariables = getVariables(phase, bpmnFileName)
+      val processTimers = getTimers(phase, bpmnFileName)
+      val processActivities = getActivities(phase, bpmnFileName)
+      val returnValue = new Document("xml", xml).append("variables", processVariables).
+        append("timers", processTimers).append("activities", processActivities)
+      response.getWriter.println(bson2json(returnValue))
+      response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.log(getClass.getName, "doGet", "EXIT-OK", request)
     } catch {
--- src/main/webapp/js/controller-bpmn.js
+++ src/main/webapp/js/controller-bpmn.js
@@ -1,23 +1,28 @@
 angular.module('BuildWhizApp')
 
-.controller("BpmnCtrl", ['$log', '$http', 'AuthenticationService', '$window', '$routeParams', '$sce',
-    function ($log, $http, AuthService, $window, $routeParams) {
+.controller("BpmnCtrl", ['$log', '$http', '$routeParams', '$sce',
+    function ($log, $http, $routeParams) {
 
   var self = this;
 
-  self.processName = $routeParams.hasOwnProperty("process") ? $routeParams.process : null;
-  self.projectId = $routeParams.hasOwnProperty("project_id") ? $routeParams.project_id : null;
-  self.projectName = $routeParams.hasOwnProperty("project_name") ? $routeParams.project_name : null;
-  self.phaseId = $routeParams.hasOwnProperty("phase_id") ? $routeParams.phase_id : null;
+  self.processName = $routeParams.process;
+  self.projectId = $routeParams.project_id;
+  self.projectName = $routeParams.project_name;
+  self.phaseId = $routeParams.phase_id;
 
-  $log.log('Process: ' + self.processName);
-  $log.log('projectId: ' + self.projectId);
-  $log.log('PhaseId: ' + self.phaseId);
+  $log.log('Process-Name: ' + self.processName);
+  $log.log('Project-Id: ' + self.projectId);
+  $log.log('Project-Name: ' + self.projectName);
+  $log.log('Phase-Id: ' + self.phaseId);
 
   // https://github.com/bpmn-io/bower-bpmn-js
   // https://github.com/bpmn-io/bpmn-js-examples/tree/master/interaction
   // https://github.com/bpmn-io/bpmn-js-examples/tree/master/overlay
 
+  var processTimers = [];
+  var processVariables = [];
+  var processActivities = [];
+  var selectedElement = null;
   var bpmnViewer = new BpmnJS({container: '#canvas'});
   var overlayId = null;
   var overlays = bpmnViewer.get('overlays');
@@ -30,39 +35,65 @@ angular.module('BuildWhizApp')
         (parseInt(height) + 10) + 'px;">&nbsp;</div>';
   }
 
-  var extraEventHandlingCode = function() {
+  var extraEventHandlingCode = function(element) {
+    $log.log('Called extraEventHandlingCode()')
     // All additions here
   }
 
-  var eventHandler = function(e) {
-    // e.element = the model element
-    // e.gfx = the graphical element
+  var selectElement = function(element) {
+    $log.log('Called selectElement()')
     if (overlayId != null) {
       overlays.remove(overlayId);
     }
-    overlayId = overlays.add(e.element.id, {
+    overlayId = overlays.add(element.id, {
       position: {top: -5, left: -5},
-      html: overlayHtml(e.element.width, e.element.height)
+      html: overlayHtml(element.width, element.height)
     });
-    $log.log(JSON.stringify(e.element));
-    extraEventHandlingCode();
+    selectedElement = element;
+  }
+
+  var eventHandler = function(event) {
+    // e.element = the model element
+    // e.gfx = the graphical element
+    var element = event.element;
+    $log.log(JSON.stringify(element));
+    selectElement(element);
+    extraEventHandlingCode(element);
   }
 
   events.forEach(function(event) {
     eventBus.on(event, eventHandler);
   });
 
-  var q = 'baf/PhaseBpmnXml?bpmn_name=' + self.processName;
+  var annotateBpmn = function() {
+    processTimers.forEach(function(timer) {
+      $log.log(JSON.stringify(timer));
+    });
+    processVariables.forEach(function(variable) {
+      $log.log(JSON.stringify(variable));
+    });
+    processActivities.forEach(function(activity) {
+      $log.log(JSON.stringify(activity));
+    });
+  }
+
+  var q = 'baf/PhaseBpmnXml?bpmn_name=' + self.processName + '&phase_id=' + self.phaseId;
   $http.get(q).then(
     function(resp) {
-      bpmnViewer.importXML(resp.data, function(err) {
+      bpmnViewer.importXML(resp.data.xml, function(err) {
         if (err) {
           // import failed :-(
-          $log.log('FAIL importXML');
+          $log.log('FAIL importXML: ' + err);
         } else {
           // we did well!
-          var canvas = viewer.get('canvas');
+          var canvas = bpmnViewer.get('canvas');
           canvas.zoom('fit-viewport');
+          processTimers = resp.data.timers;
+          processVariables = resp.data.variables;
+          processActivities = resp.data.activities;
+          $log.log('OK importXML, timers: ' + processTimers.length + ', variables: ' + processVariables.length +
+            ', activities: ' + processActivities.length);
+          annotateBpmn();
         }
       })
       $log.log('OK GET ' + q);

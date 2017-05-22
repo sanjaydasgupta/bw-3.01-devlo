cc6fbb431c6607dfa79254f1082899f5e200ddb7
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-05-22 22:32:45 +0530

    Clearly distinguished transition name and current status name

--- src/main/scala/com/buildwhiz/jelly/TimerTransitions.scala
+++ src/main/scala/com/buildwhiz/jelly/TimerTransitions.scala
@@ -15,12 +15,15 @@ class TimerTransitions extends ExecutionListener with BpmnUtils {
       val phaseOid = new ObjectId(de.getVariable("phase_id").asInstanceOf[String])
       val bpmnName = getBpmnName(de)
       val timerId = de.getCurrentActivityId
-      val event = de.getEventName
+      val (event, status) = de.getEventName match {
+        case "start" => ("start", "started")
+        case "end" => ("end", "ended")
+      }
       val phase: DynDoc = BWMongoDB3.phases.find(Map("_id" -> phaseOid)).head
       val timers: Seq[DynDoc] = phase.timers[Many[Document]]
       val timerIdx = timers.indexWhere(t => t.bpmn_id[String] == timerId && t.bpmn_name[String] == bpmnName)
       val updateResult = BWMongoDB3.phases.updateOne(Map("_id" -> phaseOid),
-          Map("$set" -> Map(s"timers.$timerIdx.status" -> event),
+          Map("$set" -> Map(s"timers.$timerIdx.status" -> status),
           "$addToSet" -> Map(s"timers.$timerIdx.timestamps" -> Map(event -> System.currentTimeMillis))))
       if (updateResult.getModifiedCount == 0)
         throw new IllegalArgumentException(s"MongoDB error: $updateResult")

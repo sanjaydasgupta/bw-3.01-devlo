23945ad43b58abe1441d7a3e8196fc844e0e35f5
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-05-24 08:12:28 +0530

    Initial load of new trait

---
+++ src/main/scala/com/buildwhiz/utils/ProjectUtils.scala
@@ -0,0 +1,23 @@
+package com.buildwhiz.utils
+
+import com.buildwhiz.infra.BWMongoDB3
+import BWMongoDB3._
+import org.bson.Document
+
+trait ProjectUtils extends DateTimeUtils {
+
+  def getActivityDuration(activity: DynDoc): String = {
+    val actions: Seq[DynDoc] = activity.actions[Many[Document]]
+    val prerequisitesMs = actions.filter(_.`type`[String] == "prerequisite") match {
+      case Nil => 0
+      case prerequisites => prerequisites.map(p => duration2ms(p.duration[String])).max
+    }
+    val reviewsMs = actions.filter(_.`type`[String] == "review") match {
+      case Nil => 0
+      case reviews => reviews.map(p => duration2ms(p.duration[String])).max
+    }
+    val mainMs = duration2ms(actions.find(_.`type`[String] == "main").get.duration[String])
+    ms2duration(prerequisitesMs + reviewsMs + mainMs)
+  }
+
+}

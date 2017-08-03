48ffc65b53156404228eef00908b6ae8ae55ca0a
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-08-03 16:48:13 +0530

    Added instance-global email-enable flag

--- src/main/scala/com/buildwhiz/utils/MailUtils.scala
+++ src/main/scala/com/buildwhiz/utils/MailUtils.scala
@@ -26,47 +26,50 @@ trait MailUtils {
 
   def sendMail(recipientOids: Seq[ObjectId], subject: String, body: String, request: Option[HttpServletRequest]): Unit = {
     BWLogger.log(getClass.getName, "sendMail", s"ENTRY", request)
-    val username = "430forest@gmail.com"
 
-    import scala.concurrent.ExecutionContext.Implicits.global
-    Future {
-      val session = MailUtils.session
+    val instanceInfo: DynDoc = BWMongoDB3.instance_info.find().head
+    val emailsEnabled = instanceInfo.has("emails_enabled") && instanceInfo.emails_enabled[Boolean]
 
-      val allowedRecs: Seq[DynDoc] = BWMongoDB3.persons.
-        find(Map("_id" -> Map("$in" -> recipientOids), "email_enabled" -> true))
-
-      if (allowedRecs.nonEmpty) {
-        val allowedOids = allowedRecs.map(_._id[ObjectId])
-        val message = new MimeMessage(session)
-        message.setFrom(new InternetAddress(username))
-        val persons: Seq[DynDoc] = BWMongoDB3.persons.find(Map("_id" -> Map("$in" -> allowedOids)))
-        val emails = persons.map(_.emails[Many[Document]].find(_.`type`[String] == "work").head.email[String])
-        //val names = persons.map(person => (person.first_name[String], person.last_name[String]))
-        //val emailParts = username.split('@')
-        val recipients: Seq[Address] = emails.flatMap(email => InternetAddress.parse(email))
-        message.setRecipients(Message.RecipientType.TO, recipients.toArray)
-        message.setSubject(subject)
-        val multipart = new MimeMultipart("alternative")
-        val textPart = new MimeBodyPart()
-        val bodyText = body + (if (body.endsWith("\n")) "" else "\n") + emailSignature
-        textPart.setText(bodyText, "utf-8")
-        multipart.addBodyPart(textPart)
-        val htmlPart = new MimeBodyPart()
-        val bodyHtml = s"""<span>${bodyText.replaceAll("\n", "<br/>")}</span>"""
-        htmlPart.setContent(bodyHtml, "text/html; charset=utf-8")
-        multipart.addBodyPart(htmlPart)
-        message.setContent(multipart)
-        message.saveChanges()
-        Transport.send(message)
-        BWLogger.log(getClass.getName, "sendMail", s"EXIT-OK (${recipients.length} recipients)", request)
-      } else {
-        BWLogger.log(getClass.getName, "sendMail", s"EXIT-OK (NO RECIPIENTS)", request)
+    if (emailsEnabled) {
+      import scala.concurrent.ExecutionContext.Implicits.global
+      Future {
+        val allowedRecipients: Seq[DynDoc] = BWMongoDB3.persons.
+          find(Map("_id" -> Map("$in" -> recipientOids), "email_enabled" -> true))
+        if (allowedRecipients.nonEmpty) {
+          val username = "430forest@gmail.com"
+          val allowedOids = allowedRecipients.map(_._id[ObjectId])
+          val session = MailUtils.session
+          val message = new MimeMessage(session)
+          message.setFrom(new InternetAddress(username))
+          val persons: Seq[DynDoc] = BWMongoDB3.persons.find(Map("_id" -> Map("$in" -> allowedOids)))
+          val emails = persons.map(_.emails[Many[Document]].find(_.`type`[String] == "work").head.email[String])
+          val recipients: Seq[Address] = emails.flatMap(email => InternetAddress.parse(email))
+          message.setRecipients(Message.RecipientType.TO, recipients.toArray)
+          message.setSubject(subject)
+          val multipart = new MimeMultipart("alternative")
+          val textPart = new MimeBodyPart()
+          val bodyText = body + (if (body.endsWith("\n")) "" else "\n") + emailSignature
+          textPart.setText(bodyText, "utf-8")
+          multipart.addBodyPart(textPart)
+          val htmlPart = new MimeBodyPart()
+          val bodyHtml = s"""<span>${bodyText.replaceAll("\n", "<br/>")}</span>"""
+          htmlPart.setContent(bodyHtml, "text/html; charset=utf-8")
+          multipart.addBodyPart(htmlPart)
+          message.setContent(multipart)
+          message.saveChanges()
+          Transport.send(message)
+          BWLogger.log(getClass.getName, "sendMail", s"EXIT-OK (${recipients.length} recipients)", request)
+        } else {
+          BWLogger.log(getClass.getName, "sendMail", s"EXIT-OK (NO RECIPIENTS)", request)
+        }
+      } onComplete {
+        case Failure(t: Throwable) =>
+          BWLogger.log(getClass.getName, "sendMail", s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+          t.printStackTrace()
+        case _ =>
       }
-    } onComplete {
-      case Failure(t: Throwable) =>
-        BWLogger.log(getClass.getName, "sendMail", s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
-        t.printStackTrace()
-      case _ =>
+    } else {
+      BWLogger.log(getClass.getName, "sendMail", s"EXIT-OK (INSTANCE-DISABLED)", request)
     }
   }
 

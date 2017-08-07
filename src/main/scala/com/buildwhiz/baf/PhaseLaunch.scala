225b239ed95934ffe97e5d43da254bc6870e1e16
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2017-08-07 18:18:26 +0530

    Authorization checks and enable/disable for timer duration setting

--- src/main/scala/com/buildwhiz/baf/PhaseLaunch.scala
+++ src/main/scala/com/buildwhiz/baf/PhaseLaunch.scala
@@ -17,13 +17,12 @@ class PhaseLaunch extends HttpServlet with HttpUtils {
       val phaseId = parameters("phase_id")
       val bpmnName = parameters("phase_bpmn_name")
       val rts = ProcessEngines.getDefaultProcessEngine.getRuntimeService
-      rts.startProcessInstanceByKey(bpmnName,
+      val processInstance = rts.startProcessInstanceByKey(bpmnName,
         Map("project_id" -> parameters("project_id"), "phase_id" -> phaseId, "top_level_bpmn" -> bpmnName))
-//      val updateResult = BWMongoDB3.phases.updateOne(Map("_id" -> new ObjectId(phaseId)),
-//        Map("$set" -> Map("status" -> "running", "timestamps.start" -> System.currentTimeMillis),
-//          "$inc" -> Map("active_bpmn_count" -> 1)))
-//      if (updateResult.getModifiedCount == 0)
-//        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+      val updateResult = BWMongoDB3.phases.updateOne(Map("_id" -> new ObjectId(phaseId)),
+        Map("$set" -> Map("process_instance_id" -> processInstance.getProcessInstanceId)))
+      if (updateResult.getModifiedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
       response.setContentType("text/plain")
       response.setStatus(HttpServletResponse.SC_OK)
       val thePhase: DynDoc = BWMongoDB3.phases.find(Map("_id" -> new ObjectId(phaseId))).head
--- src/main/scala/com/buildwhiz/baf/TimerDurationSet.scala
+++ src/main/scala/com/buildwhiz/baf/TimerDurationSet.scala
@@ -7,24 +7,32 @@ import com.buildwhiz.infra.BWMongoDB3
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import org.bson.Document
 import org.bson.types.ObjectId
+import org.camunda.bpm.engine.ProcessEngines
 
 class TimerDurationSet extends HttpServlet with HttpUtils {
+
+  private def duration2iso(duration: String): String = {
+    val Array(days, hours, minutes) = duration.split(":").map(_.toInt)
+    s"P${days}DT${hours}H${minutes}M"
+  }
+
+  private def formatDuration(d: String): String = {
+    val parts = d.split(":").map(_.toInt) match {
+      case arr3 @ Array(_, _, _) => arr3
+      case arr2 @ Array(_, _) => 0 +: arr2
+      case arr1 @ Array(_) => 0 +: 0 +: arr1
+    }
+    parts.map(p => f"$p%02d").mkString(":")
+  }
 
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
-    def formatDuration(d: String): String = {
-      val parts = d.split(":").map(_.toInt) match {
-        case arr3 @ Array(_, _, _) => arr3
-        case arr2 @ Array(_, _) => 0 +: arr2
-        case arr1 @ Array(_) => 0 +: 0 +: arr1
-      }
-      parts.map(p => f"$p%02d").mkString(":")
-    }
     try {
-      val duration = parameters("duration")
-      if (!duration.matches("(?:(?:\\d{1,2}\\:)?\\d{1,2}\\:)?\\d{1,2}"))
+      val rawDuration = parameters("duration")
+      if (!rawDuration.matches("(?:(?:\\d{1,2}\\:)?\\d{1,2}\\:)?\\d{1,2}"))
         throw new IllegalArgumentException("Bad duration format")
+      val duration = formatDuration(rawDuration)
       val phaseOid = new ObjectId(parameters("phase_id"))
       val thePhase: DynDoc = BWMongoDB3.phases.find(Map("_id" -> phaseOid)).head
       val bpmnName = parameters("bpmn_name")
@@ -37,17 +45,25 @@ class TimerDurationSet extends HttpServlet with HttpUtils {
             t.bpmn_name[String] == bpmnName)
         case _ => throw new IllegalArgumentException("Timer id or name not provided")
       }
-      val updateResult = BWMongoDB3.phases.updateOne(Map("_id" -> phaseOid),
-        Map("$set" -> Map(s"timers.$timerIdx.duration" -> formatDuration(duration))))
-      if (updateResult.getMatchedCount == 0)
-        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
-      else {
-        val topLevelBpmn = thePhase.bpmn_name[String]
-        PhaseBpmnTraverse.scheduleBpmnElements(topLevelBpmn, phaseOid, request, response)
-      }
+      if (thePhase.status[String] != "ended") {
+        if (thePhase.has("process_instance_id")) {
+          val rts = ProcessEngines.getDefaultProcessEngine.getRuntimeService
+          val processInstanceId = thePhase.process_instance_id[String]
+          rts.setVariable(processInstanceId, timers(timerIdx).variable[String], duration2iso(duration))
+        }
+        val updateResult = BWMongoDB3.phases.updateOne(Map("_id" -> phaseOid),
+          Map("$set" -> Map(s"timers.$timerIdx.duration" -> duration)))
+        if (updateResult.getMatchedCount == 0)
+          throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+        else {
+          val topLevelBpmn = thePhase.bpmn_name[String]
+          PhaseBpmnTraverse.scheduleBpmnElements(topLevelBpmn, phaseOid, request, response)
+        }
+        val timerLog = s"'${timers(timerIdx).name[String]}'"
+        BWLogger.audit(getClass.getName, "doPost", s"""Set duration of timer $timerLog""", request)
+      } else
+        BWLogger.log(getClass.getName, "doPost", "EXIT-OK (Process already ended, no changes)", request)
       response.setStatus(HttpServletResponse.SC_OK)
-      val timerLog = s"'${timers(timerIdx).name[String]}'"
-      BWLogger.audit(getClass.getName, "doPost", s"""Set duration of timer $timerLog""", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)

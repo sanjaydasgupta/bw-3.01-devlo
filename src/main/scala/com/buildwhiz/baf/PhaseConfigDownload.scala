5b140d7fa945a0fa97a211505b2e596da15ee1c1
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-02-06 12:08:19 +0530

    Separated out variables and timers sheet, and styled the header

--- src/main/scala/com/buildwhiz/baf/PhaseConfigDownload.scala
+++ src/main/scala/com/buildwhiz/baf/PhaseConfigDownload.scala
@@ -6,24 +6,36 @@ import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.apache.poi.ss.usermodel.HorizontalAlignment
+import org.apache.poi.xssf.usermodel.{XSSFSheet, XSSFWorkbook}
 import org.bson.types.ObjectId
 import org.bson.Document
 
 class PhaseConfigDownload extends HttpServlet with HttpUtils {
+
+  private def makeHeaderRow(taskSheet: XSSFSheet, headerInfo: Seq[(String, Int)]): Unit = {
+    val cellStyle = taskSheet.getWorkbook.createCellStyle()
+    cellStyle.setAlignment(HorizontalAlignment.CENTER)
+    val cellFont = cellStyle.getFont
+    cellFont.setBold(true)
+    cellStyle.setFont(cellFont)
+    cellStyle.setLocked(true)
+    val headerRow = taskSheet.createRow(0)
+    headerRow.setRowStyle(cellStyle)
+    for (hdrInfo <- headerInfo.zipWithIndex) {
+      val cell = headerRow.createCell(hdrInfo._2)
+      cell.setCellValue(hdrInfo._1._1)
+      cell.getSheet.setColumnWidth(hdrInfo._2, hdrInfo._1._2 * 125)
+    }
+  }
 
   private def addTasksSheet(workbook: XSSFWorkbook, project: DynDoc, phase: DynDoc, bpmnName: String): Int = {
     val activityIds: Seq[ObjectId] = phase.activity_ids[Many[ObjectId]]
     val activities: Seq[DynDoc] = BWMongoDB3.activities.find(Map("_id" -> Map("$in" -> activityIds),
       "bpmn_name" -> bpmnName))
     val taskSheet = workbook.createSheet("Tasks")
-    val headerRow = taskSheet.createRow(0)
-    val headerInfo = Seq(("Task", 60), ("Type", 20), ("Parent", 60), ("Duration", 20), ("Assignee", 50), ("Description", 100))
-    for (hdrInfo <- headerInfo.zipWithIndex) {
-      val cell = headerRow.createCell(hdrInfo._2)
-      cell.setCellValue(hdrInfo._1._1)
-      taskSheet.setColumnWidth(hdrInfo._2, hdrInfo._1._2 * 125)
-    }
+    val headerInfo = Seq(("Task-Name", 60), ("Type", 20), ("Parent", 60), ("Duration", 20), ("Assignee", 50), ("Description", 100))
+    makeHeaderRow(taskSheet, headerInfo)
     val projectName = project.name[String]
     val phaseName = phase.name[String]
     for (activity <- activities) {
@@ -47,41 +59,42 @@ class PhaseConfigDownload extends HttpServlet with HttpUtils {
     taskSheet.getLastRowNum + 1
   }
 
-  private def addVariablesAndTimersSheet(workbook: XSSFWorkbook, project: DynDoc, phase: DynDoc, bpmnName: String): Int = {
-    val variablesSheet = workbook.createSheet("Variables & Timers")
-    val headerRow = variablesSheet.createRow(0)
-    val headerInfo = Seq(("Variable/Timer", 20), ("Name", 60), ("Type", 20), ("Value/Duration", 20))
-    for (hdrInfo <- headerInfo.zipWithIndex) {
-      val cell = headerRow.createCell(hdrInfo._2)
-      cell.setCellValue(hdrInfo._1._1)
-      variablesSheet.setColumnWidth(hdrInfo._2, hdrInfo._1._2 * 125)
-    }
+  private def addVariablesSheet(workbook: XSSFWorkbook, project: DynDoc, phase: DynDoc, bpmnName: String): Int = {
+    val variablesSheet = workbook.createSheet("Variables")
+    val headerInfo = Seq(("Variable-Name", 60), ("Type", 20), ("Value", 20))
+    makeHeaderRow(variablesSheet, headerInfo)
     val projectName = project.name[String]
     val phaseName = phase.name[String]
     val variables: Seq[DynDoc] = phase.variables[Many[Document]].filter(_.bpmn_name[String] == bpmnName)
-    val timers: Seq[DynDoc] = phase.timers[Many[Document]].filter(_.bpmn_name[String] == bpmnName)
     for (variable <- variables) {
       val row = variablesSheet.createRow(variablesSheet.getLastRowNum + 1)
-      row.createCell(0).setCellValue("VARIABLE")
       val variableName = variable.label[String]
-      row.createCell(1).setCellValue(s"$variableName ($projectName/$phaseName)")
+      row.createCell(0).setCellValue(s"$variableName ($projectName/$phaseName)")
       val vType = variable.`type`[String]
-      row.createCell(2).setCellValue(s"$vType")
+      row.createCell(1).setCellValue(s"$vType")
       val value = variable.value[AnyRef].toString
-      row.createCell(3).setCellValue(s"$value")
+      row.createCell(2).setCellValue(s"$value")
       //val buffer = s"VARIABLE, $variableName ($projectName/$phaseName), $vType, $value"
     }
-    for (timer <- timers) {
-      val row = variablesSheet.createRow(variablesSheet.getLastRowNum + 1)
-      row.createCell(0).setCellValue("TIMER")
-      val timerName = timer.name[String]
-      row.createCell(1).setCellValue(s"$timerName ($projectName/$phaseName)")
-      row.createCell(2).setCellValue("DURATION")
-      val duration = timer.duration[String]
-      row.createCell(3).setCellValue(duration)
-      //val buffer = s"TIMER, $timerName ($projectName/$phaseName), DURATION, $duration"
-    }
     variablesSheet.getLastRowNum + 1
+  }
+
+  private def addTimersSheet(workbook: XSSFWorkbook, project: DynDoc, phase: DynDoc, bpmnName: String): Int = {
+    val timersSheet = workbook.createSheet("Timers")
+    val headerInfo = Seq(("Timer-Name", 60), ("Type", 20), ("Value", 20))
+    makeHeaderRow(timersSheet, headerInfo)
+    val projectName = project.name[String]
+    val phaseName = phase.name[String]
+    val timers: Seq[DynDoc] = phase.timers[Many[Document]].filter(_.bpmn_name[String] == bpmnName)
+    for (timer <- timers) {
+      val row = timersSheet.createRow(timersSheet.getLastRowNum + 1)
+      val timerName = timer.name[String]
+      row.createCell(0).setCellValue(s"$timerName ($projectName/$phaseName)")
+      row.createCell(1).setCellValue("DURATION")
+      val duration = timer.duration[String]
+      row.createCell(2).setCellValue(duration)
+    }
+    timersSheet.getLastRowNum + 1
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
@@ -95,11 +108,13 @@ class PhaseConfigDownload extends HttpServlet with HttpUtils {
       val project: DynDoc = BWMongoDB3.projects.find(Map("_id" -> projectOid)).head
       val phase: DynDoc = BWMongoDB3.phases.find(Map("_id" -> phaseOid)).head
       val taskCount = addTasksSheet(workbook, project, phase, bpmnName)
-      val vtCount = addVariablesAndTimersSheet(workbook, project, phase, bpmnName)
+      val variableCount = addVariablesSheet(workbook, project, phase, bpmnName)
+      val timerCount = addTimersSheet(workbook, project, phase, bpmnName)
       workbook.write(response.getOutputStream)
       response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.log(getClass.getName, "doGet", s"EXIT-OK ($taskCount tasks, $vtCount variables/timers)", request)
+      BWLogger.log(getClass.getName, "doGet",
+        s"EXIT-OK ($taskCount tasks, $variableCount variables, $timerCount timers)", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, "doGet", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)

643f47a8ad4df5aa2d8a7d5163209bf535db2ce7
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-04-13 07:38:06 +0530

    Periodic updates

--- src/main/scala/com/buildwhiz/dot/GetDocumentsSummary.scala
+++ src/main/scala/com/buildwhiz/dot/GetDocumentsSummary.scala
@@ -4,38 +4,42 @@ import com.buildwhiz.infra.DynDoc
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
-import org.bson.types.ObjectId
 import org.bson.Document
 
 class GetDocumentsSummary extends HttpServlet with HttpUtils {
 
-  private def getDocuments: Seq[Document] = {
+  private def getDocuments(user: DynDoc): Seq[Document] = {
+    val count = user.first_name[String] match {
+      case "Tester2" => 5
+      case "Tester3" => 50
+      case _ => 1
+    }
     val docs: Seq[Document] = Seq(
       Map("_id" -> "a123456789012", "name" -> "Foundation Drawings", "phase" -> "School Foundation",
         "labels" -> Map("system" -> Seq("architecture", "design"), "user" -> Seq("environment", "water")),
-        "author" -> "Winston Chang", "type" -> "pdf", "date" -> "2017-11-12 13:15 PT",
-        "downloadUrl" -> ""),
-      Map("_id" -> "b123456789012", "name" -> "Roofing Special Materials", "phase" -> "School Roof",
+        "author" -> "Winston Chang", "type" -> "pdf", "date" -> "2017-11-12 13:15 PT"),
+      Map("_id" -> "b123456789012", "name" -> "Front Garden Landscape", "phase" -> "School Landscaping",
+        "labels" -> Map("system" -> Seq("design"), "user" -> Seq("landscape", "garden")),
+        "author" -> "Bhoomi Chugh", "type" -> "pdf", "date" -> "2017-07-09 09:10 PT"),
+      Map("_id" -> "c123456789012", "name" -> "Roofing Special Materials", "phase" -> "School Roofing",
         "labels" -> Map("system" -> Seq("construction", "materials"), "user" -> Seq("plastics", "environment", "rain")),
-        "author" -> "Kelly Heath", "type" -> "pdf", "date" -> "2017-11-22 10:31 PT",
-        "downloadUrl" -> "")
+        "author" -> "Kelly Heath", "type" -> "pdf", "date" -> "2017-11-22 10:31 PT")
     )
-    docs
+    (1 to count).map(_ => docs).reduce((a, b) => a ++ b)
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    val parameters = getParameterMap(request)
+    //val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, "doGet()", s"ENTRY", request)
     val writer = response.getWriter
     try {
-      val personOid = getUser(request).y._id[ObjectId]
+      val user: DynDoc = getUser(request)
 
-      //val actionOrder = Map("prerequisite" -> 1, "main" -> 2, "review" -> 3)
-      val allDocuments = getDocuments()
+      val allDocuments = getDocuments(user)
       writer.print(allDocuments.map(document => bson2json(document)).mkString("[", ", ", "]"))
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.log(getClass.getName, "doGet()", s"EXIT-OK", request)
+      BWLogger.log(getClass.getName, "doGet()", s"EXIT-OK (${allDocuments.length})", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, "doGet()", s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)

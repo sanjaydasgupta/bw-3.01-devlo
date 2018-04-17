08a04f611239a84a5a8351e6d666e93925611501
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-04-17 06:06:50 +0530

    Transitioning to real document list

--- src/main/scala/com/buildwhiz/dot/GetDocumentsSummary.scala
+++ src/main/scala/com/buildwhiz/dot/GetDocumentsSummary.scala
@@ -1,17 +1,20 @@
 package com.buildwhiz.dot
 
-import com.buildwhiz.infra.DynDoc
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
-import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import com.buildwhiz.utils.{BWLogger, DateTimeUtils, HttpUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.Document
+import org.bson.types.ObjectId
 
-class GetDocumentsSummary extends HttpServlet with HttpUtils {
+import scala.collection.mutable
 
-  private def getDocuments(user: DynDoc): Seq[Document] = {
+class GetDocumentsSummary extends HttpServlet with HttpUtils with DateTimeUtils {
+
+  private def getDocuments2(user: DynDoc): Seq[Document] = {
     val count = user.first_name[String] match {
       case "Tester2" => 5
-      case "Tester3" => 50
       case _ => 1
     }
     val docs: Seq[Document] = Seq(
@@ -28,6 +31,38 @@ class GetDocumentsSummary extends HttpServlet with HttpUtils {
     (1 to count).map(_ => docs).reduce((a, b) => a ++ b)
   }
 
+  private def getDocuments(user: DynDoc): Seq[Document] = {
+    val userLabels: Seq[DynDoc] = user.labels[Many[Document]]
+    val docOid2labels = mutable.Map.empty[ObjectId, mutable.Buffer[String]]
+    for (label <- userLabels) {
+      val labelName = label.name[String]
+      val docOids: Seq[ObjectId] = label.document_ids[Many[ObjectId]]
+      for (docOid <- docOids) {
+        val docLabels: mutable.Buffer[String] = docOid2labels.getOrElse(docOid, mutable.Buffer.empty[String])
+        docLabels.append(labelName)
+        docOid2labels.put(docOid, docLabels)
+      }
+    }
+    val docs: Seq[DynDoc] = BWMongoDB3.document_master.
+      find(Map("category" -> Map("$exists" -> true), "category" -> Map("$ne" -> "SYSTEM"),
+        "subcategory" -> Map("$exists" -> true), "versions.0.file_name" -> Map("$exists" -> true)))
+    val docProperties: Seq[Document] = docs.map(d => {
+      val lastVersion: DynDoc = d.versions[Many[Document]].head
+      val fileType = lastVersion.file_name[String].split("\\.").last
+      val date = dateTimeString(lastVersion.timestamp[Long], Some(user.tz[String]))
+      val authorOid = lastVersion.author_person_id[ObjectId]
+      val author: DynDoc = BWMongoDB3.persons.find(Map("_id" -> authorOid)).head
+      val authorName = s"${author.first_name[String]} ${author.last_name[String]}"
+      val systemLabels = Seq(d.category[String], d.subcategory[String])
+      val userLabels = docOid2labels.getOrElse(d._id[ObjectId], Seq.empty[String])
+      val prop: Document = Map("name" -> d.description[String], "_id" -> d._id[ObjectId].toString, "phase" -> "???",
+        "labels" -> Map("system" -> systemLabels, "user" -> userLabels), "type" -> fileType,
+        "author" -> authorName, "date" -> date)
+      prop
+    })
+    docProperties
+  }
+
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     //val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, "doGet()", s"ENTRY", request)
@@ -35,7 +70,7 @@ class GetDocumentsSummary extends HttpServlet with HttpUtils {
     try {
       val user: DynDoc = getUser(request)
 
-      val allDocuments = getDocuments(user)
+      val allDocuments = if (user.first_name[String] == "Tester3") getDocuments(user) else getDocuments2(user)
       writer.print(allDocuments.map(document => bson2json(document)).mkString("[", ", ", "]"))
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)

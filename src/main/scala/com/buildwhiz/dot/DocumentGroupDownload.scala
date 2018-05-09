5d9a4da943cdc99b7bca6c8f69c87b365d0c6ec9
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2018-05-09 12:52:19 +0530

    Periodic update

--- src/main/scala/com/buildwhiz/dot/DocumentGroupDownload.scala
+++ src/main/scala/com/buildwhiz/dot/DocumentGroupDownload.scala
@@ -13,9 +13,21 @@ import org.bson.Document
 
 class DocumentGroupDownload extends HttpServlet with HttpUtils {
 
-  private def zipMultipleStreams(inputs: Seq[(String, InputStream)], outStream: OutputStream) = {
+  private def documentNameAndStream(documentId: String, projectId: String): (String, InputStream) = {
+    val documentOid = new ObjectId(documentId)
+    val documentRecord: DynDoc = BWMongoDB3.document_master.find(Map("_id" -> documentOid)).head
+    val version: DynDoc = documentRecord.versions[Many[Document]].head
+    val fileName = version.file_name[String]
+    val timestamp = version.timestamp[Long]
+    val amazonS3Key = f"$projectId-$documentOid-$timestamp%x"
+    val inputStream: InputStream = AmazonS3.getObject(amazonS3Key).getObjectContent
+    (fileName, inputStream)
+  }
+
+  private def zipMultipleDocuments(documentAndProjectIds: Seq[(String, String)], outStream: OutputStream) = {
     val zipOutputStream = new ZipOutputStream(outStream)
-    for ((fileName, inputStream) <- inputs) {
+    for (docId <- documentAndProjectIds) {
+      val (fileName, inputStream) = documentNameAndStream(docId._1, docId._2)
       val zipEntry = new ZipEntry(fileName)
       zipOutputStream.putNextEntry(zipEntry)
       val bytes = Array.ofDim[Byte](1024)
@@ -29,37 +41,25 @@ class DocumentGroupDownload extends HttpServlet with HttpUtils {
     //outStream.close()
   }
 
-  val contentTypes = Map("MS-Excel" -> "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    "MS-PPT" -> "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-    "MS-Word" -> "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    "PDF" -> "application/pdf", "Text" -> "text/plain", "XML" -> "application/xml")
-
-  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    val parameters = getParameterMap(request)
-    BWLogger.log(getClass.getName, "doGet", "ENTRY", request)
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    //val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
     try {
-      val timestamp = parameters("timestamp").toLong
-      val documentOid = new ObjectId(parameters("document_master_id"))
-      val docRec: DynDoc = BWMongoDB3.document_master.find(Map("_id" -> documentOid)).head
-      val version: DynDoc = docRec.versions[Many[Document]].find(_.timestamp[Long] == timestamp).get
-      val fileName = version.file_name[String]
-      val amazonS3Key = f"$project430ForestOid-$documentOid-$timestamp%x"
-      val inputStream: InputStream = AmazonS3.getObject(amazonS3Key).getObjectContent
+      val postData = getStreamData(request)
+      val parameters: DynDoc = if (postData.nonEmpty) Document.parse(postData) else new Document()
+      val ids: Seq[DynDoc] = if (parameters.has("ids"))
+        parameters.ids[Many[Document]]
+      else
+        throw new IllegalArgumentException(s"parameter 'ids' not found")
+      val docAndProjTuples = ids.map(id => (id.document_id[String], id.project_id[String]))
       val outputStream = response.getOutputStream
-      val buffer = new Array[Byte](4096)
-      var len = inputStream.read(buffer)
-      while (len > 0) {
-        outputStream.write(buffer, 0, len)
-        len = inputStream.read(buffer)
-      }
-      val document: DynDoc = BWMongoDB3.document_master.find(Map("_id" -> documentOid)).head
-      if (document.has("content") && contentTypes.contains(document.content[String]))
-        response.setContentType(contentTypes(document.content[String]))
+      zipMultipleDocuments(docAndProjTuples, outputStream)
+      response.setContentType("application/zip")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.log(getClass.getName, "doGet", "EXIT-OK", request)
+      BWLogger.log(getClass.getName, "doPost", "EXIT-OK", request)
     } catch {
       case t: Throwable =>
-        BWLogger.log(getClass.getName, "doGet", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
         //t.printStackTrace()
         throw t
     }

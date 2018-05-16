959b27f1098ee9de4e4614c53eec2e9a0dd6397b
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-05-16 10:35:04 +0530

    Added GET interface

--- src/main/scala/com/buildwhiz/dot/DocumentGroupDownload.scala
+++ src/main/scala/com/buildwhiz/dot/DocumentGroupDownload.scala
@@ -24,17 +24,22 @@ class DocumentGroupDownload extends HttpServlet with HttpUtils {
     (fileName, inputStream)
   }
 
-  private def zipMultipleDocuments(documentAndProjectIds: Seq[(String, String)], outStream: OutputStream): Unit = {
+  private def zipMultipleDocuments(documentAndProjectIds: Seq[(String, String)], outStream: OutputStream,
+        request: HttpServletRequest): Unit = {
     val zipOutputStream = new ZipOutputStream(outStream)
-    for (docId <- documentAndProjectIds) {
-      val (fileName, inputStream) = documentNameAndStream(docId._1, docId._2)
+    for (idPair <- documentAndProjectIds) {
+      val (fileName, inputStream) = documentNameAndStream(idPair._1, idPair._2)
       val zipEntry = new ZipEntry(fileName)
       zipOutputStream.putNextEntry(zipEntry)
       val bytes = Array.ofDim[Byte](1024)
-      var length = 0
-      while ({length = inputStream.read(bytes); length} >= 0) {
+      var length, totalLength = 0
+      while ({length = inputStream.read(bytes); length} > 0) {
         zipOutputStream.write(bytes, 0, length)
+        totalLength += length
       }
+      zipOutputStream.closeEntry()
+      BWLogger.log(getClass.getName, "zipMultipleDocuments",
+        s"ZipOutputStream: added $totalLength bytes to '$fileName'", request)
       inputStream.close()
     }
     zipOutputStream.close()
@@ -51,15 +56,37 @@ class DocumentGroupDownload extends HttpServlet with HttpUtils {
         parameters.ids[Many[Document]]
       else
         throw new IllegalArgumentException(s"parameter 'ids' not found")
+      BWLogger.log(getClass.getName, "doPost",
+        s"POST parameters: ${parameters.asDoc.toJson}", request)
       val docAndProjTuples = ids.map(id => (id.document_id[String], id.project_id[String]))
       val outputStream = response.getOutputStream
-      zipMultipleDocuments(docAndProjTuples, outputStream)
+      zipMultipleDocuments(docAndProjTuples, outputStream, request)
       response.setContentType("application/zip")
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.log(getClass.getName, "doPost", "EXIT-OK", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, "doGet", "ENTRY", request)
+    try {
+      val documentIds = parameters("document_ids").split(",").map(_.trim)
+      val projectIds = parameters("project_ids").split(",").map(_.trim)
+      val docAndProjTuples = documentIds.zip(projectIds).map(id => (id._1, id._2))
+      val outputStream = response.getOutputStream
+      zipMultipleDocuments(docAndProjTuples, outputStream, request)
+      response.setContentType("application/zip")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, "doGet", "EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "doGet", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
         //t.printStackTrace()
         throw t
     }

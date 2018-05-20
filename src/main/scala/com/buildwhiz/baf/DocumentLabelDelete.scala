b916cb0d19f2c1dfe62c44a706e89445fc3f076f
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-05-20 22:34:04 +0530

    Periodic updates

--- src/main/scala/com/buildwhiz/baf/DocumentLabelDelete.scala
+++ src/main/scala/com/buildwhiz/baf/DocumentLabelDelete.scala
@@ -19,16 +19,19 @@ class DocumentLabelDelete extends HttpServlet with HttpUtils {
       val labelName: String = parameters("label_name")
       val user: DynDoc = getUser(request)
       val freshUserRecord: DynDoc = BWMongoDB3.persons.find(Map("_id" -> user._id[ObjectId])).head
-      val labels: Seq[DynDoc] = if (freshUserRecord.has("labels"))
+      val userLabels: Seq[DynDoc] = if (freshUserRecord.has("labels"))
         freshUserRecord.labels[Many[Document]] else Seq.empty[Document]
-      if (!labels.exists(_.name[String] == labelName))
+      if (!userLabels.exists(_.name[String] == labelName))
         throw new IllegalArgumentException(s"label '$labelName' not found")
       val updateResult = BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId]),
         Map("$pull" -> Map("labels" -> Map("name" -> labelName))))
       if (updateResult.getModifiedCount == 0)
         throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+      BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId],
+        "document_filter_labels" -> Map("$exists" -> true)),
+        Map("$pull" -> Map("document_filter_labels" -> labelName)))
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.audit(getClass.getName, "doPost", s"Deleted document-label $labelName", request)
+      BWLogger.audit(getClass.getName, "doPost", s"Deleted document-label '$labelName'", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)

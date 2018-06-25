09c78e4c3fc4ccdb82952f8f699b43a99c04a682
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-06-25 13:31:57 +0530

    Fixed error in closing RFI

--- src/main/scala/com/buildwhiz/baf/RFIClose.scala
+++ src/main/scala/com/buildwhiz/baf/RFIClose.scala
@@ -35,7 +35,8 @@ class RFIClose extends HttpServlet with HttpUtils with MailUtils with DateTimeUt
       if (updateResult.getModifiedCount == 0)
         throw new IllegalArgumentException(s"MongoDB error: $updateResult")
       val rfiRecord: DynDoc = BWMongoDB3.rfi_messages.find(Map("_id" -> rfiOid)).head
-      val members: Seq[ObjectId] = rfiRecord.members[Many[ObjectId]]
+      val members: Seq[ObjectId] = if (rfiRecord.has("members"))
+        rfiRecord.members[Many[ObjectId]] else Seq.empty[ObjectId]
       val originatorOid = getUser(request).get("_id").asInstanceOf[ObjectId]
       val url = request.getRequestURL.toString.split("/").reverse.drop(2).reverse.mkString("/") +
         s"/#/rfi?rfi_id=$rfiOid"

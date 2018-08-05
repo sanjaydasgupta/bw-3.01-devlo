dface392a50bebafec4a300ffc4abdff6bd07401
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-08-05 08:20:56 +0530

    Fixing error in reading members list

--- src/main/scala/com/buildwhiz/dot/RFIMessageSubmit.scala
+++ src/main/scala/com/buildwhiz/dot/RFIMessageSubmit.scala
@@ -52,7 +52,10 @@ class RFIMessageSubmit extends HttpServlet with HttpUtils with MailUtils with Da
       BWMongoDB3.rfi_messages.updateOne(Map("_id" -> rfiOid), Map("$push" -> Map("messages" -> message),
         "$set" -> Map("status" -> "active")))
       val rfiMessage: DynDoc = BWMongoDB3.rfi_messages.find(Map("_id" -> rfiOid)).head
-      val members: Seq[ObjectId] = rfiMessage.members[Many[ObjectId]]
+      val members: Seq[ObjectId] = if (rfiMessage.has("members"))
+        rfiMessage.members[Many[ObjectId]]
+      else
+        Seq.empty[ObjectId]
       val subject: String = rfiMessage.subject[String]
       val url = request.getRequestURL.toString.split("/").reverse.drop(2).reverse.mkString("/") +
         s"/#/rfi?rfi_id=$rfiOid"

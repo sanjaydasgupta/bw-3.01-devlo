5785599e112dcbf03ffd69e48634caae5a1c6fb4
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-08-25 17:44:38 +0530

    Initial version

---
+++ src/main/scala/com/buildwhiz/jelly/NoOpDelegate.scala
@@ -0,0 +1,13 @@
+package com.buildwhiz.jelly
+
+import com.buildwhiz.utils.BWLogger
+import org.camunda.bpm.engine.delegate.{DelegateExecution, JavaDelegate}
+
+class NoOpDelegate extends JavaDelegate {
+
+  def execute(de: DelegateExecution): Unit = {
+    BWLogger.log(getClass.getName, "execute()", "ENTRY", de)
+    BWLogger.log(getClass.getName, "execute()", "EXIT-OK", de)
+  }
+
+}

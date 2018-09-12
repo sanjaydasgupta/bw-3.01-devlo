aecac05fdd3a823204cf2f4ef1dfa51edb55c0d6
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-09-12 20:51:27 +0530

    Adjustment for label logic

--- src/main/scala/com/buildwhiz/baf/DocumentLabelsFetch.scala
+++ src/main/scala/com/buildwhiz/baf/DocumentLabelsFetch.scala
@@ -17,9 +17,13 @@ class DocumentLabelsFetch extends HttpServlet with HttpUtils {
     try {
       val user: DynDoc = getUser(request)
       val person: DynDoc = BWMongoDB3.persons.find(Map("_id" -> user._id[ObjectId])).head
-      val labelObjects: Seq[DynDoc] = if (person.has("labels")) person.labels[Many[Document]] else Seq.empty[Document]
-      val sortedLabelObjects: Seq[DynDoc] = labelObjects.sortBy(_.name[String])
-      response.getWriter.println(sortedLabelObjects.map(d => bson2json(d.asDoc)).mkString("[", ", ", "]"))
+      val labelObjects: Seq[Document] = if (person.has("labels")) person.labels[Many[Document]] else Seq.empty[Document]
+      for (labelObject <- labelObjects) {
+        if (!labelObject.has("logic"))
+          labelObject.put("logic", "")
+      }
+      val sortedLabelObjects: Seq[Document] = labelObjects.sortBy(_.get("name").asInstanceOf[String])
+      response.getWriter.println(sortedLabelObjects.map(d => bson2json(d)).mkString("[", ", ", "]"))
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.log(getClass.getName, "doGet", s"EXIT-OK (${labelObjects.length} labels)", request)

c531d6737b02165377eac10b81a84b86368a7345
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-09-14 14:01:16 +0530

    Added validation for label name syntax

--- src/main/scala/com/buildwhiz/baf/DocumentLabelAdd.scala
+++ src/main/scala/com/buildwhiz/baf/DocumentLabelAdd.scala
@@ -1,7 +1,6 @@
 package com.buildwhiz.baf
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
-
 import com.buildwhiz.infra.DynDoc
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.BWMongoDB3
@@ -16,24 +15,39 @@ class DocumentLabelAdd extends HttpServlet with HttpUtils {
     BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
     val parameters = getParameterMap(request)
     try {
-      val newLabelName: String = parameters("label_name")
+      val labelName: String = parameters("label_name")
+      if (!DocumentLabelAdd.isValidLabel(labelName))
+        throw new IllegalArgumentException(s"invalid label name '$labelName'")
       val user: DynDoc = getUser(request)
       val freshUserRecord: DynDoc = BWMongoDB3.persons.find(Map("_id" -> user._id[ObjectId])).head
       val labels: Seq[DynDoc] = if (freshUserRecord.has("labels"))
         freshUserRecord.labels[Many[Document]] else Seq.empty[Document]
-      if (labels.exists(_.name[String] == newLabelName))
-        throw new IllegalArgumentException(s"label '$newLabelName' already exists")
+      if (labels.exists(_.name[String] == labelName))
+        throw new IllegalArgumentException(s"label '$labelName' already exists")
       val updateResult = BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId]),
-        Map("$addToSet" -> Map("labels" -> Map("name" -> newLabelName, "document_ids" -> Seq.empty[Many[ObjectId]]))))
+        Map("$addToSet" -> Map("labels" -> Map("name" -> labelName, "document_ids" -> Seq.empty[Many[ObjectId]]))))
       if (updateResult.getModifiedCount == 0)
         throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.audit(getClass.getName, "doPost", s"Added document-label $newLabelName", request)
+      BWLogger.audit(getClass.getName, "doPost", s"Added document-label $labelName", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
         throw t
     }
+  }
+
+}
+
+object DocumentLabelAdd {
+
+  private val validLabelRe = "(?i)[A-Z0-9]+(?:[A-Z0-9-]*[A-Z0-9])?".r
+  private val invalidLabelRe = "(?i)OR|AND".r
+
+  def isValidLabel(label: String): Boolean = label match {
+      case invalidLabelRe() => false
+      case validLabelRe() => true
+      case _ => false
   }
 
 }

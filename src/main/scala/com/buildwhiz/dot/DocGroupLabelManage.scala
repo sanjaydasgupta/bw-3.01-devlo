583e8d7cc267c89bd8351e320841bdc857323686
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-09-16 22:48:46 +0530

    Corrected errors in group label manipulation

--- src/main/scala/com/buildwhiz/dot/DocGroupLabelManage.scala
+++ src/main/scala/com/buildwhiz/dot/DocGroupLabelManage.scala
@@ -11,7 +11,7 @@ import org.bson.types.ObjectId
 class DocGroupLabelManage extends HttpServlet with HttpUtils {
 
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
+    BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
     //val parameters = getParameterMap(request)
     try {
       val user: DynDoc = getUser(request)
@@ -23,31 +23,33 @@ class DocGroupLabelManage extends HttpServlet with HttpUtils {
       val docOids: Seq[ObjectId] = labelData.docIds[Many[String]].map(id => new ObjectId(id))
       val labels: Seq[String] = labelData.labels[Many[String]]
       val op = labelData.op[String]
-      //val labelName = parameters("label_name")
       val labelIndices = labels.map(label => usersLabelRecords.indexWhere(_.name[String] == label))
-      val unknownLabels = labelIndices.zip(labels).filter(_._1 == -1).map(_._2).mkString(", ")
-      if (unknownLabels.nonEmpty)
-        throw new IllegalArgumentException(s"labels '$unknownLabels' not found")
-      //val documentOid = new ObjectId(parameters("document_id"))
       val (modifiedCount, opName) = op match {
         case "add" =>
           val updateResults = docOids.flatMap(docOid =>
-            labelIndices.map(labelIndex => BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId]),
-            Map("$addToSet" -> Map(s"labels.$labelIndex.document_ids" -> docOid)))))
+            labelIndices.zip(labels).map(idx =>
+              if (idx._1 == -1)
+                BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId]),
+                  Map("$addToSet" -> Map("labels" -> Map("name" -> idx._2, "document_ids" -> Seq(docOid)))))
+              else
+                BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId]),
+                  Map("$addToSet" -> Map(s"labels.${idx._1}.document_ids" -> docOid)))
+            )
+          )
           (updateResults.map(_.getModifiedCount).sum, "Added")
         case "remove" =>
           val updateResults = docOids.flatMap(docOid =>
-            labelIndices.map(labelIndex => BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId]),
+            labelIndices.filter(_ != -1).map(labelIndex => BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId]),
             Map("$pull" -> Map(s"labels.$labelIndex.document_ids" -> docOid)))))
           (updateResults.map(_.getModifiedCount).sum, "Removed")
         case unknown => throw new IllegalArgumentException(s"operation '$unknown' not recognized")
       }
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.audit(getClass.getName, "doPost",
+      BWLogger.audit(getClass.getName, request.getMethod,
         s"""$opName document '${docOids.mkString(",")}' to label '${labels.mkString(",")}' [$modifiedCount Ok]""", request)
     } catch {
       case t: Throwable =>
-        BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
         throw t
     }
   }

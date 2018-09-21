41a1835b54b10dbc1b44fea51301edc0d7f22f9b
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-09-21 21:33:00 +0530

    Added parser and interpreter for logical label expressions

--- src/main/scala/com/buildwhiz/baf/DocumentUserLabelLogicSet.scala
+++ src/main/scala/com/buildwhiz/baf/DocumentUserLabelLogicSet.scala
@@ -8,6 +8,8 @@ import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.Document
 import org.bson.types.ObjectId
 
+import scala.util.parsing.combinator.RegexParsers
+
 class DocumentUserLabelLogicSet extends HttpServlet with HttpUtils {
 
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
@@ -16,26 +18,52 @@ class DocumentUserLabelLogicSet extends HttpServlet with HttpUtils {
     try {
       val labelName: String = parameters("label_name")
       val logic: String = parameters("logic")
+      val logicOk = DocumentUserLabelLogicSet.parse(logic).successful
+      if (!logicOk)
+        throw new IllegalArgumentException(s"illegal logic expression '$logic'")
       val user: DynDoc = getUser(request)
       val freshUserRecord: DynDoc = BWMongoDB3.persons.find(Map("_id" -> user._id[ObjectId])).head
       val userLabels: Seq[DynDoc] = if (freshUserRecord.has("labels"))
         freshUserRecord.labels[Many[Document]] else Seq.empty[Document]
-      if (!userLabels.exists(_.name[String] == labelName))
-        throw new IllegalArgumentException(s"label '$labelName' not found")
       val idx = userLabels.indexWhere(_.name[String] == labelName)
+      if (idx == -1)
+        throw new IllegalArgumentException(s"label '$labelName' not found")
       val updateResult = BWMongoDB3.persons.updateOne(Map("_id" -> user._id[ObjectId]),
         Map("$set" -> Map(s"labels.$idx.logic" -> logic)))
       if (updateResult.getMatchedCount == 0)
         throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
-      //
-      //  ToDo Evaluate applicable document list
-      //
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.audit(getClass.getName, request.getMethod, s"Added logic '$logic' to label '$labelName'", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
         throw t
+    }
+  }
+
+}
+
+object DocumentUserLabelLogicSet extends RegexParsers {
+
+  type TestSet = Set[String] => Boolean
+
+  def label: Parser[String] = "(?i)[A-Z](?:[A-Z0-9-]*[A-Z0-9])?".r
+
+  def or: Parser[TestSet] = (label <~ "(?i)OR".r) ~ expression ^^
+    { case lbl ~ expr => set => set.contains(lbl) || expr(set) }
+
+  def and: Parser[TestSet] = (label <~ "(?i)AND".r) ~ expression ^^
+    { case lbl ~ expr => set => set.contains(lbl) && expr(set) }
+
+  def expression: Parser[TestSet] = and | or | "(" ~> expression <~ ")" |
+    label ^^ { lbl => set => set.contains(lbl) }
+
+  def parse(str: String): ParseResult[TestSet] = parseAll(expression, str)
+
+  def eval(expr: String, set: Set[String]): Boolean = {
+    parse(expr) match {
+      case Success(test, _) => test(set)
+      case NoSuccess(_, _) => false
     }
   }
 

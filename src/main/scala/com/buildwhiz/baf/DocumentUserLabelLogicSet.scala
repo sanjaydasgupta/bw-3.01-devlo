c6226befee492f6cd34adb8725715d466043f5b2
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-09-24 11:24:19 +0530

    Incorporated PackratParsers and tested

--- src/main/scala/com/buildwhiz/baf/DocumentUserLabelLogicSet.scala
+++ src/main/scala/com/buildwhiz/baf/DocumentUserLabelLogicSet.scala
@@ -8,7 +8,7 @@ import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.Document
 import org.bson.types.ObjectId
 
-import scala.util.parsing.combinator.RegexParsers
+import scala.util.parsing.combinator.{PackratParsers, RegexParsers}
 
 class DocumentUserLabelLogicSet extends HttpServlet with HttpUtils {
 
@@ -18,9 +18,9 @@ class DocumentUserLabelLogicSet extends HttpServlet with HttpUtils {
     try {
       val labelName: String = parameters("label_name")
       val logic: String = parameters("logic")
-      val logicOk = DocumentUserLabelLogicSet.parse(logic).successful
-      if (!logicOk)
-        throw new IllegalArgumentException(s"illegal logic expression '$logic'")
+      val parseResult = DocumentUserLabelLogicSet.parse(logic)
+      if (!parseResult.successful)
+        throw new IllegalArgumentException(s"bad logic at '${parseResult.next.source}'")
       val user: DynDoc = getUser(request)
       val freshUserRecord: DynDoc = BWMongoDB3.persons.find(Map("_id" -> user._id[ObjectId])).head
       val userLabels: Seq[DynDoc] = if (freshUserRecord.has("labels"))
@@ -43,26 +43,30 @@ class DocumentUserLabelLogicSet extends HttpServlet with HttpUtils {
 
 }
 
-object DocumentUserLabelLogicSet extends RegexParsers {
+object DocumentUserLabelLogicSet extends RegexParsers with PackratParsers {
 
   type TestSet = Set[String] => Boolean
 
-  def label: Parser[TestSet] = "(?i)[A-Z](?:[A-Z0-9-]*[A-Z0-9])?".r ^^
-    { lbl => set => set.contains(lbl) }
+  val AND: Parser[String] = "(?i)AND(?![A-Z0-9-])".r
+  val OR: Parser[String] = "(?i)OR(?![A-Z0-9-])".r
+  val NOT: Parser[String] = "(?i)NOT(?![A-Z0-9-])".r
 
-  def or: Parser[TestSet] = (label <~ "(?i)OR".r) ~ expression ^^
-    { case lbl ~ expr => set => lbl(set) || expr(set) }
+  val LABEL: Parser[TestSet] = guard(not(AND | OR | NOT)) ~> "(?i)[A-Z](?:[A-Z0-9-]*[A-Z0-9])?".r ^^
+    { lbl => (set: Set[String]) => set.contains(lbl) }
 
-  def and: Parser[TestSet] = (label <~ "(?i)AND".r) ~ expression ^^
-    { case lbl ~ expr => set => lbl(set) && expr(set) }
+  val or: PackratParser[TestSet] = (expression <~ OR) ~ expression ^^
+    { case expr1 ~ expr2 => (set: Set[String]) => expr1(set) || expr2(set) }
+  val and: PackratParser[TestSet] = (expression <~ AND) ~ expression ^^
+    { case expr1 ~ expr2 => (set: Set[String]) => expr1(set) && expr2(set) }
+  val not: PackratParser[TestSet] = NOT ~> expression ^^ { expr => (set: Set[String]) => !expr(set) }
 
-  def expression: Parser[TestSet] = and | or | "(" ~> expression <~ ")" | label
+  lazy val expression: PackratParser[TestSet] = not | and | or | "(" ~> expression <~ ")" | LABEL
 
   def parse(str: String): ParseResult[TestSet] = parseAll(expression, str)
 
-  def eval(expr: String, set: Set[String]): Boolean = {
-    parse(expr) match {
-      case Success(test, _) => test(set)
+  def eval(stringToParse: String, labels: Set[String]): Boolean = {
+    parse(stringToParse) match {
+      case Success(expr, _) => expr(labels)
       case NoSuccess(_, _) => false
     }
   }

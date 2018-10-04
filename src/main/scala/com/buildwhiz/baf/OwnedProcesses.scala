dd91c2c2db107d12e331c8331062d9ff20deeb47
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-10-04 17:59:26 +0530

    Added start-time and end-time to output

--- src/main/scala/com/buildwhiz/baf/OwnedProcesses.scala
+++ src/main/scala/com/buildwhiz/baf/OwnedProcesses.scala
@@ -1,21 +1,20 @@
 package com.buildwhiz.baf
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
-
 import com.buildwhiz.infra.DynDoc
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.BWMongoDB3
-import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import com.buildwhiz.utils.{BWLogger, DateTimeUtils, HttpUtils}
 import org.bson.types.ObjectId
 import org.bson.Document
 
 import scala.collection.mutable
 
-class OwnedProcesses extends HttpServlet with HttpUtils {
+class OwnedProcesses extends HttpServlet with HttpUtils with DateTimeUtils {
 
-  private def createProcesses(activities: Seq[DynDoc], variables: Seq[DynDoc], timers: Seq[DynDoc],
-        personOid: ObjectId, phase: DynDoc): Seq[DynDoc] = {
+  private def createProcesses(request: HttpServletRequest, activities: Seq[DynDoc], variables: Seq[DynDoc],
+        timers: Seq[DynDoc], personOid: ObjectId, phase: DynDoc): Seq[DynDoc] = {
     val processes = mutable.Map.empty[String, DynDoc]
     for (activity <- activities) {
       val bpmnName = activity.bpmn_name[String]
@@ -58,14 +57,28 @@ class OwnedProcesses extends HttpServlet with HttpUtils {
           case (_, "running") => "running"
           case _ => b
         })
-      if (myActivities.isEmpty/* && phase.has("bpmn_timestamps")*/) {
+      //if (myActivities.isEmpty/* && phase.has("bpmn_timestamps")*/) {
+        p._2.start_time = "0000-00-00 00:00"
+        p._2.end_time = "0000-00-00 00:00"
         val bpmnTimestamps: Seq[DynDoc] = phase.bpmn_timestamps[Many[Document]]
-        p._2.status = bpmnTimestamps.find(ts => ts.name[String] == bpmnName && ts.has("event")) match {
-          case Some(ts) if ts.event[String] == "start" => "running"
-          case Some(ts) if ts.event[String] == "end" => "ended"
-          case None => "defined"
+        bpmnTimestamps.find(ts => ts.name[String] == bpmnName && ts.has("timestamps")) match {
+          case Some(ts) =>
+            val timestamps: DynDoc = ts.timestamps[Document]
+            val user: DynDoc = getUser(request)
+            if (timestamps.has("start")) {
+              val start = timestamps.start[Long]
+              p._2.start_time = dateTimeString(start, Some(user.tz[String]))
+              p._2.status = "running"
+            }
+            if (timestamps.has("end")) {
+              val end = timestamps.end[Long]
+              p._2.end_time = dateTimeString(end, Some(user.tz[String]))
+              p._2.status = "ended"
+            }
+          case None =>
+            p._2.status = "defined"
         }
-      }
+      //}
       val actions: Seq[DynDoc] = myActivities.flatMap(_.actions[Many[Document]])
       if (actions.exists(action => action.status[String] == "waiting" && action.assignee_person_id[ObjectId] == personOid))
         p._2.display_status = "waiting"
@@ -88,7 +101,7 @@ class OwnedProcesses extends HttpServlet with HttpUtils {
       val activityOids = phase.activity_ids[Many[ObjectId]]
       val activities: Seq[DynDoc] = BWMongoDB3.activities.find(Map("_id" -> Map("$in" -> activityOids)))
       activities.foreach(a => a.is_relevant = a.actions[Many[Document]].exists(_.assignee_person_id[ObjectId] == personOid))
-      val processes = createProcesses(activities, phase.variables[Many[Document]], phase.timers[Many[Document]],
+      val processes = createProcesses(request, activities, phase.variables[Many[Document]], phase.timers[Many[Document]],
           personOid, phase)
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)

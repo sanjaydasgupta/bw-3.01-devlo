e8efcc3f2168d2e3681ba05b1f63b4fab22c4e18
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-10-09 19:21:39 +0530

    Restricted action to phase-manager only

--- src/main/scala/com/buildwhiz/baf/ProjectLaunch.scala
+++ src/main/scala/com/buildwhiz/baf/ProjectLaunch.scala
@@ -15,7 +15,11 @@ class ProjectLaunch extends HttpServlet with HttpUtils {
     val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
     try {
+      val user: DynDoc = getUser(request)
       val projectOid = new ObjectId(parameters("project_id"))
+      val theProject: DynDoc = BWMongoDB3.projects.find(Map("_id" -> projectOid)).head
+      if (user._id[ObjectId] != theProject.admin_person_id[ObjectId])
+        throw new IllegalArgumentException("Not permitted")
       // instantiate BPMN process (ProjectControl)
       //val rts = ProcessEngines.getDefaultProcessEngine.getRuntimeService
       //rts.startProcessInstanceByKey("Infra-Project-Control2", Map("project_id" -> parameters("project_id")))
@@ -25,9 +29,8 @@ class ProjectLaunch extends HttpServlet with HttpUtils {
       response.setContentType("text/plain")
       response.getWriter.print(updateResult)
       response.setStatus(HttpServletResponse.SC_OK)
-      val theProject: DynDoc = BWMongoDB3.projects.find(Map("_id" -> projectOid)).head
-      val projectLog = s"'${theProject.name[String]}' (${theProject._id[ObjectId]})"
-      BWLogger.audit(getClass.getName, "doPost", s"""Launched project $projectLog""", request)
+      val logMessage = s"Launched project '${theProject.name[String]}' (${theProject._id[ObjectId]})"
+      BWLogger.audit(getClass.getName, "doPost", logMessage, request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)

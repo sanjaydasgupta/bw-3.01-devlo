8d7a9c03a7f612d0c3633b16465103e9ce70e6ec
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-10-09 19:25:07 +0530

    Restricted action to phase-manager only

--- src/main/scala/com/buildwhiz/baf/ProjectEnd.scala
+++ src/main/scala/com/buildwhiz/baf/ProjectEnd.scala
@@ -15,17 +15,22 @@ class ProjectEnd extends HttpServlet with HttpUtils with BpmnUtils {
     val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
     try {
+      //
       //ToDo: Need to check the status of child objects (phases, ... actions)
+      //
+      val user: DynDoc = getUser(request)
       val project_id = parameters("project_id")
+      val projectOid = new ObjectId(project_id)
+      val theProject: DynDoc = BWMongoDB3.projects.find(Map("_id" -> projectOid)).head
+      if (user._id[ObjectId] != theProject.admin_person_id[ObjectId])
+        throw new IllegalArgumentException("Not permitted")
       if (hasActiveProcesses(project_id))
         throw new IllegalArgumentException("Project has active children")
-      val projectOid = new ObjectId(project_id)
       val updateResult = BWMongoDB3.projects.updateOne(Map("_id" -> projectOid),
         Map("$set" -> Map("status" -> "ended", "timestamps.end" -> System.currentTimeMillis)))
       if (updateResult.getModifiedCount == 0)
         throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
       response.setStatus(HttpServletResponse.SC_OK)
-      val theProject: DynDoc = BWMongoDB3.projects.find(Map("_id" -> projectOid)).head
       val projectLog = s"'${theProject.name[String]}' (${theProject._id[ObjectId]})"
       BWLogger.audit(getClass.getName, "doPost", s"""Ended project $projectLog""", request)
     } catch {

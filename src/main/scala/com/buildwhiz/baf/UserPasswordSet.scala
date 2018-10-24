4150009a69b3e8ac1402a38f216ebe54be7ca67e
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-10-24 17:12:13 +0530

    Removed need to pass user's id explicitly

--- src/main/scala/com/buildwhiz/baf/UserPasswordSet.scala
+++ src/main/scala/com/buildwhiz/baf/UserPasswordSet.scala
@@ -18,12 +18,9 @@ class UserPasswordSet extends HttpServlet with HttpUtils with CryptoUtils with M
       val user: DynDoc = getUser(request)
       val userOid = user._id[ObjectId]
       val postData: DynDoc = Document.parse(getStreamData(request))
-      val personOid = new ObjectId(postData.person_id[String])
-      if (userOid != personOid)
-        throw new IllegalArgumentException("Password change by 3rd party")
       val oldPassword = postData.old_password[String]
       val newPassword = postData.new_password[String]
-       val updateResult = BWMongoDB3.persons.updateOne(Map("_id" -> personOid, "password" -> md5(oldPassword)),
+       val updateResult = BWMongoDB3.persons.updateOne(Map("_id" -> userOid, "password" -> md5(oldPassword)),
         Map("$set" -> Map(s"password" -> md5(newPassword))))
       if (updateResult.getModifiedCount == 0)
         throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
@@ -33,9 +30,9 @@ class UserPasswordSet extends HttpServlet with HttpUtils with CryptoUtils with M
         s"""Your password on '$instanceName' has just been changed.
           |If you have not changed it, please notify your contact person.
         """.stripMargin
-      sendMail(personOid, s"Password changed on '$instanceName'", mailBody, Some(request))
+      sendMail(userOid, s"Password changed on '$instanceName'", mailBody, Some(request))
       response.setStatus(HttpServletResponse.SC_OK)
-      val thePerson: DynDoc = BWMongoDB3.persons.find(Map("_id" -> personOid)).head
+      val thePerson: DynDoc = BWMongoDB3.persons.find(Map("_id" -> userOid)).head
       val personLog = s"'${thePerson.first_name[String]} ${thePerson.last_name[String]}' (${thePerson._id[ObjectId]})"
       BWLogger.audit(getClass.getName, "doPost", s"""Set password for $personLog""", request)
     } catch {

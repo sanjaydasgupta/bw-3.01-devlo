a3d114c8d2472024429c1deaefd35c4cc257f8b7
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-10-24 22:45:22 +0530

    Initial version

---
+++ src/main/scala/com/buildwhiz/baf/UserPropertyFetch.scala
@@ -0,0 +1,55 @@
+package com.buildwhiz.baf
+
+import com.buildwhiz.infra.BWMongoDB3._
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.bson.Document
+import org.bson.types.ObjectId
+
+class UserPropertyFetch extends HttpServlet with HttpUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, "doGet", "ENTRY", request)
+    try {
+      val user: DynDoc = getUser(request)
+      val freshUserRecord: DynDoc = BWMongoDB3.persons.find(Map("_id" -> user._id[ObjectId])).head
+
+      val phones: Seq[DynDoc] = if (freshUserRecord.has("phones"))
+        freshUserRecord.phones[Many[Document]]
+      else
+        Seq.empty[DynDoc]
+      val phoneWork = phones.find(_.`type`[String] == "work") match {
+        case Some(ph) => ph.phone[String]
+        case None => ""
+      }
+      val phoneMobile = phones.find(_.`type`[String] == "mobile") match {
+        case Some(ph) => ph.phone[String]
+        case None => ""
+      }
+
+      val emails: Seq[DynDoc] = if (freshUserRecord.has("emails"))
+        freshUserRecord.emails[Many[Document]]
+      else
+        Seq.empty[DynDoc]
+      val emailOther = emails.find(_.`type`[String] == "other") match {
+        case Some(em) => em.email[String]
+        case None => ""
+      }
+
+      val resultFields = Map("email_enabled" -> freshUserRecord.email_enabled[Boolean],
+        "phone_work" -> phoneWork, "phone_mobile" -> phoneMobile, "email_other" -> emailOther)
+      val resultPerson = new Document(resultFields)
+      response.getWriter.print(resultPerson.toJson)
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, "doGet", s"EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "doGet", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+}

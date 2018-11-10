4f67328d665c47073f18a047b3655adf8b23a937
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-11-10 19:55:22 +0530

    Added person roles for Project-Management

--- src/main/scala/com/buildwhiz/baf/PhaseAdministratorsFetch.scala
+++ src/main/scala/com/buildwhiz/baf/PhaseAdministratorsFetch.scala
@@ -3,28 +3,29 @@ package com.buildwhiz.baf
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
-import com.buildwhiz.utils.{BWLogger, BpmnUtils}
+import com.buildwhiz.utils.BWLogger
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.Document
 import org.bson.types.ObjectId
 
-class PhaseAdministratorsFetch extends HttpServlet with BpmnUtils {
+class PhaseAdministratorsFetch extends HttpServlet {
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     //
-    // ToDo: Use phase_id to determine possible phase-managers
+    // ToDo: Use parameters to determine possible phase-managers
     //
     BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
     try {
-      val managers: Seq[Document] = Seq("Sourav", "Gouri", "Sanjay").map(fn => {
-        val person: DynDoc = BWMongoDB3.persons.find(Map("first_name" -> fn)).head
-        val name = s"${person.first_name[String]} ${person.last_name[String]}"
-        new Document("_id", person._id[ObjectId].toString).append("name", name)
+      val roleRegex = s".*Project-Management.*"
+      val candidates: Seq[DynDoc] = BWMongoDB3.persons.find(Map("roles" -> Map("$regex" -> roleRegex)))
+      val managers: Seq[Document] = candidates.map(candidate => {
+        val name = s"${candidate.first_name[String]} ${candidate.last_name[String]}"
+        Map("person_id" -> candidate._id[ObjectId].toString, "name" -> name)
       })
       response.getWriter.print(managers.map(_.toJson).mkString("[", ", ", "]"))
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${managers.length})", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)

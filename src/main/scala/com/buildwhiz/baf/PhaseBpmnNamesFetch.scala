f46a981884d9d4f260be9d918a7e9756fac62d55
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2018-12-09 18:34:59 +0530

    Enabled grouping of processes in multiple directories

--- src/main/scala/com/buildwhiz/baf/PhaseBpmnNamesFetch.scala
+++ src/main/scala/com/buildwhiz/baf/PhaseBpmnNamesFetch.scala
@@ -9,13 +9,18 @@ class PhaseBpmnNamesFetch extends HttpServlet with BpmnUtils {
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     BWLogger.log(getClass.getName, "doGet()", s"ENTRY", request)
     try {
-      val prefix = "1.01/Phase-"
+      val prefix = ".+/Phase-"
       val phaseNames: Seq[String] = getDeployedResources.map(_.getName).filter(_.matches(s"$prefix.+\\.bpmn")).
-        map(s => s""""${s.substring(prefix.length, s.length - 5)}"""").distinct
+        map(s => {
+          val idx = s.lastIndexOf("/Phase-")
+          val idx2 = s.lastIndexOf(".bpmn")
+          val pn = s.substring(idx + "/Phase-".length, idx2)
+          s""" "$pn" """.trim
+        }).distinct
       response.getWriter.print(phaseNames.sorted.mkString("[", ", ", "]"))
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.log(getClass.getName, "doGet()", s"EXIT-OK", request)
+      BWLogger.log(getClass.getName, "doGet()", s"EXIT-OK (${phaseNames.length})", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, "doGet()", s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)

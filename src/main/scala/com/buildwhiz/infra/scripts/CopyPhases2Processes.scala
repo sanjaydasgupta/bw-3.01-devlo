ad9f88d69c52b6b3c6ad1dc2ece9fa27d12451a9
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-01-08 13:21:15 +0530

    Script to copy phases to processes

---
+++ src/main/scala/com/buildwhiz/infra/scripts/CopyPhases2Processes.scala
@@ -0,0 +1,11 @@
+package com.buildwhiz.infra.scripts
+
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import BWMongoDB3._
+
+object CopyPhases2Processes extends App {
+  val phases: Seq[DynDoc] = BWMongoDB3.phases.find()
+  for (phase <- phases) {
+    BWMongoDB3.processes.insertOne(phase.asDoc)
+  }
+}

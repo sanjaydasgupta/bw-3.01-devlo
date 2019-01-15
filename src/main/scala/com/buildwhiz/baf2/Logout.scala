2d32c53e57649b9d56e883b6a7a18b650b26cdc4
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-01-15 14:02:20 +0530

    Logout service for baf2 namespace

---
+++ src/main/scala/com/buildwhiz/baf2/Logout.scala
@@ -0,0 +1,29 @@
+package com.buildwhiz.baf2
+
+import com.buildwhiz.infra.DynDoc
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.bson.types.ObjectId
+
+class Logout extends HttpServlet with HttpUtils {
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, "doPost", "ENTRY", request, isLogin = true)
+    try {
+      if (request.getSession.getAttribute("bw-user") != null) {
+        val user: DynDoc = getUser(request)
+        request.getSession.removeAttribute("bw-user")
+        request.getSession.invalidate()
+        val userNameAndId = f"${user.first_name[String]}%s ${user.last_name[String]}%s (${user._id[ObjectId]}%s)"
+        BWLogger.log(getClass.getName, "doPost", s"Logout ($userNameAndId)", request)
+      }
+    } catch {
+      case t: Throwable =>
+        val parameters = getParameterMap(request)
+        BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", parameters.toSeq: _*)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+}

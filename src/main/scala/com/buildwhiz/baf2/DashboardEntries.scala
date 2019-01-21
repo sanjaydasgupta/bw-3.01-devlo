3a77639af499adacb0b5670ed870968c7aa0ea59
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-01-21 19:14:59 +0530

    Updates needed due to refactored database structure

--- src/main/scala/com/buildwhiz/baf2/DashboardEntries.scala
+++ src/main/scala/com/buildwhiz/baf2/DashboardEntries.scala
@@ -12,20 +12,20 @@ import scala.collection.JavaConverters._
 
 class DashboardEntries extends HttpServlet with HttpUtils with DateTimeUtils {
 
-  def compareDashboardEntries(a: Document, b: Document): Boolean = {
+  def compareDashboardEntries(a: DynDoc, b: DynDoc): Boolean = {
     val levels = Map("urgent" -> 3, "important" -> 2, "normal" -> 1)
-    val (lvla, lvlb) = (levels(a.y.status[String]), levels(b.y.status[String]))
-    val (dda, ddb) = (a.y.due_date[String], b.y.due_date[String])
+    val (lvla, lvlb) = (levels(a.status[String]), levels(b.status[String]))
+    val (dda, ddb) = (a.due_date[String], b.due_date[String])
     (lvla > lvlb) || (lvla == lvlb) && (dda < ddb)
   }
 
-  private def dashboardEntries(user: DynDoc): Seq[Document] = {
+  private def dashboardEntries(user: DynDoc): Seq[DynDoc] = {
     val timeZone = user.tz[String]
-    val projectOids: Seq[ObjectId] = user.project_ids[Many[ObjectId]]
-    val projects: Seq[DynDoc] = BWMongoDB3.projects.find(Map("_id" -> Map("$in" -> projectOids)))
+    val userOid = user._id[ObjectId]
 
-    val dashboardProjects: Seq[Document] = projects.
-        filter(_.admin_person_id[ObjectId] == user._id[ObjectId]).map(project => {
+
+    val projects: Seq[DynDoc] = ProjectApi.projectsByUser(userOid)
+    val dashboardProjects: Seq[DynDoc] = projects.map(project => {
       val projectName = project.name[String]
       val projectStatus = project.status[String] match {
         case "defined" | "idle" => "urgent"
@@ -38,14 +38,8 @@ class DashboardEntries extends HttpServlet with HttpUtils with DateTimeUtils {
         "status_date" -> statusDate, "status" -> projectStatus, "due_date" -> "0000-00-00")
     })
 
-    val allPhases: Seq[DynDoc] = projects.flatMap(project => {
-      val phaseOids = project.process_ids[Many[ObjectId]]
-      val phases: Seq[DynDoc] = BWMongoDB3.processes.find(Map("_id" -> Map("$in" -> phaseOids)))
-      phases
-    })
-
-    val dashboardPhases: Seq[Document] = allPhases.
-      filter(_.admin_person_id[ObjectId] == user._id[ObjectId]).map(phase => {
+    val phases: Seq[DynDoc] = projects.flatMap(project => ProjectApi.phasesByUser(userOid, project))
+    val dashboardPhases: Seq[DynDoc] = phases.map(phase => {
       val phaseName = phase.name[String]
       val phaseStatus = phase.status[String] match {
         case "defined" => "urgent"
@@ -58,29 +52,29 @@ class DashboardEntries extends HttpServlet with HttpUtils with DateTimeUtils {
         "status_date" -> statusDate, "status" -> phaseStatus, "due_date" -> "0000-00-00")
     })
 
-    val activityOids: Seq[ObjectId] = allPhases.flatMap(_.activity_ids[Many[ObjectId]])
-    val allActivities: Seq[DynDoc] = BWMongoDB3.activities.find(Map("_id" -> Map("$in" -> activityOids)))
-    val allActions: Seq[DynDoc] = allActivities.flatMap(_.actions[Many[Document]])
+    //val activityOids: Seq[ObjectId] = phases.flatMap(_.activity_ids[Many[ObjectId]])
+    //val allActivities: Seq[DynDoc] = BWMongoDB3.activities.find(Map("_id" -> Map("$in" -> activityOids)))
+    //val allActions: Seq[DynDoc] = allActivities.flatMap(_.actions[Many[Document]])
 
-    val dashboardActions: Seq[Document] = allActions.
-        filter(_.assignee_person_id[ObjectId] == user._id[ObjectId]).map(action => {
-      val actionName = action.name[String]
-      val actionStatus = action.status[String] match {
-        case "waiting" => "urgent"
-        case "ended" => "normal"
-        case _ => "important"
-      }
-      val statusDate = if (action.has("timestamps")) {
-        val statusTime = action.timestamps[Document].values.asScala.map(_.asInstanceOf[Long]).max
-        dateTimeString(statusTime, Some(timeZone))
-      } else {
-        "0000-00-00"
-      }
-      Map("url" -> "tasks", "description" -> s"Task '$actionName' is ${action.status[String]}",
-        "status_date" -> statusDate, "status" -> actionStatus, "due_date" -> "0000-00-00")
-    })
+//    val dashboardActions: Seq[Document] = allActions.
+//        filter(_.assignee_person_id[ObjectId] == user._id[ObjectId]).map(action => {
+//      val actionName = action.name[String]
+//      val actionStatus = action.status[String] match {
+//        case "waiting" => "urgent"
+//        case "ended" => "normal"
+//        case _ => "important"
+//      }
+//      val statusDate = if (action.has("timestamps")) {
+//        val statusTime = action.timestamps[Document].values.asScala.map(_.asInstanceOf[Long]).max
+//        dateTimeString(statusTime, Some(timeZone))
+//      } else {
+//        "0000-00-00"
+//      }
+//      Map("url" -> "tasks", "description" -> s"Task '$actionName' is ${action.status[String]}",
+//        "status_date" -> statusDate, "status" -> actionStatus, "due_date" -> "0000-00-00")
+//    })
 
-    (dashboardProjects ++ dashboardPhases ++ dashboardActions).sortWith(compareDashboardEntries)
+    (dashboardProjects ++ dashboardPhases /*++ dashboardActions*/).sortWith(compareDashboardEntries)
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
@@ -88,9 +82,9 @@ class DashboardEntries extends HttpServlet with HttpUtils with DateTimeUtils {
     try {
       val user: DynDoc = getUser(request)
       val freshUserRecord: DynDoc = BWMongoDB3.persons.find(Map("_id" -> user._id[ObjectId])).head
-      val entries: Seq[Document] = dashboardEntries(freshUserRecord)
+      val entries: Seq[DynDoc] = dashboardEntries(freshUserRecord)
 
-      response.getWriter.println(entries.map(_.toJson).mkString("[", ", ", "]"))
+      response.getWriter.println(entries.map(_.asDoc.toJson).mkString("[", ", ", "]"))
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${entries.length})", request)

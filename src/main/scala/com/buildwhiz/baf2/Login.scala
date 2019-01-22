d15fe066da22d09644bc17af41ab5ff8475692fc
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-01-22 17:38:11 +0530

    Added Documents to the menu items returned

--- src/main/scala/com/buildwhiz/baf2/Login.scala
+++ src/main/scala/com/buildwhiz/baf2/Login.scala
@@ -47,6 +47,7 @@ class Login extends HttpServlet with HttpUtils with CryptoUtils {
 
   private def addMenuItems(person: DynDoc): Unit = {
     val menuItems: Seq[Document] = Seq(
+      Map("name" -> "Documents", "url" -> "/documents", "icon" -> "documents"),
       Map("name" -> "RFIs", "url" -> "/rfi", "icon" -> "rfis"),
       //Map("name" -> "Projects", "url" -> "/projects", "icon" -> "projects"),
       Map("name" -> "Tasks", "url" -> "/tasks", "icon" -> "tasks"),

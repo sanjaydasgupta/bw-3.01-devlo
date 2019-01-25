7d472ab93a3ea2c553f844df9d20de349649b032
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-01-25 12:21:25 +0530

    Feature updates

--- src/main/scala/com/buildwhiz/baf2/ProjectInfo.scala
+++ src/main/scala/com/buildwhiz/baf2/ProjectInfo.scala
@@ -1,9 +1,9 @@
 package com.buildwhiz.baf2
 
-import com.buildwhiz.infra.BWMongoDB3._
-import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import com.buildwhiz.infra.BWMongoDB3._
+import com.buildwhiz.infra.DynDoc._
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.Document
 import org.bson.types.ObjectId
@@ -18,29 +18,38 @@ class ProjectInfo extends HttpServlet with HttpUtils {
         exists(role => role.person_id[ObjectId] == userOid && role.role_name[String] == "Project-Manager")
   }
 
+  private def phaseInformtion(project: DynDoc): Many[Document] = {
+    val phaseOids: Seq[ObjectId] = project.phase_ids[Many[ObjectId]]
+    val phases: Seq[DynDoc] = BWMongoDB3.phases.find(Map("_id" -> Map("$in" -> phaseOids)))
+    val returnValue: Seq[Document] = phases.map(phase => {
+      Map("name" -> phase.name[String], "status" -> phase.status[String],
+          "start_date" -> "???", "end_date" -> "???")
+    })
+    returnValue.asJava
+  }
+
   private def project2json(project: DynDoc, editable: Boolean): String = {
-    val bareGpsLocation: Document = if (project.has("gps_location"))
-      project.gps_location[Document]
-    else
-      Document.parse("""{"latitude": 0.0, "longitude": 0.0}""")
-    val gpsLocation = new Document("editable", editable).append("value", bareGpsLocation)
-    val bareAddress: Document = if (project.has("address"))
-      project.address[Document]
-    else
-      Document.parse("{\"line1\": \"...\", \"line2\": \"...\", \"line3\": \"...\", " +
-        "\"postal_code\": \"...\", \"country\": {\"name\": \"United States\", \"code\": \"US\"}," +
-        "\"state\": {\"name\": \"California\", \"code\": \"CA\"}}")
-    val address = new Document("editable", editable).append("value", bareAddress)
-    val bareSystemLabels: Seq[String] = if (project.has("system_labels"))
+    val bareSystemLabels: Many[String] = if (project.has("system_labels"))
       project.system_labels[Many[String]]
     else
-      Seq.empty[String]
-    val systemLabels = new Document("editable", false).append("value", bareSystemLabels.asJava)
+      Seq.empty[String].asJava
+    val systemLabels = new Document("editable", false).append("value", bareSystemLabels)
     val description = new Document("editable", editable).append("value", project.description[String])
     val status = new Document("editable", false).append("value", project.status[String])
     val name = new Document("editable", editable).append("value", project.name[String])
-    val projectDoc = new Document("name", name).append("description", description).append("gps_location", gpsLocation).
-        append("address", address).append("status", status).append("system_labels", systemLabels)
+    val address: Document = new Document("editable", editable).append("value", project.address[Document])
+    val constructionType: Document = new Document("editable", editable).append("value", project.construction_type[String])
+    val `type`: Document = new Document("editable", editable).append("value", project.`type`[String])
+    val budgetMmUsd: Document = new Document("editable", editable).append("value", project.budget_mm_usd[Double])
+    val constAreaSqFt: Document = new Document("editable", editable).append("value", project.construction_area_sqft[Double])
+    val landAreaAcres: Document = new Document("editable", editable).append("value", project.land_area_acres[Double])
+    val maxBldgHeightFt: Document = new Document("editable", editable).append("value", project.max_building_height_ft[Double])
+    val phaseInfo: Document = new Document("editable", false).append("value", phaseInformtion(project))
+    val projectDoc = new Document("name", name).append("description", description).
+        append("address", address).append("status", status).append("system_labels", systemLabels).
+        append("type", `type`).append("construction_type", constructionType).append("budget_mm_usd", budgetMmUsd).
+        append("construction_area_sqft", constAreaSqFt).append("land_area_acres", landAreaAcres).
+        append("max_building_height_ft", maxBldgHeightFt).append("phase_info", phaseInfo)
     projectDoc.toJson
   }
 

84f9bdf47274590b5f69a61debb3ddf55f23916e
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-01-26 17:17:26 +0530

    Periodic update

--- src/main/scala/com/buildwhiz/baf2/ProjectApi.scala
+++ src/main/scala/com/buildwhiz/baf2/ProjectApi.scala
@@ -3,110 +3,15 @@ package com.buildwhiz.baf2
 import java.net.URI
 
 import com.amazonaws.services.s3.model.S3ObjectSummary
-import com.buildwhiz.api.RestUtils
-import com.buildwhiz.baf.OwnedProjects
-import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.{AmazonS3, BWMongoDB3, DynDoc}
+import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
-import com.buildwhiz.utils.BWLogger
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.Document
 import org.bson.types.ObjectId
 
 import scala.collection.JavaConverters._
-
-class ProjectApi extends HttpServlet with RestUtils {
-
-  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    BWLogger.log(getClass.getName, "doPost()", "ENTRY", request)
-    try {
-      val postData = getStreamData(request)
-      BWLogger.log(getClass.getName, "doPost(): postData", postData)
-      val project: DynDoc = Document.parse(postData)
-      if (!(project has "name"))
-        throw new IllegalArgumentException("No 'name' found")
-      if (!(project has "admin_person_id"))
-        throw new IllegalArgumentException("No 'admin_person_id' found")
-      if (!project.admin_person_id[AnyRef].isInstanceOf[ObjectId])
-        throw new IllegalArgumentException("Type of 'admin_person_id' not ObjectId")
-      project.description = s"This is the description of the '${project.name[String]}' project. " * 5
-      val latLong: DynDoc = Map("latitude" -> 37.7857971, "longitude" -> -122.4142195)
-      project.gps_location = latLong.asDoc
-      val address: DynDoc = Map("line1" -> "First line of the address", "line2" -> "Second line of the address",
-        "line3" -> "Third line of the address", "state" -> Map("name" -> "California", "code" -> "CA"),
-        "country" -> Map("name" -> "United States", "code" -> "US"), "postal_code" -> "94102")
-      project.address = address.asDoc
-      project.phase_ids = new java.util.ArrayList[ObjectId]
-      project.assigned_roles = new java.util.ArrayList[ObjectId]
-      project.system_labels = new java.util.ArrayList[ObjectId]
-      project.timestamps = new Document("created", System.currentTimeMillis)
-      project.status = "defined" // Initial status on creation
-      project.process_ids = new java.util.ArrayList[ObjectId]
-      BWMongoDB3.projects.insertOne(project.asDoc)
-
-      BWMongoDB3.persons.updateOne(Map("_id" -> project.admin_person_id[ObjectId]),
-        Map("$addToSet" -> Map("project_ids" -> project._id[ObjectId])))
-
-      val adminPersonOid = project.admin_person_id[ObjectId]
-      response.getWriter.println(bson2json(OwnedProjects.processProject(project, adminPersonOid).asDoc))
-      response.setContentType("application/json")
-      response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.audit(getClass.getName, "doPost", s"""Added Project '${project.name[String]}'""", request)
-    } catch {
-      case t: Throwable =>
-        BWLogger.log(getClass.getName, "doPost()", s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
-        //t.printStackTrace()
-        throw t
-    }
-  }
-
-/*
-  override def doPut(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    handleRestPut(request, response, "Project", "projects")
-  }
-*/
-
-/*
-  override def doDelete(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    BWLogger.log(getClass.getName, "doDelete()", s"ENTRY", request)
-    try {
-      val uriParts = new URI(request.getRequestURI.replace("[", "%5B").replace("]", "%5D")).getPath.split("/").toSeq.reverse
-      val projectOid = uriParts match {
-        case idString +: "Project" +: _ => new ObjectId(idString)
-        case _ => throw new IllegalArgumentException("Id not found")
-      }
-      val theProject: DynDoc = BWMongoDB3.projects.find(Map("_id" -> projectOid)).head
-      val phaseOids: Seq[ObjectId] = theProject.process_ids[Many[ObjectId]]
-      val phases: Seq[DynDoc] = BWMongoDB3.processes.find(Map("_id" -> Map("$in" -> phaseOids)))
-      val activityOids: Seq[ObjectId] = phases.flatMap(_.activity_ids[Many[ObjectId]])
-      BWMongoDB3.activities.deleteMany(Map("_id" -> Map("$in" -> activityOids)))
-      BWMongoDB3.processes.deleteMany(Map("_id" -> Map("$in" -> phaseOids)))
-      BWMongoDB3.mails.deleteMany(Map("project_id" -> projectOid))
-      BWMongoDB3.persons.updateMany(Map.empty[String, Any], Map("$pull" -> Map("project_ids" -> projectOid)))
-      BWMongoDB3.projects.deleteOne(Map("_id" -> projectOid))
-      // Delete project's documents
-      val objectSummaries: Seq[S3ObjectSummary] = AmazonS3.listObjects(projectOid.toString).getObjectSummaries.asScala
-      for (summary <- objectSummaries) {
-        AmazonS3.deleteObject(summary.getKey)
-      }
-      val projectNameAndId = s"""${theProject.name[String]} (${theProject._id[ObjectId]})"""
-      BWLogger.audit(getClass.getName, "doDelete", s"""Deleted Project '$projectNameAndId'""", request)
-    } catch {
-      case t: Throwable =>
-        BWLogger.log(getClass.getName, "doDelete()", s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
-        //t.printStackTrace()
-        throw t
-    }
-  }
-*/
-
-/*
-  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    handleRestGet(request, response, "Project", "projects")
-  }
-*/
-
-}
 
 object ProjectApi {
 

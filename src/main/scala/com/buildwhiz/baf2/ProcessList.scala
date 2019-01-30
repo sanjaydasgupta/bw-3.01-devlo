e6d90f84069b2d8f7321805c6d172b96a525b01b
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-01-30 23:00:17 +0530

    Augmented fields in process objects

--- src/main/scala/com/buildwhiz/baf2/ProcessList.scala
+++ src/main/scala/com/buildwhiz/baf2/ProcessList.scala
@@ -1,15 +1,25 @@
 package com.buildwhiz.baf2
 
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import BWMongoDB3._
+import DynDoc._
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.types.ObjectId
+import org.bson.Document
 
 class ProcessList extends HttpServlet with HttpUtils {
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
 
-    def process2json(process: DynDoc) = s"""{"_id": "${process._id[ObjectId]}", "name": "${process.name[String]}",""" +
-      s""" "status": "${process.status[String]}"}"""
+    def process2json(process: DynDoc): String = {
+      val adminPersonOid = process.admin_person_id[ObjectId]
+      val adminPerson: DynDoc = BWMongoDB3.persons.find(Map("_id" -> adminPersonOid)).head
+      val adminName = s"${adminPerson.first_name[String]} ${adminPerson.last_name[String]}"
+      val processDoc = new Document("_id", process._id[ObjectId]).append("name", process.name[String]).
+          append("status", process.status[String]).append("start_time", "0000-00-00 00:00").
+          append("end_time", "0000-00-00 00:00").append("manager", adminName)
+      bson2json(processDoc)
+    }
 
     BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
     val parameters = getParameterMap(request)

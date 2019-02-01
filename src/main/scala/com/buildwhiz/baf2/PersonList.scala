6167f816f2bed58a50c6209eb2ae27a6c5a98301
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-02-01 14:50:37 +0530

    Initial version

---
+++ src/main/scala/com/buildwhiz/baf2/PersonList.scala
@@ -0,0 +1,31 @@
+package com.buildwhiz.baf2
+
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import BWMongoDB3._
+import DynDoc._
+import com.buildwhiz.utils.{BWLogger, BpmnUtils}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.bson.types.ObjectId
+
+class PersonList extends HttpServlet with BpmnUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    try {
+      val persons: Seq[DynDoc] = BWMongoDB3.persons.find(Map("roles" -> "BW-Testing"))
+      val personJsons = persons.map(person => {
+        s"""{"name": "${person.first_name[String]} ${person.last_name[String]}", "_id": "${person._id[ObjectId]}"}"""
+      })
+      response.getWriter.print(personJsons.mkString("[", ", ", "]"))
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${persons.length})", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}

74ac537dac2bfaf99fcde064e3545c470b8a9701
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-02-03 14:16:16 +0530

    Corrections and changes to initial version

--- src/main/scala/com/buildwhiz/baf2/ProjectInfoSet.scala
+++ src/main/scala/com/buildwhiz/baf2/ProjectInfoSet.scala
@@ -11,22 +11,26 @@ class ProjectInfoSet extends HttpServlet with HttpUtils {
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
 
     BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
-    val parameters = getParameterMap(request)
     try {
-      val projectOid = new ObjectId(parameters("project_id"))
       val parameterNames = Seq("name", "description", "address", "construction_type", "type", "budget_mm_usd",
-        "construction_area_sqft", "land_area_acres", "max_building_height_ft")
+        "construction_area_sqft", "land_area_acres", "max_building_height_ft", "project_id")
       val postData = Document.parse(getStreamData(request))
       val unknownParameters = postData.keySet.toArray.filterNot(parameterNames.contains)
       if (unknownParameters.nonEmpty)
         throw new IllegalArgumentException(s"""Unknown parameter(s): ${unknownParameters.mkString(", ")}""")
-      val fieldValues = parameterNames.filter(postData.containsKey).
-          map(fldName => (fldName, postData.getString(fldName)))
-      val updateResult = BWMongoDB3.projects.updateOne(Map("_id" -> projectOid), Map("$set" -> fieldValues.toMap))
+      if (!postData.containsKey("project_id"))
+        throw new IllegalArgumentException("project_id not provided")
+      val projectOid = new ObjectId(postData.remove("project_id").asInstanceOf[String])
+      val paramNameValuePairs = parameterNames.filter(postData.containsKey).
+          map(paramName => (paramName, postData.getString(paramName)))
+      if (paramNameValuePairs.isEmpty)
+        throw new IllegalArgumentException("No parameters found")
+      val updateResult = BWMongoDB3.projects.updateOne(Map("_id" -> projectOid),
+          Map("$set" -> paramNameValuePairs.toMap))
       if (updateResult.getMatchedCount == 0)
         throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${fieldValues.length}", request)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${paramNameValuePairs.length}", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)

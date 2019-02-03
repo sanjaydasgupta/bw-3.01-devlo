215263e70a1dbdbe832d30c4e972a003d7d33b49
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-02-03 10:34:02 +0530

    Initial version

---
+++ src/main/scala/com/buildwhiz/baf2/ProjectInfoSet.scala
@@ -0,0 +1,38 @@
+package com.buildwhiz.baf2
+
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.infra.BWMongoDB3
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.bson.Document
+import org.bson.types.ObjectId
+
+class ProjectInfoSet extends HttpServlet with HttpUtils {
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val parameters = getParameterMap(request)
+    try {
+      val projectOid = new ObjectId(parameters("project_id"))
+      val parameterNames = Seq("name", "description", "address", "construction_type", "type", "budget_mm_usd",
+        "construction_area_sqft", "land_area_acres", "max_building_height_ft")
+      val postData = Document.parse(getStreamData(request))
+      val unknownParameters = postData.keySet.toArray.filterNot(parameterNames.contains)
+      if (unknownParameters.nonEmpty)
+        throw new IllegalArgumentException(s"""Unknown parameter(s): ${unknownParameters.mkString(", ")}""")
+      val fieldValues = parameterNames.filter(postData.containsKey).
+          map(fldName => (fldName, postData.getString(fldName)))
+      val updateResult = BWMongoDB3.projects.updateOne(Map("_id" -> projectOid), Map("$set" -> fieldValues.toMap))
+      if (updateResult.getMatchedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${fieldValues.length}", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}

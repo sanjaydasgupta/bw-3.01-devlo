5eed034aacdf4d8a48f7e5f4c704168b59eef2f7
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-02-08 07:53:44 +0530

    Enabling more granular updates

--- src/main/scala/com/buildwhiz/baf2/ProjectInfoSet.scala
+++ src/main/scala/com/buildwhiz/baf2/ProjectInfoSet.scala
@@ -12,8 +12,17 @@ class ProjectInfoSet extends HttpServlet with HttpUtils {
 
     BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
     try {
-      val parameterNames = Seq("name", "description", "address", "construction_type", "type", "budget_mm_usd",
-        "construction_area_sqft", "land_area_acres", "max_building_height_ft", "project_id")
+      val parameterConverters: Map[String, String => Any] = Map(
+        ("name", s => s), ("description", s => s),
+        ("address.state.name", s => s), ("address.state.code", s => s),
+        ("address.country.name", s => s), ("address.country.code", s => s),
+        ("address.gps_location.latitude", _.toDouble), ("address.gps_location.longitude", _.toDouble),
+        ("address.line1", s => s), ("address.line2", s => s), ("address.line3", s => s),
+        ("address.postal_code", s => s),
+        ("construction_type", s => s), ("type", s => s), ("budget_mm_usd", _.toDouble),
+        ("construction_area_sqft", _.toDouble), ("land_area_acres", _.toDouble),
+        ("max_building_height_ft", _.toDouble), ("project_id", s => s))
+      val parameterNames = parameterConverters.keys.toSeq
       val postData = Document.parse(getStreamData(request))
       val unknownParameters = postData.keySet.toArray.filterNot(parameterNames.contains)
       if (unknownParameters.nonEmpty)
@@ -22,7 +31,7 @@ class ProjectInfoSet extends HttpServlet with HttpUtils {
         throw new IllegalArgumentException("project_id not provided")
       val projectOid = new ObjectId(postData.remove("project_id").asInstanceOf[String])
       val paramNameValuePairs = parameterNames.filter(postData.containsKey).
-          map(paramName => (paramName, postData.getString(paramName)))
+          map(paramName => (paramName, parameterConverters(paramName)(postData.getString(paramName))))
       if (paramNameValuePairs.isEmpty)
         throw new IllegalArgumentException("No parameters found")
       val updateResult = BWMongoDB3.projects.updateOne(Map("_id" -> projectOid),

28a9b653a81ea9376e03cc7c1167a57621590d8a
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-02-16 12:29:27 +0530

    Moved storeAmazonS3() here

--- src/main/scala/com/buildwhiz/baf2/DocumentApi.scala
+++ src/main/scala/com/buildwhiz/baf2/DocumentApi.scala
@@ -1,10 +1,15 @@
 package com.buildwhiz.baf2
 
-import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import java.io.{File, FileOutputStream, InputStream}
+
+import com.buildwhiz.infra.{AmazonS3, BWMongoDB3, DynDoc}
 import BWMongoDB3._
 import DynDoc._
+import com.buildwhiz.utils.BWLogger
+import javax.servlet.http.HttpServletRequest
 import org.bson.types.ObjectId
 
+import scala.annotation.tailrec
 import scala.util.parsing.combinator.RegexParsers
 
 object DocumentApi {
@@ -13,7 +18,46 @@ object DocumentApi {
 
   def exists(documentOid: ObjectId): Boolean = BWMongoDB3.document_master.find(Map("_id" -> documentOid)).nonEmpty
 
-  object logic extends RegexParsers {
+  def storeAmazonS3(fileName: String, is: InputStream, projectId: String, documentOid: ObjectId, timestamp: Long,
+      comments: String, authorOid: ObjectId, request: HttpServletRequest): (String, Long) = {
+    BWLogger.log(getClass.getName, "storeAmazonS3", "ENTRY", request)
+    val s3key = f"$projectId-$documentOid-$timestamp%x"
+    BWLogger.log(getClass.getName, "storeAmazonS3", s"amazonS3Key: $s3key", request)
+    val file = new File(s3key)
+    var fileLength = 0L
+    try {
+      val outFile = new FileOutputStream(file)
+      val buffer = new Array[Byte](4096)
+      @tailrec def handleBlock(length: Int = 0): Int = {
+        val bytesRead = is.read(buffer)
+        if (bytesRead > 0) {
+          outFile.write(buffer, 0, bytesRead)
+          handleBlock(length + bytesRead)
+        } else {
+          outFile.close()
+          length
+        }
+      }
+      fileLength = handleBlock()
+      AmazonS3.putObject(s3key, file)
+      val versionRecord = Map("comments" -> comments, "timestamp" -> timestamp, "author_person_id" -> authorOid,
+        "file_name" -> fileName, "size" -> fileLength)
+      val updateResult = BWMongoDB3.document_master.
+        updateOne(Map("_id" -> documentOid), Map("$push" -> Map("versions" -> versionRecord)))
+      if (updateResult.getModifiedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+      BWLogger.log(getClass.getName, s"storeAmazonS3 ($fileLength)", "EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "storeAmazonS3", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+    try {file.delete()} catch {case _: Throwable => /* No recovery */}
+    (s3key, fileLength)
+  }
+
+  object tagLogic extends RegexParsers {
 
     type TestSet = Set[String] => Boolean
 

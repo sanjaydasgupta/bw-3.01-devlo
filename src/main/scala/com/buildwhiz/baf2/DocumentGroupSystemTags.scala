3bb49e6b7e63314b7deeab0c6415baf5d7a28d89
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-02-20 08:17:57 +0530

    Updated to support (all/some/none) response for each tag

--- src/main/scala/com/buildwhiz/baf2/DocumentGroupSystemTags.scala
+++ src/main/scala/com/buildwhiz/baf2/DocumentGroupSystemTags.scala
@@ -6,6 +6,7 @@ import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.types.ObjectId
+import org.bson.Document
 
 class DocumentGroupSystemTags extends HttpServlet with HttpUtils {
 
@@ -35,15 +36,35 @@ class DocumentGroupSystemTags extends HttpServlet with HttpUtils {
       val docOids: Seq[ObjectId] = docIds.map(id => new ObjectId(id.trim))
 
       val documentRecords: Seq[DynDoc] = BWMongoDB3.document_master.find(Map("_id" -> Map("$in" -> docOids)))
+      val projectOids = documentRecords.map(_.project_id[ObjectId])
+      val commonProjectOid = projectOids.reduceLeft((a, b) => if (a == b) a else b)
+      if (commonProjectOid != projectOids.head)
+        throw new IllegalArgumentException("Documents not from same project")
 
-      val systemTags: Seq[String] = documentRecords.
-        flatMap(docRecord => DocumentApi.getSystemTags(docRecord)).distinct.sorted
-      val systemTagsArray = systemTags.map(ul => s""""$ul"""").mkString("[", ", ", "]")
+      val allSystemTagNames: Seq[String] = ProjectApi.documentTags(ProjectApi.projectById(commonProjectOid)).
+          map(_.name[String])
+      val systemTagsByDocument: Seq[Seq[String]] =
+          documentRecords.map(docRecord => DocumentApi.getSystemTags(docRecord))
+      val documentCountsByTagName: Seq[(String, Int)] = allSystemTagNames.map(tagName =>
+        (tagName, systemTagsByDocument.map(_.contains(tagName)).count(b => b))
+      )
+      val statusByTagName: Seq[(String, String)] = documentCountsByTagName.map(count => {(count._1,
+        if (count._2 == 0)
+          "none"
+        else if (count._2 == docOids.length)
+          "all"
+        else
+          "some"
+        )
+      }).sortBy(_._1)
 
-      response.getWriter.println(systemTagsArray)
+      val systemTagsStatus: Seq[Document] = statusByTagName.map(p => new Document("name", p._1).append("scope", p._2))
+      val systemTagsJsonString = systemTagsStatus.map(bson2json).mkString("[", ", ", "]")
+
+      response.getWriter.println(systemTagsJsonString)
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
-      systemTagsArray.length
+      systemTagsJsonString.length
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
--- src/main/scala/com/buildwhiz/baf2/DocumentGroupUserTags.scala
+++ src/main/scala/com/buildwhiz/baf2/DocumentGroupUserTags.scala
@@ -1,10 +1,10 @@
 package com.buildwhiz.baf2
 
-import com.buildwhiz.infra.BWMongoDB3._
+import com.buildwhiz.infra.DynDoc
 import com.buildwhiz.infra.DynDoc._
-import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.bson.Document
 import org.bson.types.ObjectId
 
 class DocumentGroupUserTags extends HttpServlet with HttpUtils {
@@ -27,7 +27,7 @@ class DocumentGroupUserTags extends HttpServlet with HttpUtils {
     val parameters = getParameterMap(request)
     try {
       val user: DynDoc = getUser(request)
-      val person: DynDoc = PersonApi.personById(user._id[ObjectId])
+      val freshUserRecord: DynDoc = PersonApi.personById(user._id[ObjectId])
 
       val docIds: Seq[String] = (if (request.getMethod == "GET") {
         parameters("document_ids")
@@ -35,31 +35,31 @@ class DocumentGroupUserTags extends HttpServlet with HttpUtils {
         getStreamData(request)
       }).split(",").map(_.trim)
 
-      val docOids: Seq[ObjectId] = docIds.map(id => new ObjectId(id.trim))
+      val docOids: Seq[ObjectId] = docIds.map(id => new ObjectId(id))
 
-      val documentRecords: Seq[DynDoc] = BWMongoDB3.document_master.find(Map("_id" -> Map("$in" -> docOids)))
+      val allUserTagInfo = PersonApi.documentTags(freshUserRecord)
 
-      val docOid2UserTags: Map[ObjectId, Seq[String]] = DocumentApi.docOid2UserTags(person)
-      val docOid2SystemTags: Map[ObjectId, Seq[String]] = documentRecords.
-          map(docRec => (docRec._id[ObjectId], DocumentApi.getSystemTags(docRec))).toMap
+      val documentCountsByTagName: Seq[(String, Int)] = allUserTagInfo.map(tagInfo => {
+        (tagInfo.name[String], docOids.map(oid => tagInfo.document_ids[Many[ObjectId]].contains(oid)).count(b => b))
+      })
 
-      val allUserTags: Seq[String] = docOids.flatMap(oid =>
-        if (docOid2UserTags.contains(oid) || docOid2SystemTags.contains(oid)) {
-          val userTags = if (docOid2UserTags.contains(oid)) docOid2UserTags(oid) else Seq.empty[String]
-          val systemTags = if (docOid2SystemTags.contains(oid)) docOid2SystemTags(oid) else Seq.empty[String]
-          val logicTags = DocumentApi.getLogicalTags(userTags ++ systemTags, person)
-          userTags ++ logicTags
-        } else {
-          Seq.empty[String]
-        }
-      ).distinct
+      val statusByTagName: Seq[(String, String)] = documentCountsByTagName.map(count => {(count._1,
+        if (count._2 == 0)
+          "none"
+        else if (count._2 == docOids.length)
+          "all"
+        else
+          "some"
+      )
+      }).sortBy(_._1)
 
-      val userTagsArray = allUserTags.sorted.map(ul => s""""$ul"""").mkString("[", ", ", "]")
+      val userTagsStatus: Seq[Document] = statusByTagName.map(p => new Document("name", p._1).append("scope", p._2))
+      val userTagsJsonString = userTagsStatus.map(bson2json).mkString("[", ", ", "]")
 
-      response.getWriter.println(userTagsArray)
+      response.getWriter.println(userTagsJsonString)
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
-      userTagsArray.length
+      userTagsJsonString.length
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
@@ -68,4 +68,3 @@ class DocumentGroupUserTags extends HttpServlet with HttpUtils {
   }
 
 }
-

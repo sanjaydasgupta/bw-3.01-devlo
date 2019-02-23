e635baf1603f91ae3242ed684e36e6b50f62ed88
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-02-23 13:03:30 +0530

    Initial version

---
+++ src/main/scala/com/buildwhiz/baf2/RfiApi.scala
@@ -0,0 +1,26 @@
+package com.buildwhiz.baf2
+
+import com.buildwhiz.infra.BWMongoDB3._
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import org.bson.Document
+import org.bson.types.ObjectId
+
+object RfiApi {
+
+  def rfiById(rfiOid: ObjectId): DynDoc = BWMongoDB3.rfi_messages.find(Map("_id" -> rfiOid)).headOption match {
+    case None => throw new IllegalArgumentException(s"Bad rfi-id: '$rfiOid'")
+    case Some(rfi) => rfi
+  }
+
+  def exists(rfiOid: ObjectId): Boolean = BWMongoDB3.rfi_messages.find(Map("_id" -> rfiOid)).nonEmpty
+
+  def rfisByUser(userOid: ObjectId): Seq[DynDoc] = {
+    val rfis: Seq[DynDoc] = BWMongoDB3.activities.find()
+    rfis
+  }
+
+  //def hasRole(personOid: ObjectId, activity: DynDoc): Boolean =
+  //  allActions(activity).exists(_.assignee_person_id[ObjectId] == personOid)
+
+}

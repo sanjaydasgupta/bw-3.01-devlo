275fa637d9e754e5a8baa79e80f805fbc1324cd6
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-02-26 09:45:22 +0530

    Integrated with database (replacing dummy data)

--- src/main/scala/com/buildwhiz/baf2/TaskDocumentInfo.scala
+++ src/main/scala/com/buildwhiz/baf2/TaskDocumentInfo.scala
@@ -1,6 +1,5 @@
 package com.buildwhiz.baf2
 
-import com.buildwhiz.dot.GetDocumentVersionsList
 import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
@@ -11,47 +10,51 @@ import org.bson.types.ObjectId
 
 class TaskDocumentInfo extends HttpServlet with HttpUtils with DateTimeUtils {
 
-  private def requiredDocumentItems(user: DynDoc, activity: DynDoc, process: DynDoc, phase: DynDoc, project: DynDoc):
-      Seq[Document] = {
-    (1 to 7).map(n => {
-      val docType = Seq("pdf", "excel")(n % 2)
-      val count = Seq(0, 1)(n % 2)
-      val itemNumber = n.toString * 7
-      val date = s"2019-01-2$n"
-      new Document("name", s"Req-Doc-$itemNumber").append("type", docType).append("version_date", date).
-        append("version_count", count)
+  private def taskAssociatedDocuments(user: DynDoc, activity: DynDoc, actionName: String): Seq[DynDoc] = {
+    val documentRecords: Seq[DynDoc] = BWMongoDB3.document_master.
+        find(Map("activity_id" -> activity._id[ObjectId], "action_name" -> actionName,
+        "category" -> Map("$exists" -> true)))
+    documentRecords
+  }
+
+  private def uiFormattedRecords(user: DynDoc, documents: Seq[DynDoc]): Seq[Document] = {
+    documents.map(document => {
+      val versions: Seq[DynDoc] = document.versions[Many[Document]]
+      val currentVersion: DynDoc = versions.last
+      val timezone = user.tz[String]
+      val versionTimestamp = currentVersion.timestamp[Long]
+      val versionDate = dateTimeString(versionTimestamp, Some(timezone))
+      val fileName = currentVersion.file_name[String]
+      val fileType = if (fileName.contains(".")) fileName.split("\\.").last else "???"
+      val downloadUrl = s"baf2/DocumentVersionDownload?document_master_id=${document._id[ObjectId]}" +
+          s"&timestamp=$versionTimestamp"
+      new Document("name", document.name[String]).append("type", fileType).append("version_date", versionDate).
+        append("version_count", versions.length).append("download_url", downloadUrl)
     })
   }
 
-  private def additionalDocumentItems(user: DynDoc, activity: DynDoc, process: DynDoc, phase: DynDoc, project: DynDoc):
-      Seq[Document] = {
-    (1 to 2).map(n => {
-      val docType = Seq("word", "excel")(n % 2)
-      val count = Seq(0, 1)(n % 2)
-      val itemNumber = n.toString * 9
-      val date = s"2019-02-2$n"
-      new Document("name", s"Addl-Doc-$itemNumber").append("type", docType).append("version_date", date).
-        append("version_count", count)
-    })
+  private def checkListItems(user: DynDoc, activity: DynDoc, actionName: String, theAction: DynDoc): Seq[Document] = {
+    if (theAction.has("check_list")) {
+      theAction.check_list[Many[Document]]
+    } else
+      Seq.empty[Document]
   }
 
-  private def checkListItems(user: DynDoc, activity: DynDoc, process: DynDoc, phase: DynDoc, project: DynDoc):
-      Seq[Document] = {
-    (1 to 5).map(n => {
-      val complete = (n % 2) == 0
-      val itemNumber = n.toString * 5
-      new Document("name", s"Check-$itemNumber").append("complete", s"$complete")
-    })
-  }
-
-  private def taskDocumentRecord(user: DynDoc, activity: DynDoc, process: DynDoc, phase: DynDoc, project: DynDoc):
-      String = {
-    val checkList = checkListItems(user, activity, process, phase, project)
-    val requiredDocuments = requiredDocumentItems(user, activity, process, phase, project)
-    val additionalDocuments = additionalDocumentItems(user, activity, process, phase, project)
+  private def taskDocumentInfoRecord(user: DynDoc, activity: DynDoc, actionName: String, action: DynDoc,
+      process: DynDoc, phase: DynDoc, project: DynDoc): String = {
+    val documents = taskAssociatedDocuments(user, activity, actionName)
+    val checkList = checkListItems(user, activity, actionName, action)
+    val requiredDocuments = uiFormattedRecords(user, documents.filter(_.category[String] == "required"))
+    val additionalDocuments = uiFormattedRecords(user, documents.filter(_.category[String] == "additional"))
+    val specificationDocuments = uiFormattedRecords(user, documents.filter(_.category[String] == "specification"))
+    val specificationUrl = specificationDocuments.headOption match {
+      case None => ""
+      case Some(doc) => doc.get("specification_url").asInstanceOf[String]
+    }
     val enableAddButtons = PersonApi.isBuildWhizAdmin(user._id[ObjectId]) ||
         ProcessApi.canManage(user._id[ObjectId], process)
-    val record = new Document("task_specification_url", "???").append("check_list", checkList).
+    // ToDo: task-specification-url incomplete
+    val record = new Document("task_specification_url", specificationUrl).append("check_list", checkList).
         append("required_documents", requiredDocuments).append("additional_documents", additionalDocuments).
         append("enable_add_buttons", enableAddButtons)
     bson2json(record)
@@ -63,12 +66,19 @@ class TaskDocumentInfo extends HttpServlet with HttpUtils with DateTimeUtils {
     try {
       val activityOid = new ObjectId(parameters("activity_id"))
       val theActivity = ActivityApi.activityById(activityOid)
+      val actions = ActivityApi.allActions(theActivity)
+      val actionName = parameters("action_name")
+      val theAction = actions.find(_.name[String] == actionName) match {
+        case Some(a) => a
+        case None => throw new IllegalArgumentException(s"Bad action-name: '$actionName'")
+      }
       val parentProcess = ActivityApi.parentProcess(activityOid)
       val ancestorPhase = ProcessApi.parentPhase(parentProcess._id[ObjectId])
       val ancestorProject = PhaseApi.parentProject(ancestorPhase._id[ObjectId])
       val user: DynDoc = getUser(request)
       val freshUserRecord = PersonApi.personById(user._id[ObjectId])
-      response.getWriter.print(taskDocumentRecord(freshUserRecord, theActivity, parentProcess, ancestorPhase, ancestorProject))
+      response.getWriter.print(taskDocumentInfoRecord(freshUserRecord, theActivity, actionName, theAction,
+          parentProcess, ancestorPhase, ancestorProject))
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.log(getClass.getName, request.getMethod, "EXIT-OK", request)

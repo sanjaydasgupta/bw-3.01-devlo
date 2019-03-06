72dd19803512629e92e6a0b1aa3e0abeaacedcc9
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-03-06 11:49:16 +0530

    Improvements in handling of data gaps

--- src/main/scala/com/buildwhiz/baf2/DocumentVersionList.scala
+++ src/main/scala/com/buildwhiz/baf2/DocumentVersionList.scala
@@ -13,8 +13,7 @@ class DocumentVersionList extends HttpServlet with HttpUtils with DateTimeUtils 
   private def versions(doc: DynDoc): Seq[DynDoc] = {
     val allVersions: Seq[DynDoc] =
       if (doc.has("versions")) doc.versions[Many[Document]] else Seq.empty[DynDoc]
-    val requiredFields = Seq("author_person_id", "file_name", "timestamp")
-    allVersions.filter(version => requiredFields.forall(field => version.has(field)))
+    allVersions.filter(_.has("timestamp"))
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
@@ -23,14 +22,17 @@ class DocumentVersionList extends HttpServlet with HttpUtils with DateTimeUtils 
     try {
       val user: DynDoc = getUser(request)
       val documentOid = new ObjectId(parameters("document_id"))
-      //val projectOid = new ObjectId(parameters("project_id"))
       val docRecord: DynDoc = BWMongoDB3.document_master.find(Map("_id" -> documentOid)).head
       val goodVersions = versions(docRecord).sortWith(_.timestamp[Long] < _.timestamp[Long])
       val versionProperties: Seq[Document] = goodVersions.map(version => {
-        val authorOid = version.author_person_id[ObjectId]
-        val author: DynDoc = BWMongoDB3.persons.find(Map("_id" -> authorOid)).head
-        val authorName = s"${author.first_name[String]} ${author.last_name[String]}"
-        val prop: Document = Map("filename" -> version.file_name[String], "author" -> authorName,
+        val authorName = if (version.has("author_person_id")) {
+          val authorOid = version.author_person_id[ObjectId]
+          val author: DynDoc = BWMongoDB3.persons.find(Map("_id" -> authorOid)).head
+          s"${author.first_name[String]} ${author.last_name[String]}"
+        } else
+          "NA"
+        val fileName = if (version.has("file_name")) version.file_name[String] else docRecord.name[String]
+        val prop: Document = Map("filename" -> fileName, "author" -> authorName,
           "timestamp" -> version.timestamp[Long], "comments" -> version.comments[String],
           "datetime" -> dateTimeString(version.timestamp[Long], Some(user.tz[String])))
         prop

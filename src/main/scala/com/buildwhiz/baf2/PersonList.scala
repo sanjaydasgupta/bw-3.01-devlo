be3ec61c9871f64bb1ed53056429f9168f2aca39
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-03-07 10:36:19 +0530

    Added parameter role as a filter

--- src/main/scala/com/buildwhiz/baf2/PersonList.scala
+++ src/main/scala/com/buildwhiz/baf2/PersonList.scala
@@ -3,16 +3,21 @@ package com.buildwhiz.baf2
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import BWMongoDB3._
 import DynDoc._
-import com.buildwhiz.utils.{BWLogger, BpmnUtils}
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.types.ObjectId
 
-class PersonList extends HttpServlet with BpmnUtils {
+class PersonList extends HttpServlet with HttpUtils {
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val parameters = getParameterMap(request)
     try {
-      val persons: Seq[DynDoc] = BWMongoDB3.persons.find(Map("roles" -> "BW-Testing"))
+      val query = parameters.get("role") match {
+        case None => Map.empty[String, Any]
+        case Some(role) => Map("roles" -> role)
+      }
+      val persons: Seq[DynDoc] = BWMongoDB3.persons.find(query)
       val personJsons = persons.map(person => {
         s"""{"name": "${person.first_name[String]} ${person.last_name[String]}", "_id": "${person._id[ObjectId]}"}"""
       })

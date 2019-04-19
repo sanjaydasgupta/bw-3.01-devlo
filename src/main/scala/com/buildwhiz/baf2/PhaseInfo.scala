e4037c04465fc4cd3b78c96254725ebf8c89bbde
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-04-19 07:45:03 +0530

    Added enable/disable flag 'can_add_process' for add-process button

--- src/main/scala/com/buildwhiz/baf2/PhaseInfo.scala
+++ src/main/scala/com/buildwhiz/baf2/PhaseInfo.scala
@@ -42,8 +42,9 @@ class PhaseInfo extends HttpServlet with HttpUtils with DateTimeUtils {
     val status = new Document("editable", false).append("value", phase.status[String])
     val displayStatus = new Document("editable", false).append("value", PhaseApi.displayStatus(phase))
     val name = new Document("editable", editable).append("value", phase.name[String])
+    val canManage = PhaseApi.canManage(user._id[ObjectId], phase)
     val projectDoc = new Document("name", name).append("description", description).
-        append("status", status).append("display_status", displayStatus).
+        append("status", status).append("display_status", displayStatus).append("can_add_process", canManage).
         append("process_info", processInformation(phase, user))
     projectDoc.toJson
   }

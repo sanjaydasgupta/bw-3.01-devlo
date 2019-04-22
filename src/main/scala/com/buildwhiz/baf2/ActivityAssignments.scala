279f987fd5be22cdc141f63f8ec376fab7c71d5b
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-04-22 18:58:16 +0530

    Initial version

--- src/main/scala/com/buildwhiz/baf2/ActivityAssignments.scala
+++ src/main/scala/com/buildwhiz/baf2/ActivityAssignments.scala
@@ -10,14 +10,45 @@ import org.bson.types.ObjectId
 class ActivityAssignments extends HttpServlet with HttpUtils {
 
   private def activityAssignments(activity: DynDoc): Seq[Document] = {
-    Seq("Principal", "Finance", "Admin", "Lead", "Member").map(indRole => {
-      new Document("_id", activity._id[ObjectId].toString).append("name", activity.name[String]).
-        append("role", activity.role[String]).append("organization_name", "Some Organization").
-        append("individual_role", indRole).append("person_name", "Some Person").
-        append("process_name", "Some Process").
-        append("person_id", "000000000000000000000000").append("organization_id", "000000000000000000000000").
-        append("doc_access", "SomeCategory")
-    })
+    val baseRole = activity.role[String]
+    val process = ActivityApi.parentProcess(activity._id[ObjectId])
+    if (activity.has("assignments")) {
+      val assignments: Seq[DynDoc] = activity.assignment[Many[Document]]
+      assignments.map(assignment => {
+        val assignmentDoc = new Document("role", assignment.role[String])
+        if (assignment.has("organization_id")) {
+          val orgOid = assignment.organization_id[ObjectId]
+          assignmentDoc.append("organization_id", orgOid)
+          val org = OrganizationApi.organizationById(orgOid)
+          assignmentDoc.append("organization_name", org.name[String])
+        } else {
+          assignmentDoc.append("organization_id", "")
+          assignmentDoc.append("organization_name", "")
+        }
+        if (assignment.has("person_id")) {
+          val personOid = assignment.person_id[ObjectId]
+          assignmentDoc.append("person_id", personOid)
+          val person = PersonApi.personById(personOid)
+          assignmentDoc.append("person_name", s"${person.first_name[String]} ${person.last_name[String]}")
+        } else {
+          assignmentDoc.append("person_id", "")
+          assignmentDoc.append("person_name", "")
+        }
+        if (assignment.has("individual_role")) {
+          val indRole = assignment.individual_role[String]
+          assignmentDoc.append("individual_role", indRole)
+        } else {
+          assignmentDoc.append("individual_role", "")
+        }
+        assignmentDoc
+      })
+    } else {
+      Seq(new Document("_id", activity._id[ObjectId].toString).append("name", activity.name[String]).
+        append("role", baseRole).append("organization_name", "").
+        append("individual_role", "").append("person_name", "").append("process_name", process.name[String]).
+        append("person_id", "").append("organization_id", "").append("doc_access", "").append("can_delete", false))
+    }
+
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {

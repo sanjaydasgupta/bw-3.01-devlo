0c7d4a29fcf2b49398c518235cbc85436b9eb599
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-04-22 18:11:11 +0530

    Initial version

---
+++ src/main/scala/com/buildwhiz/baf2/RoleListSecondary.scala
@@ -0,0 +1,33 @@
+package com.buildwhiz.baf2
+
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.bson.types.ObjectId
+
+class RoleListSecondary extends HttpServlet with HttpUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
+    try {
+      val activityOid = new ObjectId(parameters("activity_id"))
+      val theActivity = ActivityApi.activityById(activityOid)
+//      val user: DynDoc = getUser(request)
+//      if (!ProcessApi.canManage(user._id[ObjectId], theActivity))
+//        throw new IllegalArgumentException("Not permitted")
+      response.getWriter.print(RoleListSecondary.secondaryRoles.mkString("[\"", "\", \"", "\"]"))
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+}
+
+object RoleListSecondary {
+  val secondaryRoles: Seq[String] = Seq("pre-approval", "post-approval", "other")
+}

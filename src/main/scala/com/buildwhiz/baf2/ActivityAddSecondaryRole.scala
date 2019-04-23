fb5e7d7c5c3b8ecb9ba4997811a58bc092015981
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-04-23 17:09:36 +0530

    Added optional parameter organization_id

--- src/main/scala/com/buildwhiz/baf2/ActivityAddSecondaryRole.scala
+++ src/main/scala/com/buildwhiz/baf2/ActivityAddSecondaryRole.scala
@@ -16,22 +16,37 @@ class ActivityAddSecondaryRole extends HttpServlet with HttpUtils {
       //      val user: DynDoc = getUser(request)
       //      if (!ProcessApi.canManage(user._id[ObjectId], theActivity))
       //        throw new IllegalArgumentException("Not permitted")
+      val newRole = parameters("role")
+      if (!RoleListSecondary.secondaryRoles.contains(newRole))
+        throw new IllegalArgumentException(s"Bad role: '$newRole'")
+
       val activityOid = new ObjectId(parameters("activity_id"))
       val theActivity = ActivityApi.activityById(activityOid)
+
       val assignments: Seq[DynDoc] = if (theActivity.has("assignments")) {
         theActivity.assignments[Many[Document]]
       } else {
         Seq.empty[DynDoc]
       }
-      val newRole = parameters("role")
-      if (!RoleListSecondary.secondaryRoles.contains(newRole))
-        throw new IllegalArgumentException(s"Bad role: '$newRole'")
       if (assignments.exists(_.role[String] == newRole))
         throw new IllegalArgumentException(s"Role '$newRole' already exists")
-      val updateResult = BWMongoDB3.activities.updateOne(Map("_id" -> activityOid),
-        Map("$push" -> Map("assignments" -> Map("role" -> newRole))))
+
+      val dbUpdater = parameters.get("organization_id").map(new ObjectId(_)) match {
+        case None => Map("$push" -> Map("assignments" -> Map("role" -> newRole)))
+        case Some(organizationOid) =>
+          if (!OrganizationApi.exists(organizationOid))
+            throw new IllegalArgumentException(s"Bad organization_id: '$organizationOid'")
+          val orgRecord = OrganizationApi.organizationById(organizationOid)
+          val orgSkills: Seq[String] = orgRecord.skills[Many[String]]
+          if (!orgSkills.contains(newRole))
+            throw new IllegalArgumentException(s"Organization '$organizationOid' does not have skill '$newRole'")
+          Map("$push" -> Map("assignments" -> Map("role" -> newRole, "organization_id" -> organizationOid)))
+      }
+
+      val updateResult = BWMongoDB3.activities.updateOne(Map("_id" -> activityOid), dbUpdater)
       if (updateResult.getMatchedCount == 0)
         throw new IllegalArgumentException(s"MongoDB error: $updateResult")
+
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)
     } catch {

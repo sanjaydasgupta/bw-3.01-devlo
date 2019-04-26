6bd38e4c3beb5593f2aefdcda8f2bbda5bb05420
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-04-26 09:32:20 +0530

    Initial version

---
+++ src/main/scala/com/buildwhiz/baf2/PersonIndividualRolesList.scala
@@ -0,0 +1,25 @@
+package com.buildwhiz.baf2
+
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+class PersonIndividualRolesList extends HttpServlet with HttpUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    //val parameters = getParameterMap(request)
+    try {
+      val individualRoles = Seq("Principal", "Admin", "Finance", "Contract", "Lead", "Contributor")
+      response.getWriter.print(individualRoles.map(ir => "\"%s\"".format(ir)).mkString("[", ",", "]"))
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${individualRoles.length})", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}

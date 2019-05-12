7a05ff8bd0c538ce7e55424b9e89396247fcdbdd
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-05-12 18:01:08 +0530

    Initial version

---
+++ src/main/scala/com/buildwhiz/baf2/ZoneAdd.scala
@@ -0,0 +1,55 @@
+package com.buildwhiz.baf2
+
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.bson.Document
+import org.bson.types.ObjectId
+
+import scala.collection.JavaConverters._
+
+class ZoneAdd extends HttpServlet with HttpUtils {
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val parameters = getParameterMap(request)
+    try {
+
+      val phaseOid = new ObjectId(parameters("phase_id"))
+      if (!PhaseApi.exists(phaseOid))
+        throw new IllegalArgumentException(s"Unknown phase-id: '$phaseOid'")
+
+      val zoneName = parameters("zone_name")
+
+      val parentProject = PhaseApi.parentProject(phaseOid)
+      val projectOid = parentProject._id[ObjectId]
+
+      val user: DynDoc = getUser(request)
+      val userOid = user._id[ObjectId]
+      if (!PhaseApi.canManage(userOid, PhaseApi.phaseById(phaseOid)))
+        throw new IllegalArgumentException("Not permitted")
+
+      val queryRecord = Map("name" -> zoneName, "project_id" -> projectOid, "phase_id" -> phaseOid)
+      if (BWMongoDB3.zones.count(queryRecord) > 0)
+        throw new IllegalArgumentException(s"Zone '$zoneName' already exists")
+
+      val area = parameters("area")
+      val location = parameters("location")
+      val description = parameters("description")
+
+      val newZoneRecord: Document = queryRecord ++ Map("activity_ids" -> Seq.empty[ObjectId].asJava,
+          "area" -> area, "location" -> location, "description" -> description)
+      BWMongoDB3.zones.insertOne(newZoneRecord)
+
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, "EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}
---
+++ src/main/scala/com/buildwhiz/baf2/ZoneInfo.scala
@@ -0,0 +1,71 @@
+package com.buildwhiz.baf2
+
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.infra.DynDoc
+import com.buildwhiz.utils.{BWLogger, DateTimeUtils, HttpUtils}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.bson.Document
+import org.bson.types.ObjectId
+
+import scala.collection.JavaConverters._
+
+class ZoneInfo extends HttpServlet with HttpUtils with DateTimeUtils {
+
+  private def isEditable(zone: DynDoc, user: DynDoc): Boolean = {
+    val userOid = user._id[ObjectId]
+    ZoneApi.canManage(userOid, zone)
+  }
+
+  private def activityInformation(zone: DynDoc, user: DynDoc): Many[Document] = {
+    val activities: Seq[DynDoc] = ZoneApi.allActivities(zone)
+    val returnValue: Seq[Document] = activities.map(activity => {
+      val timestamps: DynDoc = activity.timestamps[Document]
+      val timeZone = user.tz[String]
+      val (startTime, endTime) = if (timestamps.has("end")) {
+        (dateTimeString(timestamps.start[Long], Some(timeZone)), dateTimeString(timestamps.end[Long], Some(timeZone)))
+      } else if (timestamps.has("start")) {
+        (dateTimeString(timestamps.start[Long], Some(timeZone)), "NA")
+      } else {
+        ("NA", "NA")
+      }
+      Map("_id" -> activity._id[ObjectId].toString, "name" -> activity.name[String],
+          "bpmn_name" -> activity.bpmn_name[String], "status" -> activity.status[String],
+          "start_date" -> startTime, "end_date" -> endTime)
+    })
+    returnValue.asJava
+  }
+
+  private def zone2json(zone: DynDoc, user: DynDoc, editable: Boolean): String = {
+    val description = new Document("editable", editable).append("value", zone.description[String])
+    val location = new Document("editable", editable).append("value", zone.location[String])
+    val area = new Document("editable", editable).append("value", zone.area[String])
+    val name = new Document("editable", editable).append("value", zone.name[String])
+
+    val zoneDoc = new Document("name", name).append("description", description).
+        append("location", location).append("area", area).
+        append("activity_info", activityInformation(zone, user))
+    zoneDoc.toJson
+  }
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val parameters = getParameterMap(request)
+    try {
+      val zoneOid = new ObjectId(parameters("zone_id"))
+      val zoneRecord: DynDoc = ZoneApi.zoneById(zoneOid)
+      val user: DynDoc = getUser(request)
+      val zoneIsEditable = isEditable(zoneRecord, user)
+      response.getWriter.print(zone2json(zoneRecord, user, zoneIsEditable))
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, "EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}

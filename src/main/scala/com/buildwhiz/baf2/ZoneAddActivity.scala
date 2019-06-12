78c61cea766a8c899da7f40bda9e3cab016a8e60
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-06-12 12:54:00 +0530

    Allowed multiple zones to be assigned together

--- src/main/scala/com/buildwhiz/baf2/ZoneAddActivity.scala
+++ src/main/scala/com/buildwhiz/baf2/ZoneAddActivity.scala
@@ -14,10 +14,11 @@ class ZoneAddActivity extends HttpServlet with HttpUtils {
     try {
       val parameters = getParameterMap(request)
 
-      val zoneOid = new ObjectId(parameters("zone_id"))
-      if (!ZoneApi.exists(zoneOid))
-        throw new IllegalArgumentException(s"Bad zone_id '$zoneOid'")
-      val theZone = ZoneApi.zoneById(zoneOid)
+      val zoneOids: Seq[ObjectId] = parameters("zone_ids").split(",").map(_.trim).filter(_.nonEmpty)
+          .map(new ObjectId(_))
+      val badZoneOids = zoneOids.filterNot(ZoneApi.exists)
+      if (badZoneOids.nonEmpty)
+        throw new IllegalArgumentException(s"""Bad zone_ids '${badZoneOids.mkString(", ")}'""")
 
       val activityOid = new ObjectId(parameters("activity_id"))
       if (!ActivityApi.exists(activityOid))
@@ -27,16 +28,22 @@ class ZoneAddActivity extends HttpServlet with HttpUtils {
       val parentProcess = ActivityApi.parentProcess(activityOid)
       val parentPhase = ProcessApi.parentPhase(parentProcess._id[ObjectId])
 
-      if (parentPhase._id[ObjectId] != theZone.phase_id[ObjectId])
-        throw new IllegalArgumentException("Activity and zone in different phases")
+      val theZones = zoneOids.map(ZoneApi.zoneById)
 
-      val updateResult = BWMongoDB3.zones.updateOne(Map("_id" -> zoneOid),
+      if (theZones.exists(_.phase_id[ObjectId] != parentPhase._id[ObjectId]))
+        throw new IllegalArgumentException("Some zones and activity in different phases")
+
+      for (zoneOid <- zoneOids) {
+        val updateResult = BWMongoDB3.zones.updateOne(Map("_id" -> zoneOid),
           Map($addToSet -> Map("activity_ids" -> activityOid)))
-      if (updateResult.getMatchedCount == 0)
-        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+        if (updateResult.getMatchedCount == 0)
+          throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+      }
 
       response.setStatus(HttpServletResponse.SC_OK)
-      val message = s"Added activity '${theActivity.name[String]}' to zone '${theZone.name[String]}'"
+
+      val zoneNames = theZones.map(_.name[String]).mkString(", ")
+      val message = s"Added activity '${theActivity.name[String]}' to zones '$zoneNames'"
       BWLogger.audit(getClass.getName, request.getMethod, message, request)
     } catch {
       case t: Throwable =>

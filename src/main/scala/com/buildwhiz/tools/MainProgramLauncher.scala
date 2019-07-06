c329ec9e347a396ca8c3d2db032cbd91c3fd76e7
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-07-06 16:19:37 +0530

    Corrected try-catch nesting redundancy

--- src/main/scala/com/buildwhiz/tools/MainProgramLauncher.scala
+++ src/main/scala/com/buildwhiz/tools/MainProgramLauncher.scala
@@ -17,19 +17,17 @@ class MainProgramLauncher extends HttpServlet with HttpUtils {
       val args: Array[String] = if (parameters.contains("args")) parameters("args").split("\\s+") else Array.empty
       val clazz = Class.forName(s"""com.buildwhiz.tools.scripts.${parameters("program")}""")
       val mainMethod = clazz.getMethods.filter(_.getName == "main").map(m => (m, m.getParameterTypes.length)).head
-      try {
-        if (mainMethod._2 == 1)
-          clazz.getMethod("main", classOf[Array[String]]).invoke(null, args)
-        else if (mainMethod._2 == 2)
-          clazz.getMethod("main", classOf[HttpServletRequest], classOf[Array[String]]).
+      if (mainMethod._2 == 1)
+        clazz.getMethod("main", classOf[Array[String]]).invoke(null, args)
+      else if (mainMethod._2 == 2)
+        clazz.getMethod("main", classOf[HttpServletRequest], classOf[Array[String]]).
             invoke(null, request, args)
-        BWLogger.log(getClass.getName, "doPost/Get", s"EXIT-OK", request)
-      } catch {
-        case t: Throwable =>
-          BWLogger.log(getClass.getName, "doPost/Get", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
-          t.printStackTrace()
-          throw t
-      }
+      BWLogger.log(getClass.getName, "doPost/Get", s"EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "doPost/Get", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
     }
   }
 }

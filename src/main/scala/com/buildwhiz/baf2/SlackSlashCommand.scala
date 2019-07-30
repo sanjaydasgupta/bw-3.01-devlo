177bccf158eb362b015bd61c775813b232db9fb7
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-07-30 19:24:44 +0530

    SlackSlashCommand: Separated out event-callback functionality into its own class (SlackEventCallback)

--- src/main/scala/com/buildwhiz/baf2/SlackSlashCommand.scala
+++ src/main/scala/com/buildwhiz/baf2/SlackSlashCommand.scala
@@ -8,24 +8,30 @@ import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class SlackSlashCommand extends HttpServlet with HttpUtils with MailUtils with DateTimeUtils {
 
+  private def userBySlackId(slackUserId: String): Option[DynDoc] = {
+    BWMongoDB3.persons.find(Map("slack_id" -> slackUserId)).headOption
+  }
+
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
     val parameters = getParameterMap(request)
     try {
       val teamId = parameters("team_id")
       //val enterpriseId = parameters("enterprise_id")
-      val userId: String = parameters("user_id")
+      val slackUserId: String = parameters("user_id")
+      val slackUserName: String = parameters("user_name")
       val channelId = parameters("channel_id")
       val channelName = parameters("channel_name")
       val triggerId = parameters("trigger_id")
-      val responseUrl = parameters("response_url")
       val text = parameters("text")
-      BWMongoDB3.persons.find(Map("slack_id" -> userId)).headOption match {
+      userBySlackId(slackUserId) match {
         case Some(user: DynDoc) =>
           val userName = s"${user.first_name[String]} ${user.last_name[String]}"
-          response.getWriter.print(s"""{"text": "Hi $userName, your message is being processed"}""")
+          response.getWriter.print(s"""{"text": "Hi $slackUserName ($userName),""" +
+            """ your message is being processed"}""")
         case None =>
-          response.getWriter.print("""{"text": "Unknown user, your message was ignored"}""")
+          response.getWriter.print(s"""{"text": "Hi $slackUserName, your Slack-Id is not registered""" +
+            """ and your message was ignored"}""")
       }
       response.setContentType("application/json")
       BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)

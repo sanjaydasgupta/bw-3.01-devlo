800c53dbb4f59e3bcd3de7c9cf477c780006510a
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-08-02 18:03:17 +0530

    CommandLineProcessor: Skeleton code for several commands added

--- src/main/scala/com/buildwhiz/utils/CommandLineProcessor.scala
+++ src/main/scala/com/buildwhiz/utils/CommandLineProcessor.scala
@@ -1,64 +1,71 @@
 package com.buildwhiz.utils
 
+import com.buildwhiz.infra.DynDoc
+
 import scala.util.parsing.combinator.RegexParsers
 
 object CommandLineProcessor {
 
-  def process(command: String): String = {
-    commandLineParser.cliProcessor(command)
+  def process(command: String, user: DynDoc): String = {
+    commandLineParser.cliProcessor(command, user)
   }
 
-  private def help(): String = {
-    "Commands: list projects, list tasks, list documents"
+  private def help(user: DynDoc): String = {
+    """Commands:
+      |  list projects,
+      |  list tasks,
+      |  list documents
+      |  who am I""".stripMargin
   }
 
-  private def listDocuments(): String = {
+  private def listDocuments(user: DynDoc): String = {
     "You have no documents now"
   }
 
-  private def listProjects(): String = {
+  private def listProjects(user: DynDoc): String = {
     "You have no projects now"
   }
 
-  private def listTasks(): String = {
+  private def listTasks(user: DynDoc): String = {
     "You have no tasks now"
+  }
+
+  private def whoAmI(user: DynDoc): String = {
+    s"You are ${user.first_name[String]} ${user.last_name[String]}"
   }
 
   private object commandLineParser extends RegexParsers {
 
     // Type for CLI processor parsers ...
-    private type CLIP = Parser[String]
+    private type CLIP = Parser[DynDoc => String]
 
     // Help command ...
-    private lazy val helpCommand: CLIP = "(?i)help" ^^ {_ => help()}
+    private lazy val helpParser: CLIP = "(?i)help" ^^ {_ => help}
 
     // List projects command ...
-    private lazy val listDocumentsCommand: CLIP = "(?i)list".r ~ "(?i)documents".r ^^ {_ => listDocuments()}
+    private lazy val listDocumentsParser: CLIP = "(?i)list".r ~ "(?i)documents".r ^^ {_ => listDocuments}
 
     // List projects command ...
-    private lazy val listProjectsCommand: CLIP = "(?i)list".r ~ "(?i)projects".r ^^ {_ => listProjects()}
+    private lazy val listProjectsParser: CLIP = "(?i)list".r ~ "(?i)projects".r ^^ {_ => listProjects}
 
     // List tasks command ...
-    private lazy val listTasksCommand: CLIP = "(?i)list".r ~ "(?i)tasks".r ^^ {_ => listTasks()}
+    private lazy val listTasksParser: CLIP = "(?i)list".r ~ "(?i)tasks".r ^^ {_ => listTasks}
 
-    private lazy val none: CLIP = ".*".r ^^ {_ => help()}
+    // Who am I command ...
+    private lazy val whoAmIParser: CLIP = "(?i)who".r ~ "(?i)am".r ~ "(?i)I".r ^^ {_ => whoAmI}
 
-    private lazy val allCommands: CLIP = helpCommand | listDocumentsCommand | listProjectsCommand | listTasksCommand | none
+    private lazy val none: CLIP = ".*".r ^^ {_ => help}
 
-    def cliProcessor(command: String): String = {
+    private lazy val allCommands: CLIP = helpParser | listDocumentsParser | listProjectsParser | listTasksParser |
+        whoAmIParser | none
+
+    def cliProcessor(command: String, user: DynDoc): String = {
       parseAll(allCommands, command) match {
-        case Success(result, _) => result
+        case Success(result, _) => result(user)
         case NoSuccess(result, _) => result
       }
     }
 
   }
 
-  def main(args: Array[String]): Unit = {
-    println(process("list documents"))
-    println(process("list projects"))
-    println(process("list tasks"))
-    println(process("list tags"))
-    println(process("foo bar"))
-  }
 }

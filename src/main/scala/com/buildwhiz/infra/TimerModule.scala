681a09cbcad1ae0eca6155d965ae90c89e09fba3
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2019-11-01 20:51:58 +0530

    TimerModule: corrected code indentation

--- src/main/scala/com/buildwhiz/infra/TimerModule.scala
+++ src/main/scala/com/buildwhiz/infra/TimerModule.scala
@@ -117,28 +117,28 @@ object TimerModule extends HttpUtils {
     }
   }
 
-    private def processHealthCheck(ms: Long): Unit = {
-      try {
-        val allProcesses = ProcessApi.listProcesses()
-        val nonZombies = allProcesses.filterNot(ProcessApi.isZombie)
-        val newZombies = nonZombies.filterNot(ProcessApi.isHealthy)
-        for (zp <- newZombies) {
-          BWMongoDB3.processes.updateOne(Map("_id" -> zp._id[ObjectId]), Map($set -> Map("is_zombie" -> true)))
-          val allActivityOids: Seq[ObjectId] = zp.activity_ids[Many[ObjectId]]
-          BWMongoDB3.activities.updateOne(Map("_id" -> Map($in -> allActivityOids)), Map($set -> Map("is_zombie" -> true)))
-          val phase = ProcessApi.parentPhase(zp._id[ObjectId])
-          val project = PhaseApi.parentProject(phase._id[ObjectId])
-          val processIdentity = s"${project.name[String]}/${phase.name[String]}/${zp.name[String]}"
-          for (admin <- PersonApi.listAdmins) {
-            SlackApi.sendToUser(s"Process killed: $processIdentity", Left(admin))
-          }
-          BWLogger.log(classOf[TimerTask].getSimpleName, "processHealthCheck", s"INFO: Process killed: $processIdentity")
+  private def processHealthCheck(ms: Long): Unit = {
+    try {
+      val allProcesses = ProcessApi.listProcesses()
+      val nonZombies = allProcesses.filterNot(ProcessApi.isZombie)
+      val newZombies = nonZombies.filterNot(ProcessApi.isHealthy)
+      for (zp <- newZombies) {
+        BWMongoDB3.processes.updateOne(Map("_id" -> zp._id[ObjectId]), Map($set -> Map("is_zombie" -> true)))
+        val allActivityOids: Seq[ObjectId] = zp.activity_ids[Many[ObjectId]]
+        BWMongoDB3.activities.updateOne(Map("_id" -> Map($in -> allActivityOids)), Map($set -> Map("is_zombie" -> true)))
+        val phase = ProcessApi.parentPhase(zp._id[ObjectId])
+        val project = PhaseApi.parentProject(phase._id[ObjectId])
+        val processIdentity = s"${project.name[String]}/${phase.name[String]}/${zp.name[String]}"
+        for (admin <- PersonApi.listAdmins) {
+          SlackApi.sendToUser(s"Process killed: $processIdentity", Left(admin))
         }
-      } catch {
-        case t: Throwable =>
-          BWLogger.log(classOf[TimerTask].getSimpleName, "processHealthCheck",
-            s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})")
+        BWLogger.log(classOf[TimerTask].getSimpleName, "processHealthCheck", s"INFO: Process killed: $processIdentity")
       }
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(classOf[TimerTask].getSimpleName, "processHealthCheck",
+          s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})")
+    }
   }
 
   private def fifteenMinutes(ms: Long): Unit = {

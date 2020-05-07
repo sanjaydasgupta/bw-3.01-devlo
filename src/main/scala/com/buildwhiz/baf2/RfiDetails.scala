5ee4162d3328ee29506928af0cae680c7c027b8f
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-05-07 22:00:43 +0530

    RfiDetails: added common service access point for all user access modes (web, slack, ...)

--- src/main/scala/com/buildwhiz/baf2/RfiDetails.scala
+++ src/main/scala/com/buildwhiz/baf2/RfiDetails.scala
@@ -10,38 +10,14 @@ import org.bson.types.ObjectId
 
 class RfiDetails extends HttpServlet with HttpUtils with MailUtils with DateTimeUtils {
 
-  private def getAttachments(message: DynDoc): Seq[Document] = {
-    val attachments: Seq[DynDoc] = if (message.has("attachments")) message.attachments[Many[Document]] else Nil
-    for (attachment <- attachments) {
-      val fileName = attachment.file_name[String]
-      val docOid = attachment.document_id[ObjectId]
-      val ts = attachment.timestamp[Long]
-      attachment.link = s"baf/DocumentVersionDownload/$fileName?document_master_id=$docOid&timestamp=$ts"
-    }
-    attachments.map(_.asDoc)
-  }
-
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
     try {
-      val rfiOid = new ObjectId(parameters("rfi_id"))
-      val rfiExchange: DynDoc = RfiApi.rfiById(rfiOid)
-      val messages: Seq[DynDoc] = rfiExchange.messages[Many[Document]]
+      val rfiId = parameters("rfi_id")
       val user: DynDoc = getUser(request)
-      val messageLines: Seq[Document] = messages.sortBy(m => -m.timestamp[Long]).map(message => {
-        val sender: DynDoc = BWMongoDB3.persons.find(Map("_id" -> message.sender[ObjectId])).head
-        val senderName = PersonApi.fullName(sender)
-        val clientTimezone = user.tz[String]
-        new Document(Map("timestamp" -> dateTimeString(message.timestamp[Long], Some(clientTimezone)),
-          "text" -> (if (message.text[Any] == null) "" else message.text[String]),
-          "sender" -> senderName, "attachments" -> getAttachments(message)))
-      })
-      for (idx <- messages.indices) {
-        BWMongoDB3.rfi_messages.updateOne(Map("_id" -> rfiOid),
-          Map("$addToSet" -> Map(s"messages.$idx.read_person_ids" -> user._id[ObjectId])))
-      }
-      val results = messageLines.map(bson2json).mkString("[", ", ", "]")
+      val messageLines = RfiDetails.getMessages(rfiId, user, request)
+      val results = messageLines.map(dd => bson2json(dd.asDoc)).mkString("[", ", ", "]")
       response.getWriter.print(results)
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
@@ -53,4 +29,38 @@ class RfiDetails extends HttpServlet with HttpUtils with MailUtils with DateTime
         throw t
     }
   }
+}
+
+object RfiDetails extends HttpUtils with DateTimeUtils {
+
+  private def getAttachments(message: DynDoc): Seq[Document] = {
+    val attachments: Seq[DynDoc] = if (message.has("attachments")) message.attachments[Many[Document]] else Nil
+    for (attachment <- attachments) {
+      val fileName = attachment.file_name[String]
+      val docOid = attachment.document_id[ObjectId]
+      val ts = attachment.timestamp[Long]
+      attachment.link = s"baf/DocumentVersionDownload/$fileName?document_master_id=$docOid&timestamp=$ts"
+    }
+    attachments.map(_.asDoc)
+  }
+
+  def getMessages(rfiId: String, user: DynDoc, request: HttpServletRequest, doLog: Boolean = false): Seq[DynDoc] = {
+    val rfiOid = new ObjectId(rfiId)
+    val rfiExchange: DynDoc = RfiApi.rfiById(rfiOid)
+    val messages: Seq[DynDoc] = rfiExchange.messages[Many[Document]]
+    val messageLines: Seq[DynDoc] = messages.sortBy(m => -m.timestamp[Long]).map(message => {
+      val sender: DynDoc = BWMongoDB3.persons.find(Map("_id" -> message.sender[ObjectId])).head
+      val senderName = PersonApi.fullName(sender)
+      val clientTimezone = user.tz[String]
+      Map("timestamp" -> dateTimeString(message.timestamp[Long], Some(clientTimezone)),
+        "text" -> (if (message.text[Any] == null) "" else message.text[String]),
+        "sender" -> senderName, "attachments" -> getAttachments(message))
+    })
+    for (idx <- messages.indices) {
+      BWMongoDB3.rfi_messages.updateOne(Map("_id" -> rfiOid),
+        Map("$addToSet" -> Map(s"messages.$idx.read_person_ids" -> user._id[ObjectId])))
+    }
+    messageLines
+  }
+
 }

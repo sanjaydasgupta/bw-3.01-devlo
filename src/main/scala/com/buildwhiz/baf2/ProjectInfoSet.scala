b3f85d69fcca3ae4400a5a265a7dbedf55c36c64
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-05-11 18:39:17 +0530

    ProjectInfoSet: added common service access point for all user access modes (web, slack, ...)

--- src/main/scala/com/buildwhiz/baf2/ProjectInfoSet.scala
+++ src/main/scala/com/buildwhiz/baf2/ProjectInfoSet.scala
@@ -8,47 +8,19 @@ import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.Document
 import org.bson.types.ObjectId
 
+import scala.collection.JavaConverters._
+
 class ProjectInfoSet extends HttpServlet with HttpUtils {
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
 
-    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
     try {
-      val fullNames: Map[String, String] = Map(
-        ("name", "name"), ("description", "description"),
-        ("state_name", "address.state.name"), ("country_name", "address.country.name"),
-        ("gps_latitude", "address.gps_location.latitude"), ("gps_longitude", "address.gps_location.longitude"),
-        ("address_line1", "address.line1"), ("address_line2", "address.line2"), ("address_line3", "address.line3"),
-        ("postal_code", "address.postal_code"),
-        ("construction_type", "construction_type"), ("type", "type"), ("budget_mm_usd", "budget_mm_usd"),
-        ("construction_area_sqft", "construction_area_sqft"), ("land_area_acres", "land_area_acres"),
-        ("max_building_height_ft", "max_building_height_ft"), ("project_id", "project_id"))
-      val parameterNames = fullNames.keys.toSeq
       val postData = Document.parse(getStreamData(request))
-      val unknownParameters = postData.keySet.toArray.filterNot(parameterNames.contains)
-      if (unknownParameters.nonEmpty)
-        throw new IllegalArgumentException(s"""Unknown parameter(s): ${unknownParameters.mkString(", ")}""")
       if (!postData.containsKey("project_id"))
         throw new IllegalArgumentException("project_id not provided")
-      val projectOid = new ObjectId(postData.remove("project_id").asInstanceOf[String])
-      val mongoDbNameValuePairs = parameterNames.filter(postData.containsKey).
-          map(paramName => (fullNames(paramName), postData.getString(paramName)))
-      if (mongoDbNameValuePairs.isEmpty)
-        throw new IllegalArgumentException("No parameters found")
-      if (mongoDbNameValuePairs.exists(_._1 == "name")) {
-        val name = mongoDbNameValuePairs.find(_._1 == "name").get._2.asInstanceOf[String]
-        ProjectApi.validateNewName(name)
-      }
-      val updateResult = BWMongoDB3.projects.updateOne(Map("_id" -> projectOid),
-          Map("$set" -> mongoDbNameValuePairs.toMap))
-      if (updateResult.getMatchedCount == 0)
-        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
-      response.setStatus(HttpServletResponse.SC_OK)
-      val parametersChanged = mongoDbNameValuePairs.map(_._1).mkString("[", ", ", "]")
-      val message = s"""Updated parameters $parametersChanged of project $projectOid"""
-      val managers = ProjectApi.managers(Left(projectOid))
-      for (manager <- managers) {
-        SlackApi.sendNotification(message, Right(manager), Some(request))
-      }
+      val projectId = postData.remove("project_id").asInstanceOf[String]
+      val nameValuePairs = postData.entrySet.asScala.map(es => (es.getKey, es.getValue.asInstanceOf[String])).toSeq
+      val message = ProjectInfoSet.setProjectFields(projectId, nameValuePairs, request)
       BWLogger.audit(getClass.getName, request.getMethod, message, request)
     } catch {
       case t: Throwable =>
@@ -56,6 +28,51 @@ class ProjectInfoSet extends HttpServlet with HttpUtils {
         //t.printStackTrace()
         throw t
     }
+  }
+
+}
+
+object ProjectInfoSet {
+
+  private val fullNames: Map[String, String] = Map(
+    ("name", "name"), ("description", "description"),
+    ("state_name", "address.state.name"), ("country_name", "address.country.name"),
+    ("gps_latitude", "address.gps_location.latitude"), ("gps_longitude", "address.gps_location.longitude"),
+    ("address_line1", "address.line1"), ("address_line2", "address.line2"), ("address_line3", "address.line3"),
+    ("postal_code", "address.postal_code"),
+    ("construction_type", "construction_type"), ("type", "type"), ("budget_mm_usd", "budget_mm_usd"),
+    ("construction_area_sqft", "construction_area_sqft"), ("land_area_acres", "land_area_acres"),
+    ("max_building_height_ft", "max_building_height_ft"), ("project_id", "project_id")
+  )
+
+  def setProjectFields(projectId: String, nameValuePairs: Seq[(String, String)], request: HttpServletRequest,
+      doLog: Boolean = false): String = {
+    if (doLog)
+      BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
+    if (nameValuePairs.isEmpty)
+      throw new IllegalArgumentException("No parameters found")
+    val unknownParameters = nameValuePairs.map(_._1).filterNot(fullNames.containsKey)
+    if (unknownParameters.nonEmpty)
+      throw new IllegalArgumentException(s"""Unknown parameter(s): ${unknownParameters.mkString(", ")}""")
+    nameValuePairs.find(_._1 == "name") match {
+      case Some((_, name)) =>
+        ProjectApi.validateNewName(name)
+      case None =>
+    }
+    val projectOid = new ObjectId(projectId)
+    val mongoDbSetters = nameValuePairs.map(p => (fullNames(p._1), p._2)).toMap
+    val updateResult = BWMongoDB3.projects.updateOne(Map("_id" -> projectOid), Map("$set" -> mongoDbSetters))
+    if (updateResult.getMatchedCount == 0)
+      throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+    val parametersChanged = nameValuePairs.map(_._1).mkString("[", ", ", "]")
+    val message = s"""Updated parameters $parametersChanged of project $projectOid"""
+    val managers = ProjectApi.managers(Left(projectOid))
+    for (manager <- managers) {
+      SlackApi.sendNotification(message, Right(manager), Some(request))
+    }
+    if (doLog)
+      BWLogger.audit(getClass.getName, request.getMethod, message, request)
+    message
   }
 
 }

86cb0a8fd46fd7119e572b3e08143c0858f9cf59
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-05-24 09:03:55 +0530

    BrowseFiles: enabled usual logging

--- src/main/scala/com/buildwhiz/web/BrowseFiles.scala
+++ src/main/scala/com/buildwhiz/web/BrowseFiles.scala
@@ -3,13 +3,14 @@ package com.buildwhiz.web
 import java.io.{File, FileInputStream}
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
-import com.buildwhiz.utils.{DateTimeUtils, HttpUtils}
+import com.buildwhiz.utils.{BWLogger, DateTimeUtils, HttpUtils}
 
 import scala.sys.process._
 
 class BrowseFiles extends HttpServlet with HttpUtils with DateTimeUtils {
 
   private def browseDirectory(directory: File, request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, "browseDirectory", "ENTRY", request)
     val writer = response.getWriter
     writer.println("<html><head><title>Browse Directory</title></head><body>")
     writer.println(s"""<h3 align="center">${directory.getCanonicalPath}</h3>""")
@@ -27,7 +28,7 @@ class BrowseFiles extends HttpServlet with HttpUtils with DateTimeUtils {
         s"""<a href="$url/${file.getName}?location=$linkedName">${file.getName}</a>"""
       val dirColor = if (file.isDirectory) "bgcolor=\"yellow\"" else ""
       val dirFlag = if (file.isDirectory) "Y" else ""
-      val length = if (file.isDirectory) "-" else by3(file.length).toString
+      val length = if (file.isDirectory) "-" else by3(file.length)
       val tz = getUser(request).get("tz").asInstanceOf[String]
       val lastModified = dateTimeString(file.lastModified(), Some(tz))
       writer.println(
@@ -41,6 +42,7 @@ class BrowseFiles extends HttpServlet with HttpUtils with DateTimeUtils {
     writer.println(s"""<h3 align="center">$zipLink</h3>""")
     writer.println(s"</body></html>")
     response.setContentType("text/html")
+    BWLogger.log(getClass.getName, "browseDirectory", "EXIT-OK", request)
   }
 
   private def downloadFile(file: File, response: HttpServletResponse): Unit = {
@@ -50,6 +52,7 @@ class BrowseFiles extends HttpServlet with HttpUtils with DateTimeUtils {
     } else {
       val fis = new FileInputStream(file)
       val buffer = new Array[Byte](4096)
+      @scala.annotation.tailrec
       def copy(): Unit = {
         val length = fis.read(buffer)
         if (length == buffer.length) {
@@ -95,8 +98,8 @@ class BrowseFiles extends HttpServlet with HttpUtils with DateTimeUtils {
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
     val parameters = getParameterMap(request)
-    //BWLogger.log(getClass.getName, "doGet()", s"ENTRY", request)
     try {
       val location = parameters.get("location")
       location match {
@@ -105,6 +108,7 @@ class BrowseFiles extends HttpServlet with HttpUtils with DateTimeUtils {
           serverDir.listFiles().find(_.getName.startsWith("apache-tomcat-")) match {
             case Some(tomcatDir) => browseDirectory(new File(tomcatDir, "logs"), request, response)
             case None =>
+              BWLogger.log(getClass.getName, request.getMethod, "EXIT-ERROR (No 'apache-tomcat' directory)", request)
           }
         case Some(fn) => val file = new File(fn).getCanonicalFile
           if (file.isDirectory) {
@@ -116,10 +120,10 @@ class BrowseFiles extends HttpServlet with HttpUtils with DateTimeUtils {
           }
       }
       response.setStatus(HttpServletResponse.SC_OK)
-      //BWLogger.log(getClass.getName, "doGet()", s"EXIT-OK", request)
+      BWLogger.log(getClass.getName, request.getMethod, "EXIT-OK", request)
     } catch {
       case t: Throwable =>
-        //BWLogger.log(getClass.getName, "doGet()", s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
         t.printStackTrace()
         throw t
     }

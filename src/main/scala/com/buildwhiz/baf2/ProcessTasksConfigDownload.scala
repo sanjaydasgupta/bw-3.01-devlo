0fed84acaecddd8131fe72100c01d178312fa3b9
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-06-25 16:57:09 +0530

    ProcessTasksConfigDownload: minor changes in Excel presentation, code refactored to improve clarity and reuse

--- src/main/scala/com/buildwhiz/baf2/ProcessTasksConfigDownload.scala
+++ src/main/scala/com/buildwhiz/baf2/ProcessTasksConfigDownload.scala
@@ -5,8 +5,8 @@ import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
-import org.apache.poi.ss.usermodel.{FillPatternType, HorizontalAlignment, IndexedColors, VerticalAlignment}
-import org.apache.poi.xssf.usermodel.{XSSFSheet, XSSFWorkbook}
+import org.apache.poi.ss.usermodel.{BorderStyle, FillPatternType, HorizontalAlignment, IndexedColors, VerticalAlignment}
+import org.apache.poi.xssf.usermodel.{XSSFCellStyle, XSSFSheet, XSSFWorkbook}
 import org.bson.Document
 import org.bson.types.ObjectId
 
@@ -14,30 +14,39 @@ import scala.collection.JavaConverters._
 
 class ProcessTasksConfigDownload extends HttpServlet with HttpUtils {
 
-  private def makeHeaderRow(taskSheet: XSSFSheet, headerInfo: Seq[(String, Int)]): Unit = {
+  def getCellStyle(workbook: XSSFWorkbook, colorIndex: Short): XSSFCellStyle = {
+    val cellStyle = workbook.createCellStyle()
+    cellStyle.setAlignment(HorizontalAlignment.CENTER)
+    cellStyle.setVerticalAlignment(VerticalAlignment.CENTER)
+    cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
+    cellStyle.setFillForegroundColor(colorIndex)
+    cellStyle.setBorderRight(BorderStyle.THIN)
+    cellStyle.setRightBorderColor(IndexedColors.GREY_40_PERCENT.index)
+    cellStyle.setBorderLeft(BorderStyle.THIN)
+    cellStyle.setLeftBorderColor(IndexedColors.GREY_40_PERCENT.index)
+    cellStyle
+  }
+
+  private def addHeaderRow(taskSheet: XSSFSheet, headerInfo: Seq[(String, Int)]): Unit = {
     val headerRow = taskSheet.createRow(0)
-    //headerRow.setRowStyle(cellStyle)
     val rowHeight = if (headerInfo.exists(_._1.contains("\n"))) 36 else 18
     headerRow.setHeightInPoints(rowHeight)
     for (hdrInfo <- headerInfo.zipWithIndex) {
       val cell = headerRow.createCell(hdrInfo._2)
       cell.setCellValue(hdrInfo._1._1)
       cell.getSheet.setColumnWidth(hdrInfo._2, hdrInfo._1._2 * 125)
-      val cellStyle = taskSheet.getWorkbook.createCellStyle()
-      cellStyle.setAlignment(HorizontalAlignment.CENTER)
-      cellStyle.setVerticalAlignment(VerticalAlignment.CENTER)
+      val cellStyle = getCellStyle(taskSheet.getWorkbook, IndexedColors.CORAL.index)
       val cellFont = cellStyle.getFont
       cellFont.setBold(true)
       cellStyle.setFont(cellFont)
-      cellStyle.setFillForegroundColor(IndexedColors.CORAL.index)
-      cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
       //cellStyle.setWrapText(true)
-      //cellStyle.setLocked(true)
+      cellStyle.setLocked(true)
       cell.setCellStyle(cellStyle)
     }
   }
 
-  private def addTasksSheet(workbook: XSSFWorkbook, process: DynDoc, bpmnName: String): Int = {
+  private def addTasksConfigSheet(workbook: XSSFWorkbook, process: DynDoc, bpmnName: String): Int = {
+
     def addTaskRow(taskSheet: XSSFSheet, taskName: String): Unit = {
       val row = taskSheet.createRow(taskSheet.getLastRowNum + 1)
       row.createCell(0).setCellValue(taskName)
@@ -47,20 +56,14 @@ class ProcessTasksConfigDownload extends HttpServlet with HttpUtils {
       row.createCell(4).setCellValue("--") // constraint
       row.createCell(5).setCellValue("--") // duration
       row.createCell(6).setCellValue("--") // constraint
-      val cellStyle = taskSheet.getWorkbook.createCellStyle()
-      cellStyle.setAlignment(HorizontalAlignment.CENTER)
-      cellStyle.setVerticalAlignment(VerticalAlignment.CENTER)
-      cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
-      cellStyle.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.index)
+      val cellStyle = getCellStyle(workbook, IndexedColors.GREY_40_PERCENT.index)
+      cellStyle.setLocked(true)
       row.cellIterator().forEachRemaining(_.setCellStyle(cellStyle))
     }
+
     def addDeliverableRow(taskSheet: XSSFSheet, deliverables: Seq[DynDoc]): Unit = {
-      def addConstraintRow(taskSheet: XSSFSheet, offset: String, duration: String): Unit = {
-        val constraintCellStyle = taskSheet.getWorkbook.createCellStyle()
-        constraintCellStyle.setAlignment(HorizontalAlignment.CENTER)
-        constraintCellStyle.setVerticalAlignment(VerticalAlignment.CENTER)
-        constraintCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
-        constraintCellStyle.setFillForegroundColor(IndexedColors.SKY_BLUE.index)
+
+      def addConstraintRow(offset: String, duration: String): Unit = {
         val row = taskSheet.createRow(taskSheet.getLastRowNum + 1)
         row.createCell(0).setCellValue("--") // task
         row.createCell(1).setCellValue("--") // deliverable
@@ -69,13 +72,11 @@ class ProcessTasksConfigDownload extends HttpServlet with HttpUtils {
         row.createCell(4).setCellValue("bpmn:task:deliverable") // constraint
         row.createCell(5).setCellValue(offset) // offset
         row.createCell(6).setCellValue(duration) // duration
+        val constraintCellStyle = getCellStyle(taskSheet.getWorkbook, IndexedColors.LIGHT_CORNFLOWER_BLUE.index)
         row.cellIterator().forEachRemaining(_.setCellStyle(constraintCellStyle))
       }
-      val deliverableCellStyle = taskSheet.getWorkbook.createCellStyle()
-      deliverableCellStyle.setAlignment(HorizontalAlignment.CENTER)
-      deliverableCellStyle.setVerticalAlignment(VerticalAlignment.CENTER)
-      deliverableCellStyle.setFillForegroundColor(IndexedColors.ROYAL_BLUE.index)
-      deliverableCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
+
+      val deliverableCellStyle = getCellStyle(taskSheet.getWorkbook, IndexedColors.SKY_BLUE.index)
       for (deliverable <- deliverables) {
         val row = taskSheet.createRow(taskSheet.getLastRowNum + 1)
         row.createCell(0).setCellValue("--")
@@ -87,22 +88,22 @@ class ProcessTasksConfigDownload extends HttpServlet with HttpUtils {
         row.createCell(5).setCellValue("--") // offset
         row.createCell(6).setCellValue("--") // duration
         row.cellIterator().forEachRemaining(_.setCellStyle(deliverableCellStyle))
-        addConstraintRow(taskSheet, "10", "5")
+        addConstraintRow("10", "5")
       }
     }
+
     def createDummyDeliverable(n: Int): Document = {
       val types = Seq(("Labor", 20, 5), ("Material", 10, 0), ("Equipment", 5, 10), ("Work", 30, 0))
-      val constraints: Many[String] = Seq("task:deliverable", "bpmn:task:deliverable", "task:deliverable",
-        "bpmn:task:deliverable").asJava
       new Document("name", s"sample-deliverable-$n").append("type", types(n - 1)._1).append("duration", types(n - 1)._3)
     }
+
     val activityIds: Seq[ObjectId] = process.activity_ids[Many[ObjectId]]
     val activities: Seq[DynDoc] = BWMongoDB3.activities.find(Map("_id" -> Map("$in" -> activityIds),
       "bpmn_name" -> bpmnName))
     val taskSheet = workbook.createSheet(process._id[ObjectId].toString)
-    val headerInfo = Seq(("Task", 60), ("Deliverable", 60), ("Type", 20), ("Duration", 20),
-      ("Constraint", 60), ("Offset", 20), ("Duration", 20))
-    makeHeaderRow(taskSheet, headerInfo)
+    val headerInfo = Seq(("Task", 60), ("Deliverable", 60), ("Type", 20), ("Duration\n(days)", 20),
+      ("Constraint", 60), ("Offset\n(days)", 20), ("Duration\n(days)", 20))
+    addHeaderRow(taskSheet, headerInfo)
     for (activity <- activities) {
       addTaskRow(taskSheet, activity.name[String])
       if (activity.has("deliverables")) {
@@ -126,7 +127,7 @@ class ProcessTasksConfigDownload extends HttpServlet with HttpUtils {
       val bpmnName = parameters("bpmn_name")
       val workbook = new XSSFWorkbook()
       val process: DynDoc = ProcessApi.processById(processOid)
-      val taskNbr = addTasksSheet(workbook, process, bpmnName)
+      val taskNbr = addTasksConfigSheet(workbook, process, bpmnName)
       workbook.write(response.getOutputStream)
       response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
       response.setStatus(HttpServletResponse.SC_OK)

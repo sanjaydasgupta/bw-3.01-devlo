a018d1b1a85b61079fe452fdcc71fbd6f1090a7f
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-06-27 17:52:44 +0530

    ProcessTasksConfigUpload: completed Excel file interpreter - also validates task-, deliverable- and constraint- rows

--- src/main/scala/com/buildwhiz/baf2/ProcessTasksConfigUpload.scala
+++ src/main/scala/com/buildwhiz/baf2/ProcessTasksConfigUpload.scala
@@ -5,7 +5,7 @@ import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.utils.{BWLogger, HttpUtils, MailUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
-import org.apache.poi.ss.usermodel.{Row, Sheet}
+import org.apache.poi.ss.usermodel.{Cell, CellType, Row, Sheet}
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.bson.Document
 import org.bson.types.ObjectId
@@ -15,44 +15,72 @@ import scala.util.{Failure, Success, Try}
 
 class ProcessTasksConfigUpload extends HttpServlet with HttpUtils with MailUtils {
 
-  private def processTasks(request: HttpServletRequest, response: HttpServletResponse, taskSheet: Sheet,
-      bpmnName: String): String = {
+  case class Constraint(bpmn: Option[String], task: Option[String], deliverable: String, offset: Int, duration: Int)
+  case class Deliverable(name: String, `type`: String, duration: Int, constraints: Seq[Constraint])
+  case class Task(name: String, deliverables: Seq[Deliverable])
 
-    def setTask(name: String, description: String, typ: String, activityId: String, duration: String,
-        assignee: String, role: String): Unit = {
-      val activityOid = new ObjectId(activityId)
-      val theActivity: DynDoc = ActivityApi.activityById(activityOid)
-      val actions: Seq[DynDoc] = ActivityApi.allActions(theActivity)
-      actions.find(_.name[String] == name) match {
-        case None =>
-          ActionAdd.add(request, activityOid, name, description, typ, bpmnName, new ObjectId(assignee), duration,
-            if (role == "???") None else Some(role))
-        case Some(action) =>
-          if (action.status[String] != "defined") {
-            throw new IllegalArgumentException(s"wrong status: '${action.status[String]}'")
-          }
-          if (typ == "#")
-            ActionDelete.delete(request, activityOid, name)
-          else
-            ActionDurationSet.set(request, activityOid, name, duration, Some(description))
+  private def consumeOneConfigRow(configTasks: Seq[Task], configRow:Row): Seq[Task] = {
+    val rowNumber = configRow.getRowNum + 1
+    def getCellValue(cell: Cell): String = {
+      cell.getCellType match {
+        case CellType.NUMERIC => cell.getNumericCellValue.toString.replaceAll("\\.0+", "")
+        case CellType.STRING => cell.getStringCellValue
+        case cellType =>
+          throw new IllegalArgumentException(s"Bad cell type, found '$cellType' in row $rowNumber")
       }
     }
+    val cellValues: Seq[Option[String]] = configRow.asScala.toSeq.map(getCellValue).map(_.replaceAll("[\\s-]+", "")).
+        map(s => if (s.isEmpty) None else Some(s))
+    if (cellValues.length != 7)
+      throw new IllegalArgumentException(s"Bad row - expected 7 cells, found ${cellValues.length} in row $rowNumber")
+    cellValues match {
+      // Task row
+      case Seq(Some(taskName), None, None, None, None, None, None) =>
+        val newTask = Task(taskName, Seq.empty[Deliverable])
+        newTask +: configTasks
+      // Deliverable row
+      case Seq(None, Some(deliverableName), Some(deliverableType), Some(duration), None, None, None) =>
+        if (!deliverableType.matches("(?i)equipment|labor|material|work"))
+          throw new IllegalArgumentException(s"Bad deliverable type, found '$deliverableType' in row $rowNumber")
+        if (!duration.matches("[0-9]+"))
+          throw new IllegalArgumentException(s"Bad duration value, found '$duration' in row $rowNumber")
+        val newDeliverable = Deliverable(deliverableName, deliverableType.toLowerCase, duration.toInt,
+            Seq.empty[Constraint])
+        val currentTask = configTasks.head
+        val updatedTask = currentTask.copy(deliverables = newDeliverable +: currentTask.deliverables)
+        updatedTask +: configTasks.tail
+      // Constraint row
+      case Seq(None, None, None, None, Some(constraintSpec), Some(offset), Some(duration)) =>
+        if (!offset.matches("[0-9]+"))
+          throw new IllegalArgumentException(s"Bad offset value, found '$offset' in row $rowNumber")
+        if (!duration.matches("[0-9]+"))
+          throw new IllegalArgumentException(s"Bad duration value, found '$duration' in row $rowNumber")
+        val newConstraint = constraintSpec.split(":").toSeq.map(_.trim) match {
+          case Seq(bpmn, task, deliverable) => Constraint(Some(bpmn), Some(task), deliverable, offset.toInt, duration.toInt)
+          case Seq(task, deliverable) => Constraint(None, Some(task), deliverable, offset.toInt, duration.toInt)
+          case Seq(deliverable) => Constraint(None, None, deliverable, offset.toInt, duration.toInt)
+          case _ => throw new IllegalArgumentException(s"Bad constraint, found '$constraintSpec' in row $rowNumber")
+        }
+        val currentTask = configTasks.head
+        val currentDeliverable = currentTask.deliverables.head
+        val updatedDeliverable = currentDeliverable.copy(constraints = newConstraint +: currentDeliverable.constraints)
+        val updatedTask = currentTask.copy(deliverables = updatedDeliverable +: currentTask.deliverables.tail)
+        updatedTask +: configTasks.tail
+      // ERROR row
+      case _ =>
+        throw new IllegalArgumentException(s"Bad values in row $rowNumber")
+    }
+  }
 
-    BWLogger.log(getClass.getName, "processTasks", "ENTRY", request)
-    val rows: Iterator[Row] = taskSheet.rowIterator.asScala
-    val header = rows.take(1).toSeq.head
+  private def processTasksConfig(request: HttpServletRequest, response: HttpServletResponse, taskSheet: Sheet,
+      bpmnName: String): String = {
+    BWLogger.log(getClass.getName, "processTasksConfig", "ENTRY", request)
+    val configInfoIterator: Iterator[Row] = taskSheet.rowIterator.asScala
+    val header = configInfoIterator.take(1).next()
     val taskHeaderCellCount = header.getPhysicalNumberOfCells
     if (taskHeaderCellCount != 7)
-      throw new IllegalArgumentException(s"unexpected task header cell count ($taskHeaderCellCount)")
-    for (row <- rows) {
-      row.cellIterator.asScala.toSeq.map(_.getStringCellValue) match {
-        case Seq(name, typ, activityId, duration, assignee, role, description) =>
-          setTask(name, description, typ, activityId, duration, assignee, role)
-        case _ =>
-          val message = s"unexpected task cell count (${row.getPhysicalNumberOfCells}) in row ${row.getRowNum + 1}"
-          throw new IllegalArgumentException(message)
-      }
-    }
+      throw new IllegalArgumentException(s"Bad header - expected 7 cells, found $taskHeaderCellCount")
+    val taskConfigurations: Seq[Task] = configInfoIterator.toSeq.foldLeft(Seq.empty[Task])(consumeOneConfigRow)
     s"${taskSheet.getPhysicalNumberOfRows -1} task(s)"
   }
 
@@ -72,12 +100,12 @@ class ProcessTasksConfigUpload extends HttpServlet with HttpUtils with MailUtils
       val workbook = new XSSFWorkbook(parts.asScala.head.getInputStream)
       val nbrOfSheets = workbook.getNumberOfSheets
       if (nbrOfSheets != 1)
-        throw new IllegalArgumentException(s"unexpected number of sheets: $nbrOfSheets")
+        throw new IllegalArgumentException(s"bad sheet count - expected 1, got $nbrOfSheets")
       val theSheet: Sheet = workbook.sheetIterator.next()
       if (theSheet.getSheetName != processOid.toString)
         throw new IllegalArgumentException(s"Mismatched process_id ($processOid != ${theSheet.getSheetName})")
-      val msg = "" //processTasks(request, response, theSheet, bpmnName)
-      BWLogger.audit(getClass.getName, "doPost", s"Uploaded process configuration Excel ($nbrOfSheets sheets: $msg)", request)
+      val msg = processTasksConfig(request, response, theSheet, bpmnName)
+      BWLogger.audit(getClass.getName, "doPost", s"Uploaded process configuration Excel ($msg)", request)
       response.getWriter.print(s"""{"nbrOfSheets": "$nbrOfSheets", "items": "$msg"}""")
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)

207379663d1dece638ef10187798f294298087e3
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-06-30 14:11:13 +0530

    ProcessTasksConfigUpload: completed task-configuration storage in mongodb activities collection

--- src/main/scala/com/buildwhiz/baf2/ProcessTasksConfigUpload.scala
+++ src/main/scala/com/buildwhiz/baf2/ProcessTasksConfigUpload.scala
@@ -1,8 +1,8 @@
 package com.buildwhiz.baf2
 
-import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.utils.{BWLogger, HttpUtils, MailUtils}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.apache.poi.ss.usermodel.{Cell, CellType, Row, Sheet}
@@ -14,7 +14,15 @@ import scala.collection.JavaConverters._
 
 class ProcessTasksConfigUpload extends HttpServlet with HttpUtils with MailUtils {
 
-  private def storeTaskConfigurations(taskConfigurations: Seq[DynDoc], processOid: ObjectId): Unit = {
+  private def storeTaskConfigurations(taskConfigurations: Seq[DynDoc]): Unit = {
+    for (taskConfig <- taskConfigurations) {
+      val activityOid = taskConfig._id[ObjectId]
+      val deliverables = taskConfig.deliverables[Seq[DynDoc]]
+      val updateResult = BWMongoDB3.activities.updateOne(Map("_id" -> activityOid),
+          Map($set -> Map("deliverables" -> deliverables)))
+      if (updateResult.getMatchedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+    }
   }
 
   private def validateTaskConfigurations(taskConfigurations: Seq[DynDoc], processOid: ObjectId): Unit = {
@@ -39,11 +47,13 @@ class ProcessTasksConfigUpload extends HttpServlet with HttpUtils with MailUtils
     val activities = ProcessApi.allActivities(processOid)
     if (activities.length != taskConfigurations.length)
       throw new IllegalArgumentException(s"Bad task count - expected ${activities.length}, found ${taskConfigurations.length}")
-    val expectedTaskNames = activities.map(_.name[String]).toSet
-    val unexpectedTaskNames = taskConfigurations.map(_.name[String]).filterNot(expectedTaskNames.contains)
+    val taskNameIdMap: Map[String, ObjectId] =
+        activities.map(activity => activity.name[String] -> activity._id[ObjectId]).toMap
+    val unexpectedTaskNames = taskConfigurations.map(_.name[String]).filterNot(taskNameIdMap.containsKey)
     if (unexpectedTaskNames.nonEmpty) {
       throw new IllegalArgumentException(s"""Found unexpected task names: ${unexpectedTaskNames.mkString(", ")}""")
     }
+    taskConfigurations.foreach(config => config._id = taskNameIdMap(config.name[String]))
   }
 
   private def useOneConfigRow(configTasks: Seq[DynDoc], configRow:Row): Seq[DynDoc] = {
@@ -72,7 +82,7 @@ class ProcessTasksConfigUpload extends HttpServlet with HttpUtils with MailUtils
         if (!duration.matches("[0-9.]+"))
           throw new IllegalArgumentException(s"Bad duration value, found '$duration' in row $rowNumber")
         val newDeliverable: DynDoc = new Document("name", deliverableName).append("type", deliverableType.toLowerCase).
-            append("duration", duration.toFloat).append("constraints", Seq.empty[DynDoc])
+            append("duration", duration.toDouble).append("constraints", Seq.empty[DynDoc])
         val currentTask = configTasks.head
         val currentDeliverables = currentTask.deliverables[Seq[DynDoc]]
         val existingDeliverableNames = currentDeliverables.map(_.name[String])
@@ -88,11 +98,11 @@ class ProcessTasksConfigUpload extends HttpServlet with HttpUtils with MailUtils
           throw new IllegalArgumentException(s"Bad duration value, found '$duration' in row $rowNumber")
         val newConstraint: DynDoc = constraintSpec.split(":").toSeq.map(_.trim) match {
           case Seq(bpmn, task, deliverable) => new Document("bpmn", bpmn).append("task", task).
-              append("deliverable", deliverable).append("offset", offset.toFloat).append("duration", duration.toFloat)
+              append("deliverable", deliverable).append("offset", offset.toDouble).append("duration", duration.toDouble)
           case Seq(task, deliverable) => new Document("task", task).append("deliverable", deliverable).
-              append("offset", offset.toFloat).append("duration", duration.toFloat)
-          case Seq(deliverable) => new Document("deliverable", deliverable).append("offset", offset.toFloat).
-              append("duration", duration.toFloat)
+              append("offset", offset.toDouble).append("duration", duration.toDouble)
+          case Seq(deliverable) => new Document("deliverable", deliverable).append("offset", offset.toDouble).
+              append("duration", duration.toDouble)
           case _ => throw new IllegalArgumentException(s"Bad constraint, found '$constraintSpec' in row $rowNumber")
         }
         val currentTask = configTasks.head
@@ -137,7 +147,7 @@ class ProcessTasksConfigUpload extends HttpServlet with HttpUtils with MailUtils
       if (theSheet.getSheetName != processOid.toString)
         throw new IllegalArgumentException(s"Mismatched process_id ($processOid != ${theSheet.getSheetName})")
       val taskConfigurations = processTasksConfigurations(theSheet, processOid)
-      storeTaskConfigurations(taskConfigurations, processOid)
+      storeTaskConfigurations(taskConfigurations)
       BWLogger.audit(getClass.getName, request.getMethod, s"Uploaded process $processOid configuration Excel", request)
       response.setStatus(HttpServletResponse.SC_OK)
     } catch {

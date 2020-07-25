6e326a6732e2d99b94a339d4ed501aa78ab8d503
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-07-25 21:18:05 +0530

    BrowseAmazonS3: updated to use GoogleDrive connector instead of AmazonS3 connector

--- src/main/scala/com/buildwhiz/web/BrowseAmazonS3.scala
+++ src/main/scala/com/buildwhiz/web/BrowseAmazonS3.scala
@@ -1,13 +1,11 @@
 package com.buildwhiz.web
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
-
-import com.buildwhiz.infra.{AmazonS3 => AS3}
-import com.buildwhiz.infra.FileMetadata
+import com.buildwhiz.infra.{FileMetadata, GoogleDrive, AmazonS3 => AS3}
 
 class BrowseAmazonS3 extends HttpServlet {
 
-  private def amazonS3List(request: HttpServletRequest, response: HttpServletResponse, uriRoot: String): Unit = {
+  private def storageList(request: HttpServletRequest, response: HttpServletResponse, uriRoot: String): Unit = {
     val writer = response.getWriter
     writer.println("<html><head><title>BuildWhiz Amazon S3</title></head>")
     writer.println("<body><h2 align=\"center\">BuildWhiz Amazon S3</h2>")
@@ -15,7 +13,8 @@ class BrowseAmazonS3 extends HttpServlet {
     sb.append("<table border=\"1\" align=\"center\">")
     sb.append(List("Date", "Size", "Key").
       mkString("<tr bgcolor=\"cyan\"><td align=\"center\">", "</td><td align=\"center\">", "</td></tr>"))
-    val objectSummaries: Seq[FileMetadata] = AS3.listObjects
+    //val objectSummaries: Seq[FileMetadata] = AS3.listObjects
+    val objectSummaries: Seq[FileMetadata] = GoogleDrive.listObjects
     val s3ObjectRows = objectSummaries.map(p =>
       s"""<tr><td>${p.lastModified}</td><td align="center">${p.size}</td><td>${p.key}</td></tr>""").mkString
     sb.append(s"$s3ObjectRows</table></html>")
@@ -28,7 +27,7 @@ class BrowseAmazonS3 extends HttpServlet {
     val className = getClass.getSimpleName
     try {
       request.getRequestURI.split("/").toSeq.reverse match {
-        case `className` +: _ => amazonS3List(request, response, className)
+        case `className` +: _ => storageList(request, response, className)
         case objectName +: `className` +: _ =>
           response.setStatus(HttpServletResponse.SC_OK)
       }

e2c8fc7d24c59e40610d10b9411b1f1be759fe3f
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-07-27 12:18:12 +0530

    infra.FileMetadata: added field 'mimeType', defined default values for 'created' and 'lastModified'

--- src/main/scala/com/buildwhiz/infra/package.scala
+++ src/main/scala/com/buildwhiz/infra/package.scala
@@ -1,5 +1,5 @@
 package com.buildwhiz
 
 package object infra {
-  case class FileMetadata(key: String, size: Long, created: Long, lastModified: Long)
+  case class FileMetadata(key: String, size: Long, mimeType: String = null, created: Long = 0, lastModified: Long = 0)
 }

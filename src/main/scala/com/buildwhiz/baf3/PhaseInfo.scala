e4cd6f0263a72b60fea728dcbb86288616ed49bb
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-12-24 14:35:33 +0530

    baf3.PhaseInfo: initial version

---
+++ src/main/scala/com/buildwhiz/baf3/PhaseInfo.scala
@@ -0,0 +1,88 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.infra.DynDoc
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.utils.{BWLogger, DateTimeUtils, HttpUtils}
+import org.bson.Document
+import org.bson.types.ObjectId
+import com.buildwhiz.baf2.{PersonApi, PhaseApi, ProcessApi}
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import scala.collection.JavaConverters._
+
+class PhaseInfo extends HttpServlet with HttpUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val parameters = getParameterMap(request)
+    try {
+      val phaseOid = new ObjectId(parameters("phase_id"))
+      val phaseRecord: DynDoc = PhaseApi.phaseById(phaseOid)
+      val user: DynDoc = getUser(request)
+      response.getWriter.print(PhaseInfo.phase2json(phaseRecord, user))
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, "EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}
+
+object PhaseInfo extends DateTimeUtils {
+
+  private def isEditable(phase: DynDoc, user: DynDoc): Boolean = {
+    val userOid = user._id[ObjectId]
+    phase.admin_person_id[ObjectId] == userOid || PhaseApi.canManage(userOid, phase)
+  }
+
+  private def taskInformation(phase: DynDoc, user: DynDoc): Many[Document] = {
+    val firstProcess: Option[DynDoc] = PhaseApi.allProcesses(phase._id[ObjectId]).headOption
+    val tasks = firstProcess.toSeq.flatMap(p => ProcessApi.allActivities(p))
+    val taskRecords: Seq[Document] = tasks.map(task => {
+      val rawEndDate = task.bpmn_scheduled_end_date[Long]
+      val endDate = if (rawEndDate == -1) {
+        "Not available"
+      } else {
+        val timeZone = user.tz[String]
+        dateTimeString(task.bpmn_scheduled_end_date[Long], Some(timeZone))
+      }
+      val status = task.status[String]
+      val scope = status match {
+        case "defined" => "Future"
+        case "running" => "Current"
+        case "ended" => "Past"
+        case _ => "?"
+      }
+      Map("_id" -> task._id[ObjectId].toString, "name" -> task.name[String], "bpmn_name" -> task.bpmn_name[String],
+        "status" -> status, "display_status" -> task.status[String], "due_date" -> endDate, "scope" -> scope)
+    })
+    taskRecords.asJava
+  }
+
+  def phase2json(phase: DynDoc, user: DynDoc): String = {
+    val editable = isEditable(phase, user)
+    val description = new Document("editable", editable).append("value", phase.description[String])
+    val status = new Document("editable", false).append("value", phase.status[String])
+    val displayStatus = new Document("editable", false).append("value", PhaseApi.displayStatus(phase))
+    val name = new Document("editable", editable).append("value", phase.name[String])
+    val rawPhaseManagers = phase.assigned_roles[Many[Document]].filter(_.role_name[String] == "Project-Manager").
+      map(role => {
+        val thePerson = PersonApi.personById(role.person_id[ObjectId])
+        val personName = PersonApi.fullName(thePerson)
+        new Document("_id", thePerson._id[ObjectId].toString).append("name", personName)
+      }).asJava
+    val phaseManagers = new Document("editable", editable).append("value", rawPhaseManagers)
+    val canManage = PhaseApi.canManage(user._id[ObjectId], phase)
+    val projectDoc = new Document("name", name).append("description", description).append("status", status).
+      append("display_status", displayStatus).append("managers", phaseManagers).
+      append("process_info", taskInformation(phase, user))
+    projectDoc.toJson
+  }
+
+}

842d45064e856ed165b230a9ef63f0f208b68485
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-12-24 17:57:12 +0530

    baf3.PhaseInfo: added deliverables to tasks

--- src/main/scala/com/buildwhiz/baf3/PhaseInfo.scala
+++ src/main/scala/com/buildwhiz/baf3/PhaseInfo.scala
@@ -5,7 +5,7 @@ import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.utils.{BWLogger, DateTimeUtils, HttpUtils}
 import org.bson.Document
 import org.bson.types.ObjectId
-import com.buildwhiz.baf2.{PersonApi, PhaseApi, ProcessApi}
+import com.buildwhiz.baf2.{ActivityApi, PersonApi, PhaseApi, ProcessApi}
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import scala.collection.JavaConverters._
@@ -41,6 +41,30 @@ object PhaseInfo extends DateTimeUtils {
     phase.admin_person_id[ObjectId] == userOid || PhaseApi.canManage(userOid, phase)
   }
 
+  private def deliverables(task: DynDoc, user: DynDoc): Many[Document] = {
+    val deliverables: Seq[DynDoc] = if (task.has("deliverables")) task.deliverables[Many[Document]] else Nil
+    val deliverableRecords: Seq[Document] = deliverables.map(deliverable => {
+      val rawEndDate = deliverable.bpmn_scheduled_end_date[Long]
+      val endDate = if (rawEndDate == -1) {
+        "Not available"
+      } else {
+        val timeZone = user.tz[String]
+        dateTimeString(deliverable.bpmn_scheduled_end_date[Long], Some(timeZone))
+      }
+      val status = deliverable.status[String]
+      val scope = status match {
+        case "defined" => "Future"
+        case "running" => "Current"
+        case "ended" => "Past"
+        case _ => "?"
+      }
+      Map("_id" -> deliverable._id[ObjectId].toString, "name" -> deliverable.name[String],
+          "bpmn_name" -> deliverable.bpmn_name[String], "status" -> status, "display_status" -> "Dormant",
+          "due_date" -> endDate, "scope" -> scope)
+    })
+    deliverableRecords.asJava
+  }
+
   private def taskInformation(phase: DynDoc, user: DynDoc): Many[Document] = {
     val firstProcess: Option[DynDoc] = PhaseApi.allProcesses(phase._id[ObjectId]).headOption
     val tasks = firstProcess.toSeq.flatMap(p => ProcessApi.allActivities(p))
@@ -60,7 +84,8 @@ object PhaseInfo extends DateTimeUtils {
         case _ => "?"
       }
       Map("_id" -> task._id[ObjectId].toString, "name" -> task.name[String], "bpmn_name" -> task.bpmn_name[String],
-        "status" -> status, "display_status" -> task.status[String], "due_date" -> endDate, "scope" -> scope)
+          "status" -> status, "display_status" -> ActivityApi.displayStatus2(task), "due_date" -> endDate,
+          "scope" -> scope, "deliverables" -> deliverables(task, user))
     })
     taskRecords.asJava
   }
@@ -78,10 +103,10 @@ object PhaseInfo extends DateTimeUtils {
         new Document("_id", thePerson._id[ObjectId].toString).append("name", personName)
       }).asJava
     val phaseManagers = new Document("editable", editable).append("value", rawPhaseManagers)
-    val canManage = PhaseApi.canManage(user._id[ObjectId], phase)
+    //val canManage = PhaseApi.canManage(user._id[ObjectId], phase)
     val projectDoc = new Document("name", name).append("description", description).append("status", status).
       append("display_status", displayStatus).append("managers", phaseManagers).
-      append("process_info", taskInformation(phase, user))
+      append("task_info", taskInformation(phase, user))
     projectDoc.toJson
   }
 

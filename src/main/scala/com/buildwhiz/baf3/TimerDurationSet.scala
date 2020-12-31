3b959915416ff82e3e053607bfad495bc51c8bf7
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2020-12-31 07:51:11 +0530

    baf3.ProcessBpmnXml, baf3.TimerDurationSet, baf3.VariableValueSet: initial version (for baf3)

---
+++ src/main/scala/com/buildwhiz/baf3/TimerDurationSet.scala
@@ -0,0 +1,91 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import com.buildwhiz.utils.{BWLogger, DateTimeUtils, HttpUtils}
+import org.bson.Document
+import org.bson.types.ObjectId
+import org.camunda.bpm.engine.ProcessEngines
+
+import com.buildwhiz.baf2.PhaseApi
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+class TimerDurationSet extends HttpServlet with HttpUtils with DateTimeUtils {
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
+    try {
+      val phaseOid = new ObjectId(parameters("phase_id"))
+      val thePhase = PhaseApi.phaseById(phaseOid)
+      val user: DynDoc = getUser(request)
+      if (!PhaseApi.canManage(user._id[ObjectId], thePhase))
+        throw new IllegalArgumentException("Not permitted")
+      val (duration, bpmnName) = (parameters("duration"), parameters("bpmn_name"))
+      val (timerId, timerName) = (parameters.get("timer_id"), parameters.get("timer_name"))
+      val theProcess: DynDoc = PhaseApi.allProcesses(phaseOid).headOption match {
+        case Some(p) => p
+        case None => throw new IllegalArgumentException("Phase has no processes")
+      }
+      TimerDurationSet.set(request, theProcess, timerId, timerName, bpmnName, duration)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+}
+
+object TimerDurationSet extends DateTimeUtils {
+
+  private def formatDuration(d: String): String = {
+    val parts = d.split(":").map(_.toInt) match {
+      case arr3 @ Array(_, _, _) => arr3
+      case arr2 @ Array(_, _) => 0 +: arr2
+      case arr1 @ Array(_) => 0 +: 0 +: arr1
+    }
+    parts.map(p => f"$p%02d").mkString(":")
+  }
+
+  def set(request: HttpServletRequest, theProcess: DynDoc, timerId: Option[String], timerName: Option[String],
+          bpmnName: String, inDuration: String): Unit = {
+    //val theProcess: DynDoc = ProcessApi.processById(processOid)
+    val timers: Seq[DynDoc] = theProcess.timers[Many[Document]]
+    val timerIdx: Int = (timerId, timerName) match {
+      case (Some(tid), _) => timers.indexWhere(t => t.bpmn_id[String] == tid &&
+        t.bpmn_name[String] == bpmnName)
+      case (_, Some(tname)) =>
+        timers.indexWhere(t => t.name[String].replace("\\s+", "") == tname.replace("\\s+", "") &&
+          t.bpmn_name[String] == bpmnName)
+      case _ => throw new IllegalArgumentException("Timer id or name not provided")
+    }
+    if (!inDuration.matches("(?:(?:\\d{1,2}\\:)?\\d{1,2}\\:)?\\d{1,2}"))
+      throw new IllegalArgumentException("Bad duration format")
+    val duration = formatDuration(inDuration)
+    if (theProcess.status[String] != "ended") {
+      theProcess.bpmn_timestamps[Many[Document]].find(_.name[String] == bpmnName) match {
+        case Some(ts) => if (ts.has("process_instance_id")) {
+          val rts = ProcessEngines.getDefaultProcessEngine.getRuntimeService
+          val processInstanceId = theProcess.process_instance_id[String]
+          rts.setVariable(processInstanceId, timers(timerIdx).variable[String], duration2iso(duration))
+        }
+        case None =>
+      }
+      val processOid = theProcess._id[ObjectId]
+      val updateResult = BWMongoDB3.processes.updateOne(Map("_id" -> processOid),
+        Map("$set" -> Map(s"timers.$timerIdx.duration" -> duration)))
+      if (updateResult.getMatchedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+      else {
+        val topLevelBpmn = theProcess.bpmn_name[String]
+        //   TO BE COMPLETED
+        //ProcessBpmnTraverse.scheduleBpmnElements(topLevelBpmn, processOid, request)
+      }
+      val timerLog = s"'${timers(timerIdx).name[String]}'"
+      BWLogger.audit(getClass.getName, "doPost", s"""Set duration of timer '$timerLog' to '$duration'""", request)
+    } else
+      BWLogger.log(getClass.getName, "doPost", "EXIT-OK (Process already ended, no changes)", request)
+  }
+}
---
+++ src/main/scala/com/buildwhiz/baf3/VariableValueSet.scala
@@ -0,0 +1,82 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import org.bson.Document
+import org.bson.types.ObjectId
+import org.camunda.bpm.engine.ProcessEngines
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+import com.buildwhiz.baf2.{PhaseApi, ProcessApi}
+
+class VariableValueSet extends HttpServlet with HttpUtils {
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val parameters = getParameterMap(request)
+    BWLogger.log(getClass.getName, "doPost", "ENTRY", request)
+    try {
+      val phaseOid = new ObjectId(parameters("phase_id"))
+      val (label, bpmnName, value) = (parameters("label"), parameters("bpmn_name"), parameters("value"))
+      val theProcess: DynDoc = PhaseApi.allProcesses(phaseOid).headOption match {
+        case Some(p) => p
+        case None => throw new IllegalArgumentException("Phase has no processes")
+      }
+      val user: DynDoc = getUser(request)
+      if (!ProcessApi.canManage(user._id[ObjectId], theProcess))
+        throw new IllegalArgumentException("Not permitted")
+
+      VariableValueSet.set(request, response, theProcess, label, bpmnName, value)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "doPost", s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+}
+
+object VariableValueSet extends HttpUtils {
+
+  def set(request: HttpServletRequest, response: HttpServletResponse, theProcess: DynDoc, label: String,
+          bpmnName: String, value: String): Unit = {
+
+    val variables: Seq[DynDoc] = theProcess.variables[Many[Document]]
+    val labelsAndBpmnNames: Seq[(String, String)] = variables.map(v => (v.label[String], v.bpmn_name[String]))
+    val variableIdx = labelsAndBpmnNames.indexOf((label, bpmnName))
+
+    if (variableIdx == -1)
+      throw new IllegalArgumentException(s"no such label: '$label'")
+
+    val typedValue = variables(variableIdx).`type`[String] match {
+      case "B" => value.toBoolean
+      case "L" => value.toLong
+      case "D" => value.toDouble
+      case "S" => value
+    }
+
+    if (theProcess.status[String] != "ended") {
+      if (ProcessApi.isActive(theProcess)) {
+        theProcess.bpmn_timestamps[Many[Document]].find(_.name[String] == bpmnName) match {
+          case Some(ts) => if (ts.has("process_instance_id")) {
+            val rts = ProcessEngines.getDefaultProcessEngine.getRuntimeService
+            val processInstanceId = theProcess.process_instance_id[String]
+            rts.setVariable(processInstanceId, variables(variableIdx).name[String], typedValue)
+          }
+          case None =>
+        }
+      }
+      val processOid = theProcess._id[ObjectId]
+      val updateResult = BWMongoDB3.processes.updateOne(Map("_id" -> processOid),
+        Map("$set" -> Map(s"variables.$variableIdx.value" -> typedValue)))
+      if (updateResult.getMatchedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+//      response.getWriter.print(value)
+//      response.setContentType("text/plain")
+      response.setStatus(HttpServletResponse.SC_OK)
+      val message = s"Set value of variable '${variables(variableIdx).label[String]}' to '$value'"
+      BWLogger.audit(getClass.getName, "doPost", message, request)
+    }
+  }
+}

db94461bb2d27677e644547b524c78c8edcf1ab4
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-01-06 16:28:56 +0530

    baf3.ActivityInfo: corrected several errors

--- src/main/scala/com/buildwhiz/baf3/ActivityInfo.scala
+++ src/main/scala/com/buildwhiz/baf3/ActivityInfo.scala
@@ -1,6 +1,6 @@
 package com.buildwhiz.baf3
 
-import com.buildwhiz.baf2.{ActivityApi, PersonApi, PhaseApi}
+import com.buildwhiz.baf2.{ActivityApi, PersonApi}
 import com.buildwhiz.infra.DynDoc
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.utils.{BWLogger, DateTimeUtils, HttpUtils}
@@ -48,23 +48,27 @@ object ActivityInfo extends DateTimeUtils {
           durations.getOrDefault("optimistic", "NA"), durations.getOrDefault("pessimistic", "NA"),
           durations.getOrDefault("likely", "NA"), durations.getOrDefault("actual", "NA"))
     }
+    def resolve(opts: Option[String]): String = opts match {case None => "NA"; case Some(s) => s}
     val durationOptimistic = new Document("editable", editable).append("value", dur_opti_raw)
     val durationPessimistic = new Document("editable", editable).append("value", dur_pessi_raw)
     val durationLikely = new Document("editable", editable).append("value", dur_likely_raw)
     val durationActual = new Document("editable", false).append("value", dur_actual_raw)
-    val latestStart = new Document("editable", editable).append("value", activity.latest_start[String])
-    val dateStart = new Document("editable", editable).append("value", activity.date_start[String])
-    val dateEnd = new Document("editable", editable).append("value", activity.date_end[String])
+    val latestStart = new Document("editable", editable).
+        append("value", resolve(activity.get[String]("latest_start")))
+    val dateStart = new Document("editable", editable).
+        append("value", resolve(activity.get[String]("date_start")))
+    val dateEnd = new Document("editable", editable).
+        append("value", resolve(activity.get[String]("date_end")))
     val description = new Document("editable", editable).append("value", activity.description[String])
     val status = new Document("editable", false).append("value", activity.status[String])
     val displayStatus = new Document("editable", false).append("value", ActivityApi.displayStatus2(activity))
     val name = new Document("editable", editable).append("value", activity.name[String])
-    val bpmnName: String = PhaseApi.allProcesses(activity._id[ObjectId]).headOption match {
-      case Some(theProcess) => theProcess.bpmn_name[String]
-      case None => "not-available"
-    }
+//    val bpmnName: String = PhaseApi.allProcesses(activity._id[ObjectId]).headOption match {
+//      case Some(theProcess) => theProcess.bpmn_name[String]
+//      case None => "not-available"
+//    }
     val phaseDoc = new Document("name", name).append("description", description).append("status", status).
-        append("display_status", displayStatus).append("bpmn_name", bpmnName).
+        append("display_status", displayStatus).//append("bpmn_name", bpmnName).
         append("duration_optimistic", durationOptimistic).append("duration_pessimistic", durationPessimistic).
         append("duration_likely", durationLikely).append("duration_actual", durationActual).
         append("latest_start", latestStart).append("date_start", dateStart).append("date_end", dateEnd)

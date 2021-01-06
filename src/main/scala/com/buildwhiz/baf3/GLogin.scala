24cb3272799f1e2fa28afb01109b56de5ce53717
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-01-06 07:57:40 +0530

    baf3.GLogin, baf3.package: moved Master-Data ('masterData') to newly created baf3.package object

--- src/main/scala/com/buildwhiz/baf3/GLogin.scala
+++ src/main/scala/com/buildwhiz/baf3/GLogin.scala
@@ -94,23 +94,6 @@ class GLogin extends HttpServlet with HttpUtils with CryptoUtils {
     }
   }
 
-  private val masterData = Map(
-    "ProjectInfoSet__building_use" -> Seq(
-      "Assembly Facility", "Education Facility", "Public Service Facility", "Cultural Facility",
-      "Recreation Facility", "Housing Facility", "Retail Facility", "Health Care Facility",
-      "Hospitality Facility", "Lodging Facility", "Office Facility", "Research Facility",
-      "Production Facility", "Storage Facility", "Water Infrastructure Facility",
-      "Energy Infrastructure Facility", "Waste Infrastructure Facility",
-      "Information Infrastructure Facility", "Transportation Facility", "Mixed-Use Facility", "Land",
-    ),
-    "ProjectInfoSet__construction_type" -> Seq("Steel Frame", "Wood Frame", "Concrete"),
-    "ProjectInfoSet__project_type" -> Seq("Building", "Structure", "Mobile Structure",
-      "Linear Form", "Construction Entity Grouping"),
-    "ProjectList__scope" -> Seq("all", "current", "future", "past"),
-    "PhaseList__scope" -> Seq("all", "current", "future", "past"),
-    "PhaseInfo__task_info__scope" -> Seq("all", "current", "future", "past")
-  )
-
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     //val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request, isLogin = true)
---
+++ src/main/scala/com/buildwhiz/baf3/package.scala
@@ -0,0 +1,22 @@
+package com.buildwhiz
+
+package object baf3 {
+
+  val masterData = Map(
+    "ProjectInfoSet__building_use" -> Seq(
+      "Assembly Facility", "Education Facility", "Public Service Facility", "Cultural Facility",
+      "Recreation Facility", "Housing Facility", "Retail Facility", "Health Care Facility",
+      "Hospitality Facility", "Lodging Facility", "Office Facility", "Research Facility",
+      "Production Facility", "Storage Facility", "Water Infrastructure Facility",
+      "Energy Infrastructure Facility", "Waste Infrastructure Facility",
+      "Information Infrastructure Facility", "Transportation Facility", "Mixed-Use Facility", "Land",
+    ),
+    "ProjectInfoSet__construction_type" -> Seq("Steel Frame", "Wood Frame", "Concrete"),
+    "ProjectInfoSet__project_type" -> Seq("Building", "Structure", "Mobile Structure",
+      "Linear Form", "Construction Entity Grouping"),
+    "ProjectList__scope" -> Seq("all", "current", "future", "past"),
+    "PhaseList__scope" -> Seq("all", "current", "future", "past"),
+    "PhaseInfo__task_info__scope" -> Seq("all", "current", "future", "past")
+  )
+
+}

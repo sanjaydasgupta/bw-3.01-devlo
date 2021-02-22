b8ca24bc7959d542ec1ffb41f49284ee9788d72e
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-02-22 14:10:09 +0530

    baf3.PhaseDurationRecalculate: added verification of input, and production of a properly structured output record

--- src/main/scala/com/buildwhiz/baf3/PhaseDurationRecalculate.scala
+++ src/main/scala/com/buildwhiz/baf3/PhaseDurationRecalculate.scala
@@ -1,11 +1,9 @@
 package com.buildwhiz.baf3
 
+import com.buildwhiz.infra.DynDoc
 import com.buildwhiz.infra.DynDoc._
-import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
-import com.buildwhiz.slack.SlackApi
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import org.bson.Document
-import org.bson.types.ObjectId
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
@@ -17,6 +15,7 @@ class PhaseDurationRecalculate extends HttpServlet with HttpUtils {
       val parameterString = getStreamData(request)
       BWLogger.log(getClass.getName, request.getMethod, s"Parameter-String: $parameterString", request)
       val postData: DynDoc = Document.parse(parameterString)
+      BWLogger.log(getClass.getName, request.getMethod, s"postData: $postData", request)
       if (!postData.has("duration_values"))
         throw new IllegalArgumentException("'duration_values' not provided")
       val durationValues: Seq[DynDoc] = postData.duration_values[Many[Document]]
@@ -25,19 +24,20 @@ class PhaseDurationRecalculate extends HttpServlet with HttpUtils {
           throw new IllegalArgumentException("missing 'activity_id'")
         }
         val fieldNames = Seq("duration_optimistic", "duration_pessimistic", "duration_likely")
-        val nameValuePairs: Seq[(String, Int)] = fieldNames.map(df => (df, durationValue.get[String](df))).flatMap({
-          case (_, None) => None
-          case (name, Some(duration)) => Some((name, duration.toInt))
-        })
+        val nameValuePairs: Seq[(String, Int)] = fieldNames.flatMap(fieldName =>
+          durationValue.get[String](fieldName) match {
+            case None => None
+            case Some(duration) => Some((fieldName, duration.toInt))
+          }
+        )
+        val activityOid = durationValue.activity_id[String].toInt
       }
-      val projectId = postData.remove("project_id").asInstanceOf[String]
-      //val nameValuePairs = postData.entrySet.asScala.map(es => (es.getKey, es.getValue.asInstanceOf[String])).toSeq
-      val returnedValue = new Document("phase_duration_optimistic", "NA").append("phase_duration_pessimistic", "NA").
-          append("phase_duration_likely", "NA")
+      val returnedValue = new Document("ok", 1).append("phase_duration_optimistic", "NA").
+          append("phase_duration_pessimistic", "NA").append("phase_duration_likely", "NA")
       response.getWriter.print(returnedValue.toJson)
       response.setContentType("application/json")
       val message = s"changed durations of ${durationValues.length} tasks"
-      BWLogger.audit(getClass.getName, request.getMethod, message, request)
+      BWLogger.log(getClass.getName, request.getMethod, message, request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)

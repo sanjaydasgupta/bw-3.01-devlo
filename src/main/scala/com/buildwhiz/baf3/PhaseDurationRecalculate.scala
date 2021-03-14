587a3d077d225f5cf08b6b1eaeb3c0efc3bb1c78
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-03-14 17:44:35 +0530

    baf3.PhaseDurationRecalculate: several corrections and tweaks

--- src/main/scala/com/buildwhiz/baf3/PhaseDurationRecalculate.scala
+++ src/main/scala/com/buildwhiz/baf3/PhaseDurationRecalculate.scala
@@ -20,25 +20,22 @@ class PhaseDurationRecalculate extends HttpServlet with HttpUtils {
       if (!postData.has("duration_values"))
         throw new IllegalArgumentException("'duration_values' not provided")
       val durationValues: Seq[DynDoc] = postData.duration_values[Many[Document]]
-      for (durationValue <- durationValues) {
-        if (!durationValue.has("activity_id")) {
-          throw new IllegalArgumentException("missing 'activity_id'")
-        }
-        val fieldNames = Seq("duration_optimistic", "duration_pessimistic", "duration_likely")
-        val nameValuePairs: Seq[(String, Int)] = fieldNames.flatMap(fieldName =>
-          durationValue.get[String](fieldName) match {
-            case None => None
-            case Some(duration) => Some((fieldName, duration.toInt))
-          }
-        )
+      val badDurations = durationValues.filter(dv => !dv.has("activity_id") || !dv.has("duration_likely"))
+      if (badDurations.nonEmpty)
+        throw new IllegalArgumentException(s"found duration_values without 'activity_id' and/or 'duration_likely'")
+      val oidAndDurationMap: Map[ObjectId, Int] = durationValues.map(durationValue => {
         val activityOid = new ObjectId(durationValue.activity_id[String])
-      }
+        val durationLikely = durationValue.duration_likely[String].toInt
+        (activityOid, durationLikely)
+      }).toMap
+      val bpmnName = postData.bpmn_name[String]
+      val phaseOid = new ObjectId(postData.phase_id[String])
+      val phaseDuration = ProcessBpmnTraverse.processDurationRecalculate(bpmnName, phaseOid, oidAndDurationMap, request)
       val returnedValue = new Document("ok", 1).append("phase_duration_optimistic", "NA").
-          append("phase_duration_pessimistic", "NA").append("phase_duration_likely", "NA")
-      response.getWriter.print(returnedValue.toJson)
+          append("phase_duration_pessimistic", "NA").append("phase_duration_likely", phaseDuration.toString).toJson
+      response.getWriter.print(returnedValue)
       response.setContentType("application/json")
-      val message = s"changed durations of ${durationValues.length} tasks"
-      BWLogger.log(getClass.getName, request.getMethod, message, request)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK ($returnedValue)", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)

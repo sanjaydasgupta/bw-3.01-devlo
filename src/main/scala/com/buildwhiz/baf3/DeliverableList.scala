95d858b54bcdc9247d2cdf4fb6758c81f8e68db9
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-04-07 12:37:42 +0530

    Revert "PartnerList, PartnerList2: changes required to present ''"
    This reverts commit a45402b3

---
+++ src/main/scala/com/buildwhiz/baf3/DeliverableList.scala
@@ -0,0 +1,34 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.baf2.ActivityApi
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import org.bson.Document
+import org.bson.types.ObjectId
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+class DeliverableList extends HttpServlet with HttpUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val parameters = getParameterMap(request)
+    val activityOid = new ObjectId(parameters("activity_id"))
+    try {
+      val deliverables: Seq[Document] = ActivityApi.allDeliverables3(activityOid).
+          map(deliverable => Map("_id" -> deliverable._id[ObjectId].toString, "name" -> deliverable.name[String]))
+
+      response.getWriter.print(deliverables.map(_.toJson).mkString("[", ", ", "]"))
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${deliverables.length})", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}
+

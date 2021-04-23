621e82bf6b4931b7e7a04e5727df122ff113cf7c
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-04-23 14:28:48 +0530

    baf3.DocumentInfo: corrected JSON stringify error for timestamp

--- src/main/scala/com/buildwhiz/baf3/DocumentInfo.scala
+++ src/main/scala/com/buildwhiz/baf3/DocumentInfo.scala
@@ -43,7 +43,7 @@ class DocumentInfo extends HttpServlet with HttpUtils with DateTimeUtils {
         "NA"
       val prop: Document = Map("author" -> authorName, "comments" -> version.comments[String],
         "datetime" -> dateTimeString(version.timestamp[Long], Some(user.tz[String])), "size" -> fileSize,
-        "timestamp" -> version.timestamp[Long])
+        "timestamp" -> version.timestamp[Long].toString)
       prop
     })
   }

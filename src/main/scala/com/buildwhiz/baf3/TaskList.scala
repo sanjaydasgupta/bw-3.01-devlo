4b21480d2224a47a13b299d2f6e6c6e285014e2b
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-05-17 11:45:09 +0530

    baf3.TaskList: added field 'full_path_name'

--- src/main/scala/com/buildwhiz/baf3/TaskList.scala
+++ src/main/scala/com/buildwhiz/baf3/TaskList.scala
@@ -15,8 +15,15 @@ class TaskList extends HttpServlet with HttpUtils {
     val parameters = getParameterMap(request)
     val phaseOid = new ObjectId(parameters("phase_id"))
     try {
-      val activities: Seq[Document] = PhaseApi.allActivities(phaseOid).
-          map(activity => Map("_id" -> activity._id[ObjectId].toString, "name" -> activity.name[String]))
+
+      val activities: Seq[Document] = PhaseApi.allActivities(phaseOid).map(activity => {
+        val fullPathName = activity.get[String]("full_path_name") match {
+          case Some(fpn) => fpn
+          case None => activity.name[String]
+        }
+        val (activityOid, name) = (activity._id[ObjectId].toString, activity.name[String])
+        Map("_id" -> activityOid, "name" -> name, "full_path_name" -> fullPathName)
+      })
 
       response.getWriter.print(activities.map(_.toJson).mkString("[", ", ", "]"))
       response.setContentType("application/json")

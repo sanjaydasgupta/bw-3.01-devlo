71ed88d63e5dccf43e7a51030a69ed91d4388711
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-05-19 22:18:05 +0530

    baf3.PhaseTeamsContactInfo: initial version

---
+++ src/main/scala/com/buildwhiz/baf3/PhaseTeamsContactInfo.scala
@@ -0,0 +1,53 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.baf2.{OrganizationApi, PersonApi, PhaseApi}
+import com.buildwhiz.infra.DynDoc
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import org.bson.Document
+import org.bson.types.ObjectId
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+class PhaseTeamsContactInfo extends HttpServlet with HttpUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val parameters = getParameterMap(request)
+    try {
+      val phaseOid = new ObjectId(parameters("phase_id"))
+      val phaseRecord: DynDoc = PhaseApi.phaseById(phaseOid)
+      val user: DynDoc = getUser(request)
+      val teamOids: Seq[ObjectId] = phaseRecord.get[Many[Document]]("team_assignments") match {
+        case Some(teamAssignments) => teamAssignments.map(_.team_id[ObjectId])
+        case None => Seq.empty[ObjectId]
+      }
+      val teams = TeamApi.teamsByIds(teamOids)
+      val contactInfo: Seq[String] = teams.flatMap(team => {
+        val partner = OrganizationApi.organizationById(team.organization_id[ObjectId])
+        val partnerName = partner.name[String]
+        team.get[Many[Document]]("team_members") match {
+          case Some(memberInfos) => memberInfos.map(memberInfo => {
+            val roles = memberInfo.roles[Many[String]].mkString(", ")
+            val personRecord = PersonApi.personById(memberInfo.person_id[ObjectId])
+            val personName = PersonApi.fullName(personRecord)
+            val phone = personRecord.phones[Many[Document]].find(_.`type`[String] == "work").head.phone[String]
+            val email = personRecord.emails[Many[Document]].find(_.`type`[String] == "work").head.email[String]
+            Seq(team.team_name[String], partnerName, personName, roles, phone, email).mkString("[\"", "\", \"", "\"]")
+          })
+          case None => Seq.empty[String]
+        }
+      })
+      response.getWriter.print(contactInfo.mkString("[", ",", "]"))
+      response.setContentType("application/json")
+      BWLogger.log(getClass.getName, request.getMethod, "EXIT-OK", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}

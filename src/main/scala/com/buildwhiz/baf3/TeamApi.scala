6de5a80f5f7f605cfe364ca75137d698f7b514b9
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-06-07 13:02:50 +0530

    added 'teamsByMemberOid()' to baf3.TeamApi

--- src/main/scala/com/buildwhiz/baf3/TeamApi.scala
+++ src/main/scala/com/buildwhiz/baf3/TeamApi.scala
@@ -13,6 +13,9 @@ object TeamApi {
 
   def teamById(teamOid: ObjectId): DynDoc = BWMongoDB3.teams.find(Map("_id" -> teamOid)).head
 
+  def teamsByMemberOid(memberOid: ObjectId): Seq[DynDoc] =
+    BWMongoDB3.teams.find(Map("team_members" -> Map($elemMatch -> Map("person_id" -> memberOid))))
+
   def memberOids(team: DynDoc): Seq[ObjectId] = team.get[Many[Document]]("team_members") match {
     case Some(members) => members.map(_.person_id[ObjectId])
     case None => Seq.empty[ObjectId]

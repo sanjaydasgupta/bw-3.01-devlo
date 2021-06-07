96d4923cfb6d8143ee9388bcccf2465a662225e1
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-06-07 13:05:47 +0530

    added 'deliverablesByTeamOid()' and 'deliverablesByTeamOids()' to baf3.DeliverableApi

--- src/main/scala/com/buildwhiz/baf3/DeliverableApi.scala
+++ src/main/scala/com/buildwhiz/baf3/DeliverableApi.scala
@@ -11,4 +11,10 @@ object DeliverableApi {
 
   def deliverableById(deliverableOid: ObjectId): DynDoc = BWMongoDB3.deliverables.find(Map("_id" -> deliverableOid)).head
 
+  def deliverablesByTeamOid(teamOid: ObjectId): Seq[DynDoc] =
+    BWMongoDB3.deliverables.find(Map("team_assignments" -> Map($elemMatch -> Map("team_id" -> teamOid))))
+
+  def deliverablesByTeamOids(teamOids: Seq[ObjectId]): Seq[DynDoc] = BWMongoDB3.deliverables.
+    find(Map("team_assignments" -> Map($elemMatch -> Map("team_id" -> Map($in -> teamOids)))))
+
 }

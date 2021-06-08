75a15bf73ec7c263d2093f4b9f39f0b8bb1cd218
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-06-08 14:42:56 +0530

    added 'deliverablesByActivityOids()' and 'taskStatusMap()' to baf3.DeliverableApi

--- src/main/scala/com/buildwhiz/baf3/DeliverableApi.scala
+++ src/main/scala/com/buildwhiz/baf3/DeliverableApi.scala
@@ -4,8 +4,9 @@ import com.buildwhiz.infra.BWMongoDB3
 import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc
 import com.buildwhiz.infra.DynDoc._
-
 import org.bson.types.ObjectId
+
+import scala.collection.immutable.Map.WithDefault
 
 object DeliverableApi {
 
@@ -15,6 +16,23 @@ object DeliverableApi {
     BWMongoDB3.deliverables.find(Map("team_assignments" -> Map($elemMatch -> Map("team_id" -> teamOid))))
 
   def deliverablesByTeamOids(teamOids: Seq[ObjectId]): Seq[DynDoc] = BWMongoDB3.deliverables.
-    find(Map("team_assignments" -> Map($elemMatch -> Map("team_id" -> Map($in -> teamOids)))))
+      find(Map("team_assignments" -> Map($elemMatch -> Map("team_id" -> Map($in -> teamOids)))))
 
+  def deliverablesByActivityOids(activityOids: Seq[ObjectId]): Seq[DynDoc] =
+      BWMongoDB3.deliverables.find(Map("activity_id" -> Map($in -> activityOids)))
+
+  def taskStatusMap(deliverables: Seq[DynDoc]): Map[ObjectId, String] = {
+    def aggregateStatus(statusValues: Seq[String]): String =
+      if (statusValues.distinct.length == 1) {
+        statusValues.head
+      } else {
+        "Active"
+      }
+
+    val theMap = deliverables.groupBy(_.activity_id[String]).map(kv => (new ObjectId(kv._1), kv._2.map(_.status[String]))).
+        map(kv => (kv._1, aggregateStatus(kv._2)))
+    val mapWithDefault = new WithDefault[ObjectId, String](theMap, _ => "Unknown")
+
+    mapWithDefault
+  }
 }

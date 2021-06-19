09aa97971e0ae6226bbbeffc950b7c0af07bfea6
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-06-19 10:52:54 +0530

    Updates from PhaseDurationRecalculate2: 01/04/21, 10:53 AM baf3.PhaseDurationRecalculate2, baf3.ProcessBpmnTraverse2, baf3.ProcessBpmnXml, baf3.TaskDurationsCommit2: changes and additions for including timers in the scope of phase-duration recalculate and commit. (Issue: https://prabhas.atlassian.net/browse/U20-79)

--- src/main/scala/com/buildwhiz/baf3/PhaseDurationRecalculate.scala
+++ src/main/scala/com/buildwhiz/baf3/PhaseDurationRecalculate.scala
@@ -19,17 +19,24 @@ class PhaseDurationRecalculate extends HttpServlet with HttpUtils {
       if (!postData.has("duration_values"))
         throw new IllegalArgumentException("'duration_values' not provided")
       val durationValues: Seq[DynDoc] = postData.duration_values[Many[Document]]
-      val badDurations = durationValues.filter(dv => !dv.has("activity_id") || !dv.has("duration_likely"))
+      val badDurations = durationValues.filter(dv => !(dv.has("activity_id") && dv.has("duration_likely")) &&
+          !(dv.has("timer_id") && dv.has("duration")))
       if (badDurations.nonEmpty)
-        throw new IllegalArgumentException(s"found duration_values without 'activity_id' and/or 'duration_likely'")
-      val oidAndDurationMap: Map[ObjectId, Int] = durationValues.map(durationValue => {
-        val activityOid = new ObjectId(durationValue.activity_id[String])
-        val durationLikely = durationValue.duration_likely[String].toInt
-        (activityOid, durationLikely)
-      }).toMap
+        throw new IllegalArgumentException(s"found duration_values without expected fields")
+      val oidAndDurations: Seq[(String, String, Int)] = durationValues.map(durationValue => {
+        if (durationValue.has("activity_id")) {
+          val activityOid = durationValue.activity_id[String]
+          val durationLikely = durationValue.duration_likely[String].toInt
+          ("A", activityOid, durationLikely)
+        } else {
+          val timerId = durationValue.timer_id[String]
+          val duration = durationValue.duration[String].toInt
+          ("T", timerId, duration)
+        }
+      })
       val bpmnName = postData.bpmn_name[String]
       val phaseOid = new ObjectId(postData.phase_id[String])
-      val phaseDuration = ProcessBpmnTraverse.processDurationRecalculate(bpmnName, phaseOid, oidAndDurationMap, request)
+      val phaseDuration = ProcessBpmnTraverse.processDurationRecalculate(bpmnName, phaseOid, oidAndDurations, request)
       val returnedValue = new Document("ok", 1).append("phase_duration_optimistic", "NA").
           append("phase_duration_pessimistic", "NA").append("phase_duration_likely", phaseDuration.toString).toJson
       response.getWriter.print(returnedValue)

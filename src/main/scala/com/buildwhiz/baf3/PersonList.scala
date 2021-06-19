8eb67df9c3ef9e5948a4a32275e0538533fb6a5f
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-06-19 10:52:00 +0530

    Updates from PersonList2:       val persons = PersonApi.fetch3(None, organisationOidOption, skillOption, projectOidOption, phaseOidOption,

--- src/main/scala/com/buildwhiz/baf3/PersonList.scala
+++ src/main/scala/com/buildwhiz/baf3/PersonList.scala
@@ -1,8 +1,8 @@
 package com.buildwhiz.baf3
 
+import com.buildwhiz.baf2.PersonApi
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import org.bson.types.ObjectId
-import com.buildwhiz.baf2.PersonApi
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
@@ -12,23 +12,14 @@ class PersonList extends HttpServlet with HttpUtils {
     BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
     val parameters = getParameterMap(request)
     try {
-//      val skillOption: Option[String] = parameters.get("skill") match {
-//        case Some(skill) =>
-//          if (RoleListSecondary.secondaryRoles.contains(skill) || skill == "none")
-//          //if (RoleListSecondary.secondaryRoles.contains(skill))
-//            None
-//          else
-//            Some(skill)
-//        case other => other
-//      }
       val skillOption: Option[String] = parameters.get("skill")
       val organisationOidOption = parameters.get("organization_id").map(new ObjectId(_))
       val projectOidOption = parameters.get("project_id").map(new ObjectId(_))
       val phaseOidOption = parameters.get("phase_id").map(new ObjectId(_))
       val processOidOption = parameters.get("process_id").map(new ObjectId(_))
       val activityOidOption = parameters.get("activity_id").map(new ObjectId(_))
-      val persons = PersonApi.fetch(None, organisationOidOption, skillOption, projectOidOption, phaseOidOption,
-          processOidOption, activityOidOption)
+      val persons = PersonApi.fetch3(None, organisationOidOption, skillOption, projectOidOption, phaseOidOption,
+        processOidOption, activityOidOption)
       val personDocuments = persons.map(PersonApi.person2document).sortBy(p => p.getString("name"))
       response.getWriter.print(personDocuments.map(bson2json).mkString("[", ",", "]"))
       response.setContentType("application/json")

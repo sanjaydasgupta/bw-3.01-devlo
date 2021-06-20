e3586131effb29634ffb2fba2a2a854d0ce72883
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-06-20 11:53:03 +0530

    augmented 'taskStatusMap()' to yield values Upcoming, Current, and Ended as recommended by Roman

--- src/main/scala/com/buildwhiz/baf3/DeliverableApi.scala
+++ src/main/scala/com/buildwhiz/baf3/DeliverableApi.scala
@@ -21,6 +21,12 @@ object DeliverableApi {
   def deliverablesByActivityOids(activityOids: Seq[ObjectId]): Seq[DynDoc] =
       BWMongoDB3.deliverables.find(Map("activity_id" -> Map($in -> activityOids)))
 
+  private val externalStatusMap: Map[String, String] = Map(
+    "Not-Started" -> "Not-Started", "Pre-Approved" -> "Not-Started", "Deliverable-Started" -> "Active",
+    "Deliverable-Started And Pre-Approved" -> "Active", "Deliverable-Completed" -> "Review",
+    "Completion-Detected" -> "Completed", "Completed" -> "Completed", "Completed-pl" -> "Completed",
+    "Review-Reject" -> "Rework")
+
   def taskStatusMap(deliverables: Seq[DynDoc]): Map[ObjectId, String] = {
     def aggregateStatus(statusValues: Seq[String]): String =
       if (statusValues.distinct.length == 1) {
@@ -29,10 +35,14 @@ object DeliverableApi {
         "Active"
       }
 
+    val bpmnStatusMap: Map[String, String] = Map("Not-Started" -> "Upcoming", "Completed" -> "Completed",
+      "Active" -> "Current")
     val theGroups = deliverables.groupBy(_.activity_id[ObjectId])
-    val statusValues = theGroups.map(kv => (kv._1, kv._2.map(_.status[String])))
+    val statusValues = theGroups.map(kv =>
+      (kv._1, kv._2.map(deliverable => externalStatusMap.getOrElse(deliverable.status[String], "Unknown"))))
     val theMap = statusValues.map(kv => (kv._1, aggregateStatus(kv._2)))
-    val mapWithDefault = new WithDefault[ObjectId, String](theMap, _ => "Unknown")
+    val taskBpmnStatusMap = theMap.map(kv => (kv._1, bpmnStatusMap.getOrElse(kv._2, "Unknown")))
+    val mapWithDefault = new WithDefault[ObjectId, String](taskBpmnStatusMap, _ => "Upcoming")
 
     mapWithDefault
   }

060f55e29450efeee43df7919a1d668222e5aa9d
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-06-25 07:36:01 +0530

    corrected PartnerList to always display the user's own partner (organization) irrespective of project associations

--- src/main/scala/com/buildwhiz/baf3/PartnerList.scala
+++ src/main/scala/com/buildwhiz/baf3/PartnerList.scala
@@ -82,12 +82,17 @@ class PartnerList extends HttpServlet with HttpUtils with DateTimeUtils {
           flatMap(_.get[ObjectId]("organization_id"))
       val myProjectsOrganizations: Seq[DynDoc] = (customerOrganizationOids ++ enrolledOrganizationOids).distinct.
           map(cOid => OrganizationApi.organizationById(cOid)).filter(matchSkill).filter(matchOrganizationType)
+      val myOrganizations = if (myProjectsOrganizations.exists(_._id[ObjectId] == user.organization_id[ObjectId])) {
+        myProjectsOrganizations
+      } else {
+        OrganizationApi.organizationById(user.organization_id[ObjectId]) +: myProjectsOrganizations
+      }
       val displayAllOrganizations: Boolean = isAdmin || myProjects.exists(p => ProjectApi.canManage(user._id[ObjectId], p))
 
       val allOrganizations: Seq[DynDoc] = if (displayAllOrganizations) {
-        OrganizationApi.fetch(optOrgType = optOrganizationType, optSkill = skillParameter) ++ myProjectsOrganizations
+        OrganizationApi.fetch(optOrgType = optOrganizationType, optSkill = skillParameter) ++ myOrganizations
       } else {
-        myProjectsOrganizations
+        myOrganizations
       }
 
       val partners: Seq[DynDoc] =

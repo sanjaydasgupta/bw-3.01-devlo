326caf5ecab8a4e51350d8e0b5fcf74030f7afab
Sanjay Dasgupta <sanjay.dasgupta@gmail.com>
2021-06-25 07:34:24 +0530

    corrected PersonInfo to allow non-admin users to edit only their own person records.

--- src/main/scala/com/buildwhiz/baf3/PersonInfo.scala
+++ src/main/scala/com/buildwhiz/baf3/PersonInfo.scala
@@ -13,55 +13,46 @@ import scala.collection.JavaConverters._
 
 class PersonInfo extends HttpServlet with HttpUtils {
 
-  private def isEditable(person: DynDoc, user: DynDoc): Boolean = {
-    val userIsAdmin = PersonApi.isBuildWhizAdmin(Right(user))
-    val inSameOrganization = if (user.has("organization_id") && person.has("organization_id"))
-      user.organization_id[ObjectId] == person.organization_id[ObjectId]
-    else
-      false
-    userIsAdmin || inSameOrganization
-  }
-
   private def wrap(value: Any, editable: Boolean): Document = {
     new Document("editable", editable).append("value", value/*.toString*/)
   }
 
-  private def person2json(person: DynDoc, editable: Boolean): String = {
-    val firstName = new Document("editable", editable).append("value", person.first_name[String])
-    val lastName = new Document("editable", editable).append("value", person.last_name[String])
+  private def person2json(person: DynDoc, userIsAdmin: Boolean, userOwnsRecord: Boolean): String = {
+    val firstName = new Document("editable", userOwnsRecord).append("value", person.first_name[String])
+    val lastName = new Document("editable", userOwnsRecord).append("value", person.last_name[String])
 
-    val yearsExperience = wrap(person.years_experience[Double], editable)
-    val rating = wrap(person.rating[Int], editable)
+    val yearsExperience = wrap(person.years_experience[Double], userOwnsRecord)
+    val rating = wrap(person.rating[Int], userIsAdmin)
     val position = person.get[String]("position") match {
-      case Some(pos) => wrap(pos, editable)
-      case None => wrap("", editable)
+      case Some(pos) => wrap(pos, userOwnsRecord)
+      case None => wrap("", userOwnsRecord)
     }
-    val skills = wrap(person.skills[Many[String]], editable)
-    val active = wrap(if (person.has("enabled")) person.enabled[Boolean] else false, editable)
+    val skills = wrap(person.skills[Many[String]], userOwnsRecord)
+    val active = wrap(if (person.has("enabled")) person.enabled[Boolean] else false, userIsAdmin)
     val emails: Seq[DynDoc] = person.emails[Many[Document]]
-    val workEmail = wrap(emails.find(_.`type`[String] == "work").get.email[String], editable)
+    val workEmail = wrap(emails.find(_.`type`[String] == "work").get.email[String], userIsAdmin)
     val phones: Seq[DynDoc] = person.phones[Many[Document]]
-    val phoneCanText = wrap(if(person.has("phone_can_text")) person.phone_can_text[Boolean] else false, editable)
+    val phoneCanText = wrap(if(person.has("phone_can_text")) person.phone_can_text[Boolean] else false, userOwnsRecord)
     val bareWorkPhone = phones.find(_.`type`[String] == "work") match {
       case None => ""
       case Some(workPhoneRecord) => workPhoneRecord.phone[String]
     }
-    val workPhone = wrap(bareWorkPhone, editable)
+    val workPhone = wrap(bareWorkPhone, userOwnsRecord)
     val bareMobilePhone = phones.find(_.`type`[String] == "mobile") match {
       case None => ""
       case Some(mobilePhoneRecord) => mobilePhoneRecord.phone[String]
     }
-    val mobilePhone = wrap(bareMobilePhone, editable)
+    val mobilePhone = wrap(bareMobilePhone, userOwnsRecord)
     val workAddress = wrap(if (person.has("work_address"))
       person.work_address[String]
     else
-      "", editable)
+      "", userOwnsRecord)
     val rawIndividualRoles: java.util.Collection[String] =
       if (person.has("individual_roles"))
         person.individual_roles[Many[String]]
       else
         Seq.empty[String].asJava
-    val individualRoles = wrap(rawIndividualRoles, editable)
+    val individualRoles = wrap(rawIndividualRoles, userOwnsRecord)
 
     val personDoc = new Document("first_name", firstName).append("last_name", lastName).append("rating", rating).
         append("skills", skills).append("years_experience", yearsExperience).append("active", active).
@@ -80,8 +71,9 @@ class PersonInfo extends HttpServlet with HttpUtils {
       val personOid = new ObjectId(parameters("person_id"))
       val personRecord: DynDoc = BWMongoDB3.persons.find(Map("_id" -> personOid)).head
       val user: DynDoc = getPersona(request)
-      val personIsEditable = isEditable(personRecord, user)
-      response.getWriter.print(person2json(personRecord, personIsEditable))
+      val userIsAdmin = PersonApi.isBuildWhizAdmin(Right(user))
+      val userIsRecordOwner = user._id[ObjectId] == personRecord._id[ObjectId]
+      response.getWriter.print(person2json(personRecord, userIsAdmin, userIsRecordOwner))
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
       BWLogger.log(getClass.getName, request.getMethod, "EXIT-OK", request)

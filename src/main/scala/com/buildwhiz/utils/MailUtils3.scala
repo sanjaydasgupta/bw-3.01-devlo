ebdd0596bbd2c216a448a32012195c5a105d00b5
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-07-23 10:52:42 +0530

    changes in handling of sendgrid api key

--- src/main/scala/com/buildwhiz/utils/MailUtils3.scala
+++ src/main/scala/com/buildwhiz/utils/MailUtils3.scala
@@ -10,13 +10,34 @@ import javax.servlet.http.HttpServletRequest
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import java.io.{File => javaFile, BufferedReader, FileReader}
+
 trait MailUtils3 {
+
+  def sendGridKey(): String = {
+    val tomcatDir = new javaFile("server").listFiles.filter(_.getName.startsWith("apache-tomcat-")).head
+    val doNotTouchFolder = new javaFile(tomcatDir, "webapps/bw-3.01/WEB-INF/classes/do-not-touch")
+    if (!doNotTouchFolder.exists()) {
+      val message = s"No such file: '${doNotTouchFolder.getAbsolutePath}'"
+      BWLogger.log(getClass.getName, "getSendGridKey()", s"ERROR ($message)")
+      throw new IllegalArgumentException(message)
+    }
+    val sendGridKeyFile = new javaFile(doNotTouchFolder, "sendgrid-key.txt")
+    if (!sendGridKeyFile.exists()) {
+      val message = s"No such file: '${sendGridKeyFile.getAbsolutePath}'"
+      BWLogger.log(getClass.getName, "getSendGridKey()", s"ERROR ($message)")
+      throw new IllegalArgumentException(message)
+    }
+    val keyFileReader = new BufferedReader(new FileReader(sendGridKeyFile))
+        keyFileReader.readLine()
+  }
+
   def sendMail(recipientOids: Seq[ObjectId], subject: String, body: String, request: Option[HttpServletRequest]): Unit = {
     BWLogger.log(getClass.getName, "sendMail", s"ENTRY", request)
     try {
       val fromAddress = new Email("notifications@550of.com")
       val content = new Content("text/plain", body)
-      val sendGrid = new SendGrid(System.getenv("sendgrid_api_key"))
+      val sendGrid = new SendGrid(sendGridKey())
       for (userOid <- recipientOids) {
         Future {
           val user = PersonApi.personById(userOid)
@@ -30,11 +51,10 @@ trait MailUtils3 {
           val response = sendGrid.api(sendGridRequest)
           val statusCode = response.getStatusCode
           val statusBody = response.getBody
-          //val statusHeaders = response.getHeaders
-          if (statusCode == 200) {
+          if (Seq(200, 202).contains(statusCode)) {
             BWLogger.log(getClass.getName, "sendMail", s"EXIT-OK", request)
           } else {
-            BWLogger.log(getClass.getName, "sendMail", s"WARN-($statusCode)-$statusBody", request)
+            BWLogger.log(getClass.getName, "sendMail", s"Error-($statusCode)-$statusBody", request)
           }
         }
       }

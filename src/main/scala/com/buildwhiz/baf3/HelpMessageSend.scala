d221b7172647da139f184256051b4060f582194a
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-07-23 13:06:10 +0530

    initial version

---
+++ src/main/scala/com/buildwhiz/baf3/HelpMessageSend.scala
@@ -0,0 +1,44 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.baf2.PersonApi
+import com.buildwhiz.infra.DynDoc
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import org.bson.Document
+import org.bson.types.ObjectId
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import com.buildwhiz.utils.MailUtils3
+
+class HelpMessageSend extends HttpServlet with HttpUtils with MailUtils3 {
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    try {
+      val parameterString = getStreamData(request)
+      BWLogger.log(getClass.getName, request.getMethod, s"Parameter-String: $parameterString", request)
+      val postData = Document.parse(parameterString)
+      val subject = postData.getString("subject")
+      val body = postData.getString("body")
+      val user: DynDoc = getUser(request)
+      val fullName = PersonApi.fullName(user)
+
+      val emailSubject = s"Help-Message from $fullName (${user._id[ObjectId]})"
+      val emailBody = s"User's-Subject: $subject\n\n$body"
+      val adminOids = PersonApi.listAdmins.map(_._id[ObjectId])
+      sendMail(adminOids, emailSubject, emailBody, Some(request))
+
+      response.getWriter.print(successJson())
+      response.setContentType("application/json")
+      val message = s"$fullName sent help-message with subject '$subject'"
+      BWLogger.log(getClass.getName, request.getMethod, message, request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}

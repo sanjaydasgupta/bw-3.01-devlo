c9c13eaf008a486c6cc6d984d153347ecf60868e
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-07-29 18:34:50 +0530

    initial version (for baf3)

---
+++ src/main/scala/com/buildwhiz/baf3/BpmnList.scala
@@ -0,0 +1,32 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.utils.{BWLogger, BpmnUtils}
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+class BpmnList extends HttpServlet with BpmnUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, "doGet()", s"ENTRY", request)
+    try {
+      val prefix = ".+/Phase-"
+      val phaseNames: Seq[String] = getDeployedResources.map(_.getName).filter(_.matches(s"$prefix.+\\.bpmn")).
+        map(s => {
+          val idx = s.lastIndexOf("/Phase-")
+          val idx2 = s.lastIndexOf(".bpmn")
+          val pn = s.substring(idx + "/Phase-".length, idx2)
+          s""" "$pn" """.trim
+        }).distinct
+      response.getWriter.print(phaseNames.sorted.mkString("[", ", ", "]"))
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, "doGet()", s"EXIT-OK (${phaseNames.length})", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, "doGet()", s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}

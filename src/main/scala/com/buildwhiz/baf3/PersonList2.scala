9d246cd14664576c7c6810af81572581eb48be8d
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-09-05 11:39:01 +0530

    new version with 'menu_items' added

---
+++ src/main/scala/com/buildwhiz/baf3/PersonList2.scala
@@ -0,0 +1,42 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.baf2.PersonApi
+import com.buildwhiz.infra.DynDoc
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import org.bson.Document
+import org.bson.types.ObjectId
+import scala.collection.JavaConverters._
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+class PersonList2 extends HttpServlet with HttpUtils {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val parameters = getParameterMap(request)
+    try {
+      val skillOption: Option[String] = parameters.get("skill")
+      val organisationOidOption = parameters.get("organization_id").map(new ObjectId(_))
+      val projectOidOption = parameters.get("project_id").map(new ObjectId(_))
+      val phaseOidOption = parameters.get("phase_id").map(new ObjectId(_))
+      val processOidOption = parameters.get("process_id").map(new ObjectId(_))
+      val activityOidOption = parameters.get("activity_id").map(new ObjectId(_))
+      val persons = PersonApi.fetch3(None, organisationOidOption, skillOption, projectOidOption, phaseOidOption,
+        processOidOption, activityOidOption)
+      val personDocuments = persons.map(PersonApi.person2document).sortBy(p => p.getString("name")).asJava
+      val user: DynDoc = getPersona(request)
+      val menuItems = displayedMenuItems(PersonApi.isBuildWhizAdmin(Right(user)))
+      val result = new Document("person_list", personDocuments).append("menu_items", menuItems)
+      response.getWriter.print(result.toJson)
+      response.setContentType("application/json")
+      response.setStatus(HttpServletResponse.SC_OK)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${persons.length})", request)
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+
+}

9c2fafc78bc9216d36e8fc648ca1fe8f3b3249dc
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-09-23 16:12:52 +0530

    added code to push Slack home pages to all known Slack users on startup

--- src/main/scala/com/buildwhiz/infra/ServletContextListener.scala
+++ src/main/scala/com/buildwhiz/infra/ServletContextListener.scala
@@ -1,11 +1,14 @@
 package com.buildwhiz.infra
 
+import com.buildwhiz.slack.SlackApi
+
 import javax.servlet.{ServletContextEvent, ServletContextListener => JavaxSCL}
 
 class ServletContextListener extends JavaxSCL {
 
   override def contextInitialized(sce: ServletContextEvent): Unit = {
     TimerModule.scheduleTimer()
+    SlackApi.pushHomePages()
   }
 
   override def contextDestroyed(sce: ServletContextEvent): Unit = {

8ec6b7aee479c3f93bed5ba5b7c7f209ae31281e
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-10-03 14:26:57 +0530

    updated for migration to the Phase-Precon-Top5 BPMN

--- src/main/scala/com/buildwhiz/tools/scripts/PhaseBpmnMigration.scala
+++ src/main/scala/com/buildwhiz/tools/scripts/PhaseBpmnMigration.scala
@@ -3,7 +3,6 @@ package com.buildwhiz.tools.scripts
 import com.buildwhiz.baf2.PhaseApi
 import com.buildwhiz.baf3.DeliverableApi
 import com.buildwhiz.infra.BWMongoDB3
-import com.buildwhiz.utils.BWLogger
 import org.bson.types.ObjectId
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
@@ -39,25 +38,44 @@ object PhaseBpmnMigration {
               response.getWriter.println(s"\tCan clone deliverable '${d.name[String]}' ($activityIds)")
             }
           }
-        } else if (fromFullPathName.startsWith("DD/")) {
+        } else if (fromActivity.name[String] == "Site Permit Application") {
+          // Handle exceptions (tasks removed or relocated in the new BPMMN) in this "else" block
           response.getWriter.println(s"Disambiguating '$fromFullPathName' for any deliverables ...")
           for (d <- deliverables) {
-            val description = d.description[String]
-            val prefix = description.split(" ").head.trim()
-            toActivitiesMap.find(_._1.matches(s"$prefix[% ]+DD${fromFullPathName.substring(2)}")) match {
-              case Some((_, toActivity)) =>
-                response.getWriter.println(s"\t$fromFullPathName --> ${toActivity.full_path_name[String]}")
-                val activityIds = s"from ${fromActivity._id[ObjectId]} to ${toActivity._id[ObjectId]}"
-                if (go) {
-                  d.remove("_id")
-                  d.activity_id = toActivity._id[ObjectId]
-                  BWMongoDB3.deliverables.insertOne(d.asDoc)
-                  response.getWriter.println(s"\tCloned deliverable '${d.name[String]}' ($activityIds)")
-                } else {
-                  response.getWriter.println(s"\tCan clone deliverable '${d.name[String]}' ($activityIds)")
-                }
-              case None =>
-                response.getWriter.println(s"PROBLEM (no disambiguation found): $fromFullPathName --> Nil")
+            val deliverableName = d.name[String]
+            val deliverableNamePrefix = deliverableName.split("-").head.trim()
+            if (Set("SFPUC", "PG&E", "DPW", "SFFD").contains(deliverableNamePrefix)) {
+              toActivitiesMap.find(_._1 == s"Site Permit Application/$deliverableNamePrefix") match {
+                case Some((_, toActivity)) =>
+                  response.getWriter.println(s"\t$fromFullPathName --> ${toActivity.full_path_name[String]}")
+                  val activityIds = s"from ${fromActivity._id[ObjectId]} to ${toActivity._id[ObjectId]}"
+                  if (go) {
+                    d.remove("_id")
+                    d.activity_id = toActivity._id[ObjectId]
+                    BWMongoDB3.deliverables.insertOne(d.asDoc)
+                    response.getWriter.println(s"\tCloned deliverable '${d.name[String]}' ($activityIds)")
+                  } else {
+                    response.getWriter.println(s"\tCan clone deliverable '${d.name[String]}' ($activityIds)")
+                  }
+                case None =>
+                  response.getWriter.println(s"PROBLEM (no disambiguation found): $fromFullPathName --> Nil")
+              }
+            } else {
+              toActivitiesMap.find(_._1 == s"Site Permit Application/Application") match {
+                case Some((_, toActivity)) =>
+                  response.getWriter.println(s"\t$fromFullPathName --> ${toActivity.full_path_name[String]}")
+                  val activityIds = s"from ${fromActivity._id[ObjectId]} to ${toActivity._id[ObjectId]}"
+                  if (go) {
+                    d.remove("_id")
+                    d.activity_id = toActivity._id[ObjectId]
+                    BWMongoDB3.deliverables.insertOne(d.asDoc)
+                    response.getWriter.println(s"\tCloned deliverable '${d.name[String]}' ($activityIds)")
+                  } else {
+                    response.getWriter.println(s"\tCan clone deliverable '${d.name[String]}' ($activityIds)")
+                  }
+                case None =>
+                  response.getWriter.println(s"PROBLEM (no disambiguation found): $fromFullPathName --> Nil")
+              }
             }
           }
         } else if (deliverables.nonEmpty) {

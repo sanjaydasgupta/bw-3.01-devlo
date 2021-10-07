e5e47d80429a1da7e66f0eecf140a75c602a1311
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-10-07 09:51:14 +0530

    PhaseBpmnMigrationActivityDetails: final corrections and updates

--- src/main/scala/com/buildwhiz/tools/scripts/PhaseBpmnMigrationActivityDetails.scala
+++ src/main/scala/com/buildwhiz/tools/scripts/PhaseBpmnMigrationActivityDetails.scala
@@ -1,7 +1,7 @@
 package com.buildwhiz.tools.scripts
 
 import com.buildwhiz.baf2.PhaseApi
-import com.buildwhiz.infra.BWMongoDB3
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
 import org.bson.types.ObjectId
@@ -24,42 +24,52 @@ object PhaseBpmnMigrationActivityDetails {
       val toActivitiesMap = toActivitiesFullPathNames.zip(toActivities).toMap
       writer.println(s"Activity counts: ${fromActivities.length}, ${toActivities.length}")
       for (fromActivity <- fromActivities) {
+        val optDurations: Option[DynDoc] = fromActivity.get[Document]("durations")
+        val hasDuration = optDurations.exists(_.likely[Int] != -1)
         val fromFullPathName = fromActivity.full_path_name[String]
         val found = toActivitiesMap.contains(fromFullPathName)
         if (found) {
           val toActivity = toActivitiesMap(fromFullPathName)
-          writer.println(s"$fromFullPathName --> ${toActivity.full_path_name[String]}")
-          val durations = fromActivity.durations[Document]
-          if (go) {
-            val updateResult = BWMongoDB3.activities.updateOne(Map("_id" -> toActivity._id[ObjectId]),
-                Map($set -> Map("durations" -> durations)))
-            if (updateResult.getMatchedCount == 0)
-              throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
-            writer.println(s"\tCOPIED detail fields")
-          } else if (fromFullPathName.matches("^[CD]D/.+")) {
-            writer.println(s"\tCan copy detail fields: ${durations.toJson}")
+          writer.println(s"$fromFullPathName ($hasDuration) --> ${toActivity.full_path_name[String]}")
+          optDurations match {
+            case Some(durations) =>
+              if (go) {
+                val updateResult = BWMongoDB3.activities.updateOne(Map("_id" -> toActivity._id[ObjectId]),
+                  Map($set -> Map("durations" -> durations)))
+                if (updateResult.getMatchedCount == 0)
+                  throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+                writer.println(s"\tCOPIED detail fields")
+              } else {
+                writer.println(s"\tCan copy detail fields: ${durations.asDoc.toJson}")
+              }
+            case None =>
+              writer.println(s"\tWARNING 'durations' field missing")
           }
         } else if (fromFullPathName.matches("^[CD]D/.+")) {
           val alternatePathName = fromFullPathName.replace("CD/", "100% CD/").replace("DD/", "100% DD/")
           val found = toActivitiesMap.contains(alternatePathName)
           if (found) {
             val toActivity = toActivitiesMap(alternatePathName)
-            writer.println(s"$fromFullPathName --> ${toActivity.full_path_name[String]}")
-            val durations = fromActivity.durations[Document]
-            if (go) {
-              val updateResult = BWMongoDB3.activities.updateOne(Map("_id" -> toActivity._id[ObjectId]),
-                Map($set -> Map("durations" -> durations)))
-              if (updateResult.getMatchedCount == 0)
-                throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
-              writer.println(s"\tCOPIED detail fields")
-            } else {
-              writer.println(s"\tCan copy detail fields: ${durations.toJson}")
+            writer.println(s"$fromFullPathName ($hasDuration) --> ${toActivity.full_path_name[String]}")
+            optDurations match {
+              case Some(durations) =>
+                if (go) {
+                  val updateResult = BWMongoDB3.activities.updateOne(Map("_id" -> toActivity._id[ObjectId]),
+                    Map($set -> Map("durations" -> durations)))
+                  if (updateResult.getMatchedCount == 0)
+                    throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+                  writer.println(s"\tCOPIED detail fields")
+                } else {
+                  writer.println(s"\tCan copy detail fields: ${durations.asDoc.toJson}")
+                }
+              case None =>
+                writer.println(s"\tWARNING 'durations' field missing")
             }
           } else {
-            writer.println(s"WARNING (NOT FOUND partner activity): $fromFullPathName --> Nil")
+            writer.println(s"WARNING (NOT FOUND partner activity): $fromFullPathName ($hasDuration) --> Nil")
           }
         } else {
-          writer.println(s"WARNING (NOT FOUND partner activity): $fromFullPathName --> Nil")
+          writer.println(s"WARNING (NOT FOUND partner activity): $fromFullPathName ($hasDuration) --> Nil")
         }
       }
       writer.println(s"${getClass.getName}:main() EXIT-OK")

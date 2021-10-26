557eb4001bbeb8921912290c2ca61e1458a64f1b
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-10-26 12:49:36 +0530

    PersonList: added 'menu_items' and other assorted changes (inherited from PersonList2)

--- src/main/scala/com/buildwhiz/baf3/PersonList.scala
+++ src/main/scala/com/buildwhiz/baf3/PersonList.scala
@@ -1,8 +1,11 @@
 package com.buildwhiz.baf3
 
 import com.buildwhiz.baf2.PersonApi
+import com.buildwhiz.infra.DynDoc
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import org.bson.Document
 import org.bson.types.ObjectId
+import scala.collection.JavaConverters._
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
@@ -10,6 +13,7 @@ class PersonList extends HttpServlet with HttpUtils {
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
+    val t0 = System.currentTimeMillis()
     val parameters = getParameterMap(request)
     try {
       val skillOption: Option[String] = parameters.get("skill")
@@ -20,11 +24,23 @@ class PersonList extends HttpServlet with HttpUtils {
       val activityOidOption = parameters.get("activity_id").map(new ObjectId(_))
       val persons = PersonApi.fetch3(None, organisationOidOption, skillOption, projectOidOption, phaseOidOption,
         processOidOption, activityOidOption)
-      val personDocuments = persons.map(PersonApi.person2document).sortBy(p => p.getString("name"))
-      response.getWriter.print(personDocuments.map(bson2json).mkString("[", ",", "]"))
+      val personDocuments = persons.map(p => {
+        val pd: DynDoc = PersonApi.person2document(p)
+        pd._id = pd._id[ObjectId].toString
+        pd.asDoc
+      }).sortBy(p => p.getString("name")).asJava
+      val user: DynDoc = getPersona(request)
+      val isAdmin = PersonApi.isBuildWhizAdmin(Right(user))
+      val menuItems = uiContextSelectedManaged(request) match {
+        case None => displayedMenuItems(isAdmin, starting = true)
+        case Some((selected, managed)) => displayedMenuItems(isAdmin, managed, !selected)
+      }
+      val result = new Document("person_list", personDocuments).append("menu_items", menuItems)
+      response.getWriter.print(result.toJson)
       response.setContentType("application/json")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${persons.length})", request)
+      val delay = System.currentTimeMillis() - t0
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (${persons.length}, time: $delay ms)", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getName}(${t.getMessage})", request)

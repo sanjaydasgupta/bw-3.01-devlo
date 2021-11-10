1db740531fbab5b7d4f2e3ec4d26c4b6a7318c0e
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-11-10 20:33:28 +0530

    tools.PhaseDelete: added permission check

--- src/main/scala/com/buildwhiz/tools/scripts/PhaseDelete.scala
+++ src/main/scala/com/buildwhiz/tools/scripts/PhaseDelete.scala
@@ -1,17 +1,22 @@
 package com.buildwhiz.tools.scripts
 
-import com.buildwhiz.baf2.PhaseApi
-import com.buildwhiz.infra.BWMongoDB3
+import com.buildwhiz.baf2.{PersonApi, PhaseApi}
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.utils.HttpUtils
 import org.bson.types.ObjectId
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
-object PhaseDelete {
+object PhaseDelete extends HttpUtils {
 
   def main(request: HttpServletRequest, response: HttpServletResponse, args: Array[String]): Unit = {
     response.getWriter.println(s"${getClass.getName}:main() ENTRY")
+    val user: DynDoc = getUser(request)
+    if (!PersonApi.isBuildWhizAdmin(Right(user)) || user.first_name[String] != "Sanjay") {
+      throw new IllegalArgumentException("Not permitted")
+    }
     if (args.length >= 1) {
       val go: Boolean = args.length == 2 && args(1) == "GO"
       val phaseOid = new ObjectId(args(0))

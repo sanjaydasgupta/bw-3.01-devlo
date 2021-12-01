e00899bd2c5befb642bffd05fbdcd7718fcb0a01
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2021-12-01 13:01:58 +0530

    OrphanRecordsDelete: added detection of expected-but-missing records

--- src/main/scala/com/buildwhiz/tools/scripts/OrphanRecordsDelete.scala
+++ src/main/scala/com/buildwhiz/tools/scripts/OrphanRecordsDelete.scala
@@ -14,26 +14,35 @@ object OrphanRecordsDelete extends HttpUtils {
   private def deleteOrphanedPhases(request: HttpServletRequest, response: HttpServletResponse, go: Boolean): Unit = {
     response.getWriter.println(s"ENTRY deleteOrphanedPhases()")
     val projects: Seq[DynDoc] = BWMongoDB3.projects.find()
-    val phaseOids: Many[ObjectId] = projects.flatMap(_.phase_ids[Many[ObjectId]]).distinct
-    response.getWriter.println(s"""\tKnown phase Oids (${phaseOids.length}): ${phaseOids.mkString(", ")}""")
-    val orphanedPhases: Seq[DynDoc] =
-      BWMongoDB3.phases.find(Map("_id" -> Map($not -> Map($in -> phaseOids))))
-    val orphanedPhaseOids: Seq[ObjectId] = orphanedPhases.map(_._id[ObjectId])
+    val expectedPhaseOids: Many[ObjectId] = projects.flatMap(_.phase_ids[Many[ObjectId]]).distinct
+    response.getWriter.println(s"\tExpected phase Oids (${expectedPhaseOids.length}): " +
+        expectedPhaseOids.mkString(", "))
+    val existingPhases: Seq[DynDoc] = BWMongoDB3.phases.find()
+    val existingPhaseOids: Seq[ObjectId] = existingPhases.map(_._id[ObjectId])
+    val orphanedPhaseOids: Seq[ObjectId] = existingPhaseOids.filterNot(expectedPhaseOids.toSet.contains)
     response.getWriter.println(s"\tOrphaned phase Oids (${orphanedPhaseOids.length}): " +
       orphanedPhaseOids.mkString(", "))
+    val missingPhaseOids: Seq[ObjectId] = expectedPhaseOids.filterNot(existingPhaseOids.toSet.contains)
+    response.getWriter.println(s"\tMissing phase Oids (${missingPhaseOids.length}): " +
+      missingPhaseOids.mkString(", "))
     response.getWriter.println(s"EXIT deleteOrphanedPhases()\n\n")
   }
 
-  private def deleteOrphanedProcesses(request: HttpServletRequest, response: HttpServletResponse, go: Boolean): Unit = {
+  private def deleteOrphanedProcesses(request: HttpServletRequest, response: HttpServletResponse, go: Boolean):
+      Unit = {
     response.getWriter.println(s"ENTRY deleteOrphanedProcesses()")
     val phases: Seq[DynDoc] = BWMongoDB3.phases.find()
-    val processOids: Many[ObjectId] = phases.flatMap(_.process_ids[Many[ObjectId]]).distinct
-    response.getWriter.println(s"""\tValid process Oids (${processOids.length}): ${processOids.mkString(", ")}""")
-    val orphanedProcesses: Seq[DynDoc] =
-      BWMongoDB3.processes.find(Map("_id" -> Map($not -> Map($in -> processOids))))
-    val orphanedProcessOids: Seq[ObjectId] = orphanedProcesses.map(_._id[ObjectId])
+    val expectedProcessOids: Many[ObjectId] = phases.flatMap(_.process_ids[Many[ObjectId]]).distinct
+    response.getWriter.println(s"\tExpected process Oids (${expectedProcessOids.length}): " +
+        expectedProcessOids.mkString(", "))
+    val existingProcesses: Seq[DynDoc] = BWMongoDB3.processes.find()
+    val existingProcessOids: Seq[ObjectId] = existingProcesses.map(_._id[ObjectId])
+    val orphanedProcessOids: Seq[ObjectId] = existingProcessOids.filterNot(expectedProcessOids.toSet.contains)
     response.getWriter.println(s"\tOrphaned process Oids (${orphanedProcessOids.length}): " +
       orphanedProcessOids.mkString(", "))
+    val missingProcessOids: Seq[ObjectId] = expectedProcessOids.filterNot(existingProcessOids.toSet.contains)
+    response.getWriter.println(s"\tMissing process Oids (${missingProcessOids.length}): " +
+      missingProcessOids.mkString(", "))
     response.getWriter.println(s"EXIT deleteOrphanedProcesses()\n\n")
   }
 
@@ -41,19 +50,24 @@ object OrphanRecordsDelete extends HttpUtils {
       Unit = {
     response.getWriter.println("ENTRY deleteOrphanedActivities()")
     val processes: Seq[DynDoc] = BWMongoDB3.processes.find()
-    val activityOids: Many[ObjectId] = processes.flatMap(_.activity_ids[Many[ObjectId]]).distinct
-    response.getWriter.println(s"""\tValid activity Oids (${activityOids.length}): ${activityOids.mkString(", ")}""")
-    val orphanedActivities: Seq[DynDoc] =
-        BWMongoDB3.activities.find(Map("_id" -> Map($not -> Map($in -> activityOids))))
-    val orphanedActivityOids: Seq[ObjectId] = orphanedActivities.map(_._id[ObjectId])
+    val expectedActivityOids: Many[ObjectId] = processes.flatMap(_.activity_ids[Many[ObjectId]]).distinct
+    response.getWriter.println(s"\tExpected activity Oids (${expectedActivityOids.length}): " +
+        expectedActivityOids.mkString(", "))
+    val existingActivities: Seq[DynDoc] = BWMongoDB3.activities.find()
+    val existingActivityOids: Seq[ObjectId] = existingActivities.map(_._id[ObjectId])
+    val orphanedActivityOids: Seq[ObjectId] = existingActivityOids.filterNot(expectedActivityOids.toSet.contains)
     response.getWriter.println(s"\tOrphaned activity Oids (${orphanedActivityOids.length}): " +
         orphanedActivityOids.mkString(", "))
+    val missingActivityOids: Seq[ObjectId] = expectedActivityOids.filterNot(existingActivityOids.toSet.contains)
+    response.getWriter.println(s"\tMissing activity Oids (${missingActivityOids.length}): " +
+      missingActivityOids.mkString(", "))
     response.getWriter.println("EXIT deleteOrphanedActivities()\n\n")
   }
 
-  private def deleteOrphanedDeliverables(request: HttpServletRequest, response: HttpServletResponse, go: Boolean): Unit = {
+  private def deleteOrphanedDeliverables(request: HttpServletRequest, response: HttpServletResponse, go: Boolean):
+      Unit = {
     response.getWriter.println(s"ENTRY deleteOrphanedDeliverables()")
-    response.getWriter.println(s"EXIT deleteOrphanedDeliverables()")
+    response.getWriter.println(s"EXIT deleteOrphanedDeliverables()\t\t")
   }
 
   def main(request: HttpServletRequest, response: HttpServletResponse, args: Array[String]): Unit = {

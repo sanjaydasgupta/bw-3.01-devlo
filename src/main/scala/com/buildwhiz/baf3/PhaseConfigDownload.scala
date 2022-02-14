4abb825b9df8bfeb4eee6f5f9284ff499ced81ad
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2022-02-14 18:56:28 +0530

    PhaseConfigDownload: periodic update

--- src/main/scala/com/buildwhiz/baf3/PhaseConfigDownload.scala
+++ src/main/scala/com/buildwhiz/baf3/PhaseConfigDownload.scala
@@ -5,14 +5,13 @@ import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
-import org.apache.poi.ss.usermodel.{HorizontalAlignment, VerticalAlignment}
+import org.apache.poi.ss.usermodel.{FillPatternType, HorizontalAlignment, IndexedColors, VerticalAlignment}
 import org.apache.poi.xssf.usermodel.{XSSFColor, XSSFSheet, XSSFWorkbook}
 import org.bson.Document
 import org.bson.types.ObjectId
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.{CTCol, CTColor}
 
 import scala.jdk.CollectionConverters._
-
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class PhaseConfigDownload extends HttpServlet with HttpUtils {
@@ -36,38 +35,51 @@ class PhaseConfigDownload extends HttpServlet with HttpUtils {
     }
   }
 
-  private def addDeliverableRows(taskSheet: XSSFSheet, activityOid: ObjectId, request: HttpServletRequest): Int = {
+  private def addConstraintRows(taskSheet: XSSFSheet, deliverableOid: ObjectId, request: HttpServletRequest): Unit = {
+    BWLogger.log(getClass.getName, request.getMethod, "addConstraintRows(): ENTRY", request)
+    val cellStyle = taskSheet.getWorkbook.createCellStyle()
+    cellStyle.setAlignment(HorizontalAlignment.CENTER)
+    cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
+    cellStyle.setFillForegroundColor(IndexedColors.PALE_BLUE.index)
+    for (n <- Seq(1, 3)) {
+      val row = taskSheet.createRow(taskSheet.getLastRowNum + 1)
+      Seq.range(0, 6).foreach(row.createCell(_).setCellValue("--"))
+      row.createCell(6).setCellValue(2 + 3 * n)
+      row.createCell(7).setCellValue(5 * (n - 2))
+      row.setRowStyle(cellStyle)
+    }
+    BWLogger.log(getClass.getName, request.getMethod, "addConstraintRows(): EXIT", request)
+  }
+
+  private def addDeliverableRows(taskSheet: XSSFSheet, activityOid: ObjectId, request: HttpServletRequest): Unit = {
     BWLogger.log(getClass.getName, request.getMethod, "addDeliverableRows(): ENTRY", request)
     val cellStyle = taskSheet.getWorkbook.createCellStyle()
     cellStyle.setAlignment(HorizontalAlignment.CENTER)
-    val rgb: Array[Byte] = Seq(0f, 0f, 255).map(_.toByte).toArray
-    cellStyle.setFillBackgroundColor(new XSSFColor(rgb))
+    cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
+    cellStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.index)
     val deliverables = DeliverableApi.deliverablesByActivityOids(Seq(activityOid))
     for (deliverable <- deliverables) {
       val row = taskSheet.createRow(taskSheet.getLastRowNum + 1)
-      row.createCell(0).setCellValue("--")
-      row.createCell(1).setCellValue("--")
-      row.createCell(2).setCellValue("--")
+      Seq.range(0, 3).foreach(row.createCell(_).setCellValue("--"))
       row.createCell(3).setCellValue(deliverable.name[String])
       row.createCell(4).setCellValue(deliverable.deliverable_type[String])
       row.createCell(5).setCellValue(deliverable.duration[Int])
-      row.createCell(6).setCellValue(deliverable.status[String])
       row.setRowStyle(cellStyle)
+      addConstraintRows(taskSheet, deliverable._id[ObjectId], request)
     }
-    BWLogger.log(getClass.getName, request.getMethod, "addDeliverableRows(): ENTRY", request)
-    deliverables.length
+    BWLogger.log(getClass.getName, request.getMethod, "addDeliverableRows(): EXIT", request)
   }
 
-  private def addTasksSheet(workbook: XSSFWorkbook, activities: Seq[DynDoc], request: HttpServletRequest): Int = {
+  private def addTasksSheet(workbook: XSSFWorkbook, activities: Seq[DynDoc], request: HttpServletRequest): Unit = {
     BWLogger.log(getClass.getName, request.getMethod, "addTasksSheet(): ENTRY", request)
     val taskSheet = workbook.createSheet("Tasks")
-    val headerInfo = Seq(("Activity-Name", 60), ("A-ID", 20), ("A-Status", 20), ("Deliverable-Name", 60), ("D-Type", 20),
-      ("D-Duration", 20), ("D-Status", 60))
+    val headerInfo = Seq(("Activity-Name", 60), ("A-ID", 40), ("Takt#", 20), ("Deliverable-Name", 60), ("D-Type", 20),
+      ("D-Duration", 20), ("Const-Row#", 20), ("Const-Delay", 20))
     makeHeaderRow(taskSheet, headerInfo)
     val cellStyle = taskSheet.getWorkbook.createCellStyle()
     cellStyle.setAlignment(HorizontalAlignment.CENTER)
-    val rgb: Array[Byte] = Seq(0, 0, 255).map(_.toByte).toArray
-    cellStyle.setFillBackgroundColor(new XSSFColor(rgb))
+    cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND)
+    cellStyle.setFillForegroundColor(IndexedColors.BLUE.index)
     for (activity <- activities) {
       val row = taskSheet.createRow(taskSheet.getLastRowNum + 1)
       val activityName = activity.get[String]("full_path_name") match {
@@ -77,15 +89,14 @@ class PhaseConfigDownload extends HttpServlet with HttpUtils {
       val activityOid = activity._id[ObjectId]
       row.createCell(0).setCellValue(activityName)
       row.createCell(1).setCellValue(activityOid.toString)
-      row.createCell(2).setCellValue(activity.status[String])
+      row.createCell(2).setCellValue(activity.getOrElse[Int]("takt_unit_no", 1))
       row.setRowStyle(cellStyle)
       addDeliverableRows(taskSheet, activityOid, request)
     }
     BWLogger.log(getClass.getName, request.getMethod, "addTasksSheet(): ENTRY", request)
-    taskSheet.getLastRowNum + 1
   }
 
-  private def addVariablesSheet(workbook: XSSFWorkbook, process: DynDoc, bpmnName: String): Int = {
+  private def addVariablesSheet(workbook: XSSFWorkbook, process: DynDoc, bpmnName: String): Unit = {
     val variablesSheet = workbook.createSheet("Variables")
     val headerInfo = Seq(("Variable-Name", 60), ("Type", 20), ("Value", 20))
     makeHeaderRow(variablesSheet, headerInfo)
@@ -100,10 +111,9 @@ class PhaseConfigDownload extends HttpServlet with HttpUtils {
       row.createCell(2).setCellValue(s"$value")
       //val buffer = s"VARIABLE, $variableName ($projectName/$processName), $vType, $value"
     }
-    variablesSheet.getLastRowNum + 1
   }
 
-  private def addTimersSheet(workbook: XSSFWorkbook, process: DynDoc, bpmnName: String): Int = {
+  private def addTimersSheet(workbook: XSSFWorkbook, process: DynDoc, bpmnName: String): Unit = {
     val timersSheet = workbook.createSheet("Timers")
     val headerInfo = Seq(("Timer-Name", 60), ("Type", 20), ("Value", 20))
     makeHeaderRow(timersSheet, headerInfo)
@@ -115,7 +125,6 @@ class PhaseConfigDownload extends HttpServlet with HttpUtils {
       row.createCell(1).setCellValue("DURATION")
       row.createCell(2).setCellValue(timer.duration[String])
     }
-    timersSheet.getLastRowNum + 1
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
@@ -127,15 +136,15 @@ class PhaseConfigDownload extends HttpServlet with HttpUtils {
       val activities = PhaseApi.allActivities(Right(phase))
       BWLogger.log(getClass.getName, request.getMethod, s"Activity count: ${activities.length}", request)
       val workbook = new XSSFWorkbook()
-      //val process: DynDoc = BWMongoDB3.processes.find(Map("_id" -> phaseOid)).head
-      val taskNbr = addTasksSheet(workbook, activities, request)
-      //val varNbr = addVariablesSheet(workbook, process, bpmnName)
-      //val timerNbr = addTimersSheet(workbook, process, bpmnName)
+      addTasksSheet(workbook, activities, request)
+      val process: DynDoc = PhaseApi.allProcesses(phase).head
+      val bpmnName = process.bpmn_name[String]
+      addVariablesSheet(workbook, process, bpmnName)
+      addTimersSheet(workbook, process, bpmnName)
       workbook.write(response.getOutputStream)
       response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
       response.setStatus(HttpServletResponse.SC_OK)
-      BWLogger.log(getClass.getName, request.getMethod,
-        s"EXIT-OK ($taskNbr tasks)", request)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)

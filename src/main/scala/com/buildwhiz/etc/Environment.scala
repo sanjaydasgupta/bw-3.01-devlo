f07d0ab2f92bf99256359f3bcd49017aaecc0024
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2022-03-07 08:29:40 +0530

    Environment: added facility to enable data export

--- src/main/scala/com/buildwhiz/etc/Environment.scala
+++ src/main/scala/com/buildwhiz/etc/Environment.scala
@@ -48,6 +48,48 @@ class Environment extends HttpServlet with RestUtils {
     response.setContentType("application/json")
   }
 
+  private def reportData(envVariable: String, response: HttpServletResponse): Unit = {
+    response.setContentType("text/html")
+    val writer = response.getWriter
+    case class FieldSpec(name: String, asString: Any => String)
+    def personData(): Unit = {
+      val fields = Seq[FieldSpec](FieldSpec("_id", _.toString), FieldSpec("first_name", _.toString),
+        FieldSpec("last_name", _.toString), FieldSpec("organization_id", _.toString),
+        FieldSpec("emails", _.asInstanceOf[Many[Document]].map(em => s"${em.email[String]} (${em.`type`[String]})").mkString(",")),
+        FieldSpec("phones", _.asInstanceOf[Many[Document]].map(ph => s"${ph.phone[String]} (${ph.`type`[String]})").mkString(",")),
+        FieldSpec("rating", _.toString), FieldSpec("skills", _.asInstanceOf[Many[_]].mkString(",")),
+        FieldSpec("years_experience", _.toString))
+      writer.println(fields.map(_.name).mkString("<tr><td>", "</td><td>", "</td></tr>"))
+      val orgs: Seq[DynDoc] = BWMongoDB3.persons.find()
+      for (org <- orgs) {
+        val tds = fields.map(f => (f.name, f.asString(org.asDoc.get(f.name))))
+        writer.println(tds.map(td => td._2).mkString("<tr><td>", "</td><td>", "</td></tr>"))
+      }
+    }
+    def orgData(): Unit = {
+      val fields = Seq[FieldSpec](FieldSpec("_id", _.toString), FieldSpec("name", _.toString),
+        FieldSpec("active", _.toString), FieldSpec("areas_of_operation", _.asInstanceOf[Many[_]].mkString(",")),
+        FieldSpec("rating", _.toString), FieldSpec("skills", _.asInstanceOf[Many[_]].mkString(",")),
+        FieldSpec("years_experience", _.toString))
+      writer.println(fields.map(_.name).mkString("<tr><td>", "</td><td>", "</td></tr>"))
+      val orgs: Seq[DynDoc] = BWMongoDB3.organizations.find()
+      for (org <- orgs) {
+        val tds = fields.map(f => (f.name, f.asString(org.asDoc.get(f.name))))
+        writer.println(tds.map(td => td._2).mkString("<tr><td>", "</td><td>", "</td></tr>"))
+      }
+    }
+    writer.println("<html><body>")
+    writer.println("<h2>Organizations</h2>")
+    writer.println("""<table id="organizations" border="1">""")
+    orgData()
+    writer.println("</table>")
+    writer.println("<h2>Persons</h2>")
+    writer.println("""<table id="persons" border="1">""")
+    personData()
+    writer.println("</table>")
+    writer.println("</body></html>")
+  }
+
   private def reportEnvironment(envVariable: String, response: HttpServletResponse): Unit = {
     sys.env.get(envVariable) match {
       case Some(envValue) =>
@@ -66,6 +108,8 @@ class Environment extends HttpServlet with RestUtils {
       reportErrors(parameterMap("minutes"), response)
     } else if (parameterMap.contains("environment")) {
         reportEnvironment(parameterMap("environment"), response)
+    } else if (parameterMap.contains("data")) {
+      reportData(parameterMap("data"), response)
     } else {
       doCookies(request, response)
     }

d95f5c7c334308f6fb0660e024941d8f4280db9c
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2022-08-05 22:55:07 +0530

    Status: updated to provide detailed breakup by time-windows

--- src/main/scala/com/buildwhiz/etc/Status.scala
+++ src/main/scala/com/buildwhiz/etc/Status.scala
@@ -9,25 +9,41 @@ import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class Status extends HttpServlet with RestUtils {
 
-  private def sendStatus(errorMinutes: String, response: HttpServletResponse): Unit = {
-    try {
-      val since = System.currentTimeMillis - (errorMinutes.toLong * 60 * 1000)
-      val logInfo: Seq[DynDoc] = BWMongoDB3.trace_log.find(Map("milliseconds" -> Map("$gte" -> since)))
-      val totalCount = logInfo.length
-      val errorCount = logInfo.count(_.event_name[String].contains("ERROR"))
-      response.getWriter.println(s"""{"total": $totalCount, "bad": $errorCount, "minutes": $errorMinutes}""")
-    } catch {
-      case _: Throwable =>
-        response.getWriter.println(s"""{"total": -1, "errors": -1, "error_minutes": $errorMinutes}""")
-    }
+  private def sendStatus(response: HttpServletResponse): Unit = {
     response.setContentType("application/json")
+    try {
+      val matcher: DynDoc = Map("$match" -> Map(
+        "milliseconds" -> Map($gte -> (System.currentTimeMillis - 60L * 60 * 1000)),
+        "event_name" -> Map($regex -> "^EXIT.*"),
+        "variables.u$nm" -> Map($not -> Map($regex -> "^Sanjay.*"))
+      ))
+      val diff = Map("$subtract" -> Seq(System.currentTimeMillis, "$milliseconds"))
+      val grouper: DynDoc = Map("$group" -> Map(
+        "_id" -> Map("$ceil" -> Map("$divide" -> Seq(diff, 60000))),
+        "count" -> Map("$sum" -> 1),
+        "errors" -> Map("$sum" -> 0)
+      ))
+      val logInfo: Seq[DynDoc] = BWMongoDB3.trace_log.aggregate(Seq(matcher.asDoc, grouper.asDoc))
+      val under60 = logInfo.filter(_._id[Double] > 30).map(_.count[Int]).sum
+      val under30 = logInfo.filter(info => info._id[Double] <= 30 && info._id[Double] > 15).map(_.count[Int]).sum
+      val under15 = logInfo.filter(info => info._id[Double] <= 15 && info._id[Double] > 5).map(_.count[Int]).sum
+      val under5 = logInfo.filter(_._id[Double] <= 5).map(_.count[Int]).sum
+      val totalCount = under5 + under15
+      val errorCount = logInfo.map(_.errors[Int]).sum
+      val fields: DynDoc = Map("total" -> totalCount, "bad" -> errorCount, "under60" -> under60, "under30" -> under30,
+        "under15" -> under15, "under5" -> under5)
+      response.getWriter.println(fields.asDoc.toJson)
+    } catch {
+      case t: Throwable =>
+        response.getWriter.println(s"""{"total": -1, "errors": -1, "error_minutes": 0}""")
+        throw t
+    }
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     //BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
 
-    val parameterMap = getParameterMap(request)
-    sendStatus(parameterMap.getOrElse("minutes", "15"), response)
+    sendStatus(response)
     //BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)
   }
 

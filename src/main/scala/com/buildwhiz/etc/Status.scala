e12c4a77b1bc08f4f6c76599d77870a6107901b3
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2022-08-06 08:40:56 +0530

    Status: further updates to incorporate error count

--- src/main/scala/com/buildwhiz/etc/Status.scala
+++ src/main/scala/com/buildwhiz/etc/Status.scala
@@ -14,21 +14,22 @@ class Status extends HttpServlet with RestUtils {
     try {
       val matcher: DynDoc = Map("$match" -> Map(
         "milliseconds" -> Map($gte -> (System.currentTimeMillis - 60L * 60 * 1000)),
-        "event_name" -> Map($regex -> "^EXIT.*"),
+        "event_name" -> Map($regex -> "^(EXIT[ -]|ERROR:).*"),
+        "process_id" -> Map($not -> Map($regex -> "baf2.Login")),
         "variables.u$nm" -> Map($not -> Map($regex -> "^Sanjay.*"))
       ))
       val diff = Map("$subtract" -> Seq(System.currentTimeMillis, "$milliseconds"))
       val grouper: DynDoc = Map("$group" -> Map(
         "_id" -> Map("$ceil" -> Map("$divide" -> Seq(diff, 60000))),
         "count" -> Map("$sum" -> 1),
-        "errors" -> Map("$sum" -> 0)
+        "errors" -> Map("$sum" -> Map("$cond" -> Map("if" -> Map("$regexMatch" -> Map("input" -> "$event_name", "regex" -> "^(ERROR:|EXIT-ERROR).+")), "then" -> 1, "else" -> 0)))
       ))
       val logInfo: Seq[DynDoc] = BWMongoDB3.trace_log.aggregate(Seq(matcher.asDoc, grouper.asDoc))
       val under60 = logInfo.filter(_._id[Double] > 30).map(_.count[Int]).sum
       val under30 = logInfo.filter(info => info._id[Double] <= 30 && info._id[Double] > 15).map(_.count[Int]).sum
       val under15 = logInfo.filter(info => info._id[Double] <= 15 && info._id[Double] > 5).map(_.count[Int]).sum
       val under5 = logInfo.filter(_._id[Double] <= 5).map(_.count[Int]).sum
-      val totalCount = under5 + under15
+      val totalCount = logInfo.map(_.count[Int]).sum
       val errorCount = logInfo.map(_.errors[Int]).sum
       val fields: DynDoc = Map("total" -> totalCount, "bad" -> errorCount, "under60" -> under60, "under30" -> under30,
         "under15" -> under15, "under5" -> under5)

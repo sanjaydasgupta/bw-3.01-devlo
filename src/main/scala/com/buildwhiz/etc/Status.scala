12e26cfad0513713ae8737d3296cef0f97ee8647
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2022-08-17 09:52:03 +0530

    Status: progressive updates

--- src/main/scala/com/buildwhiz/etc/Status.scala
+++ src/main/scala/com/buildwhiz/etc/Status.scala
@@ -5,12 +5,12 @@ import com.buildwhiz.baf2.PersonApi
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.BWMongoDB3._
-
+import com.buildwhiz.utils.DateTimeUtils
 import org.bson.Document
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
-class Status extends HttpServlet with RestUtils {
+class Status extends HttpServlet with RestUtils with DateTimeUtils {
 
   private def matcher(durationInMinutes: Int, userName: String): Document = {
     val output: DynDoc = Map("$match" -> Map(
@@ -34,21 +34,25 @@ class Status extends HttpServlet with RestUtils {
     output.asDoc
   }
 
-  private def sendStatusPage(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+  private val sortOnId: Document = Map("$sort" -> Map("_id" -> 1))
+
+  private def sendStatusPage(tz: String, response: HttpServletResponse): Unit = {
     val t0 = System.currentTimeMillis()
     response.setContentType("text/html")
     val writer = response.getWriter
     val (duration, pitch) = (240, 15)
     val logInfo: Seq[DynDoc] = BWMongoDB3.trace_log.
-        aggregate(Seq(matcher(duration, "Anon Anon"), grouper(pitch)))
-    writer.println("""<html><body><table border="1" align="center">""")
-    writer.println(s"<tr><td>ID</td><td>Count</td><td>Errors</td></tr>")
+        aggregate(Seq(matcher(duration, "Anon Anon"), grouper(pitch), sortOnId))
+    writer.println("""<html><body><table border="1">""")
+    writer.println("""<tr><td align="center">Time</td><td>Count</td><td>Errors</td></tr>""")
     for (info <- logInfo) {
       val (id, count, errors) = (info._id[Double], info.count[Int], info.errors[Int])
-      writer.println(s"<tr><td>$id</td><td>$count</td><td>$errors</td></tr>")
+      val ms = t0 - id.toLong * pitch * 60000L
+      val time = dateTimeString(ms, Some(tz))
+      writer.println(s"""<tr><td>$time</td><td align="center">$count</td><td align="center">$errors</td></tr>""")
     }
     val delay = System.currentTimeMillis() - t0
-    writer.println(s"""<tr><td colspan="3">time=$delay</td></tr>""")
+    writer.println(s"""<tr><td colspan="3" align="center">time=$delay</td></tr>""")
     writer.println("</table></body></html>")
   }
 
@@ -62,7 +66,7 @@ class Status extends HttpServlet with RestUtils {
       "^" + PersonApi.fullName(user) + ".*$"
     }
     try {
-      val logInfo: Seq[DynDoc] = BWMongoDB3.trace_log.aggregate(Seq(matcher(60, userName), grouper(1)))
+      val logInfo: Seq[DynDoc] = BWMongoDB3.trace_log.aggregate(Seq(matcher(60, userName), grouper(1), sortOnId))
       val under60 = logInfo.filter(_._id[Double] > 30).map(_.count[Int]).sum
       val under30 = logInfo.filter(info => info._id[Double] <= 30 && info._id[Double] > 15).map(_.count[Int]).sum
       val under15 = logInfo.filter(info => info._id[Double] <= 15 && info._id[Double] > 5).map(_.count[Int]).sum
@@ -83,10 +87,17 @@ class Status extends HttpServlet with RestUtils {
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     //BWLogger.log(getClass.getName, request.getMethod, s"ENTRY", request)
     val parameters = getParameterMap(request)
-    if (parameters.contains("detail")) {
-      sendStatusPage(request, response)
-    } else {
-      sendStatus(request, response)
+    (parameters.get("detail"), parameters.get("tz")) match {
+      case (Some(_), Some(tz)) => sendStatusPage(tz, response)
+      case (Some(_), None) =>
+        val user = getUser(request)
+        val tz = if (user == null) {
+          "GMT"
+        } else {
+          user.get("tz").asInstanceOf[String]
+        }
+        sendStatusPage(tz, response)
+      case (None, _) => sendStatus(request, response)
     }
     //BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK", request)
   }

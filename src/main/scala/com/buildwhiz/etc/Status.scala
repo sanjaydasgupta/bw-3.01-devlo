69db3b76dc70a9a4de557ac9cdfa36fc43f268d5
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2022-08-24 15:49:25 +0530

    Status: added display of usernames will access and error counts

--- src/main/scala/com/buildwhiz/etc/Status.scala
+++ src/main/scala/com/buildwhiz/etc/Status.scala
@@ -29,7 +29,7 @@ class Status extends HttpServlet with RestUtils with DateTimeUtils {
     val output: DynDoc = Map("$group" -> Map(
       "_id" -> Map("$ceil" -> Map("$divide" -> Seq(ageInMs, pitchInMinutes * 60000))),
       "users" -> Map("$push" -> "$variables.u$nm"),
-      "errors" -> Map("$sum" -> Map("$cond" -> Map("if" -> isError, "then" -> 1, "else" -> 0)))
+      "errors" -> Map("$push" -> Map("$cond" -> Map("if" -> isError, "then" -> 1, "else" -> 0)))
     ))
     output.asDoc
   }
@@ -37,6 +37,15 @@ class Status extends HttpServlet with RestUtils with DateTimeUtils {
   private val sortOnId: Document = Map("$sort" -> Map("_id" -> 1))
 
   private def sendStatusPage(tz: String, userName: String, detail: String, response: HttpServletResponse): Unit = {
+    def cleanUserName(userName: String): String = {
+      val userNameParts = userName.split("\\s+")
+      if (userNameParts.last.matches("\\([0-9a-f]{24}\\)")) {
+        userNameParts.init.mkString(" ")
+      } else {
+        userNameParts.mkString(" ")
+      }
+    }
+
     val t0 = System.currentTimeMillis()
     response.setContentType("text/html")
     val writer = response.getWriter
@@ -50,15 +59,21 @@ class Status extends HttpServlet with RestUtils with DateTimeUtils {
     val logInfo: Seq[DynDoc] = BWMongoDB3.trace_log.
         aggregate(Seq(matcher(duration, userName), grouper(pitch), sortOnId))
     writer.println("""<html><body><table border="1">""")
-    writer.println("""<tr><td align="center">Time</td><td>Count</td><td>Errors</td></tr>""")
+    writer.println("""<tr><td align="center">Time</td><td align="center">Users</td></tr>""")
     for (info <- logInfo) {
-      val (id, count, errors) = (info._id[Double], info.users[Many[String]].size(), info.errors[Int])
+      val (id, users, errors) = (info._id[Double], info.users[Many[String]], info.errors[Many[Int]])
       val ms = t0 - id.toLong * pitch * 60000L
       val time = dateTimeString(ms, Some(tz))
-      writer.println(s"""<tr><td>$time</td><td align="center">$count</td><td align="center">$errors</td></tr>""")
+      val userDetail = users.zip(errors).groupBy(p => cleanUserName(p._1)).map(p => {
+        val name = p._1
+        val totalCount = p._2.length
+        val errorCount = p._2.map(_._2).sum
+        s"$name ($totalCount:$errorCount)"
+      })
+      writer.println(s"""<tr><td>$time</td><td align="center">${userDetail.mkString(", ")}</td></tr>""")
     }
     val delay = System.currentTimeMillis() - t0
-    writer.println(s"""<tr><td colspan="3" align="center">time=$delay</td></tr>""")
+    writer.println(s"""<tr><td colspan="2" align="center">time=$delay</td></tr>""")
     writer.println("</table></body></html>")
   }
 
@@ -74,7 +89,7 @@ class Status extends HttpServlet with RestUtils with DateTimeUtils {
           map(_.users[Many[String]].size()).sum
       val under5 = logInfo.filter(_._id[Double] <= 5).map(_.users[Many[String]].size()).sum
       val totalCount = logInfo.map(_.users[Many[String]].size()).sum
-      val errorCount = logInfo.map(_.errors[Int]).sum
+      val errorCount = logInfo.map(_.errors[Many[Int]].sum).sum
       val delay = System.currentTimeMillis() - t0
       val fields: DynDoc = Map("total" -> totalCount, "bad" -> errorCount, "under60" -> under60, "under30" -> under30,
         "under15" -> under15, "under5" -> under5, "time" -> delay)

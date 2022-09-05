ddb7efa0977978bf6cc55d6119ad9945f08e3d66
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2022-09-05 08:57:53 +0530

    ProjectCreate: the 'document_tags' array now contains Object-Ids instead of strings

--- src/main/scala/com/buildwhiz/baf3/ProjectCreate.scala
+++ src/main/scala/com/buildwhiz/baf3/ProjectCreate.scala
@@ -4,13 +4,34 @@ import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
 import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
+
+import com.mongodb.client.model.InsertOneModel
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 import org.bson.Document
 import org.bson.types.ObjectId
-
 import com.buildwhiz.baf2.{OrganizationApi, PersonApi, ProjectApi}
 
+import scala.jdk.CollectionConverters._
+
 class ProjectCreate extends HttpServlet with HttpUtils {
+
+  private def createProjectTags(projectOid: ObjectId): Unit = {
+    val projectTags = Seq("Architecture", "Contract", "Current-Plan", "EIR", "Geotech", "HRE", "Invoice",
+        "Land-Use", "Meeting-Notes", "Other", "Pre-App-Meeting", "Preservation-Alternatives", "Public-Health",
+        "Report", "Soils-Report", "Survey", "Traffic Study", "Wind-Study")
+    val bulkWriteBuffer: Seq[InsertOneModel[Document]] = projectTags.map(tag =>
+      new InsertOneModel(new Document("L1", tag).append("project_id", projectOid).append("__v", 0)))
+    val bulkWriteResult = BWMongoDB3.project_tags.bulkWrite(bulkWriteBuffer.asJava)
+    if (bulkWriteResult.getInsertedCount != projectTags.length) {
+      throw new IllegalArgumentException(s"FAILED to insert projectTags: $bulkWriteResult")
+    }
+    val tagOids: Many[ObjectId] = bulkWriteResult.getInserts.map(bwi => bwi.getId.asObjectId().getValue)
+    val updateResult = BWMongoDB3.projects.updateOne(Map("_id" -> projectOid),
+        Map($set -> Map("document_tags" -> tagOids)))
+    if (updateResult.getModifiedCount != 1) {
+      throw new IllegalArgumentException(s"FAILED to set document_tags ($updateResult) in project record $projectOid")
+    }
+  }
 
   private def doPostTransaction(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
@@ -47,21 +68,18 @@ class ProjectCreate extends HttpServlet with HttpUtils {
         "country" -> Map("name" -> "United States", "code" -> "US"), "postal_code" -> "94102",
         "gps_location" -> Map("latitude" -> 37.7857971, "longitude" -> -122.4142195))
 
-      val systemTags = Seq("Architecture", "Contract", "Current-Plan", "EIR", "Geotech", "HRE", "Invoice",
-        "Land-Use", "Meeting-Notes", "Other", "Pre-App-Meeting", "Preservation-Alternatives", "Public-Health",
-        "Report", "Soils-Report", "Survey", "Traffic Study", "Wind-Study").
-        map(tag => new Document("name", tag))
-
       val assignedRoles = Seq(Map("role_name" -> "Project-Manager", "person_id" -> adminPersonOid))
       val projectDocument: Document = Map("name" -> projectName, "summary" -> projectName, "description" -> description,
         "admin_person_id" -> adminPersonOid, "type" -> "Building", "construction_type" -> "Concrete",
         "budget_mm_usd" -> 0.0, "construction_area_sqft" -> 0.0, "land_area_acres" -> 0.0,
         "building_use" -> "Mixed-Use Facility",
         "max_building_height_ft" -> 0.0, "address" -> address, "process_ids" -> Seq.empty[ObjectId],
-        "phase_ids" -> Seq.empty[ObjectId], "assigned_roles" -> assignedRoles, "document_tags"-> systemTags,
+        "phase_ids" -> Seq.empty[ObjectId], "assigned_roles" -> assignedRoles,
         "timestamps" -> Map("created" -> System.currentTimeMillis), "total_floor_area" -> 0.0,
         "status" -> "defined", "customer_organization_id" -> customerOid, "tz" -> "GMT")
       BWMongoDB3.projects.insertOne(projectDocument)
+      val projectOid = projectDocument.get("_id").asInstanceOf[ObjectId]
+      createProjectTags(projectOid)
 
       response.getWriter.print(successJson())
       response.setContentType("application/json")

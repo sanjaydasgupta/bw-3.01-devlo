af502640e496ae99815b036a860c441d62fccfad
Sanjay Dasgupta <sanjay.dasgupta@buildwhiz.com>
2022-09-07 12:05:14 +0530

    ProjectCreate: added code to create team for interim project-manager

--- src/main/scala/com/buildwhiz/baf3/ProjectCreate.scala
+++ src/main/scala/com/buildwhiz/baf3/ProjectCreate.scala
@@ -15,6 +15,13 @@ import scala.jdk.CollectionConverters._
 
 class ProjectCreate extends HttpServlet with HttpUtils {
 
+  private def createProjectTeams(projectOid: ObjectId, personOid: ObjectId, organizationOid: ObjectId): Unit = {
+    BWMongoDB3.teams.insertOne(
+        Map("project_id" -> projectOid, "team_name" -> "Default PM Team", "organization_id" -> organizationOid,
+        "group" -> "Project Management", "skill" -> Seq("Project-Manager (33-25 BW 11)"), "color" -> "#008000",
+        "team_members" -> Seq(Map("person_id" -> personOid, "roles" -> Seq("Manager"))), "__v" -> 0))
+  }
+
   private def createProjectTags(projectOid: ObjectId): Unit = {
     val projectTags = Seq("Architecture", "Contract", "Current-Plan", "EIR", "Geotech", "HRE", "Invoice",
         "Land-Use", "Meeting-Notes", "Other", "Pre-App-Meeting", "Preservation-Alternatives", "Public-Health",
@@ -38,7 +45,7 @@ class ProjectCreate extends HttpServlet with HttpUtils {
     val parameters = getParameterMap(request)
     try {
       val loggedInUser = getUser(request)
-      if (!PersonApi.fullName(loggedInUser).matches("Prabhas Admin|Sanjay Admin"))
+      if (!PersonApi.fullName(loggedInUser).matches("Prabhas Admin|Sanjay Admin|BW2 Kannektify"))
         throw new IllegalArgumentException("Not permitted")
       val user: DynDoc = getPersona(request)
       val userOid = user._id[ObjectId]
@@ -80,6 +87,7 @@ class ProjectCreate extends HttpServlet with HttpUtils {
       BWMongoDB3.projects.insertOne(projectDocument)
       val projectOid = projectDocument.get("_id").asInstanceOf[ObjectId]
       createProjectTags(projectOid)
+      createProjectTeams(projectOid, userOid, user.organization_id[ObjectId])
 
       response.getWriter.print(successJson())
       response.setContentType("application/json")

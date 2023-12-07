22f63dfe1126a0ff39950e32a6375b7286874389
Sanjay <sanjay.dasgupta@buildwhiz.com>
2023-12-07 18:28:01 +0530

    DocumentDownload, DocumentLink: changes to enable linking of remote files

--- src/main/scala/com/buildwhiz/baf3/DocumentDownload.scala
+++ src/main/scala/com/buildwhiz/baf3/DocumentDownload.scala
@@ -8,31 +8,32 @@ import org.bson.types.ObjectId
 import com.buildwhiz.baf2.{DocumentApi, ProjectApi}
 
 import java.io.InputStream
+import java.net.URL
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class DocumentDownload extends HttpServlet with HttpUtils {
 
-  private val contentDescriptions: Map[String, (String, Boolean)] = Map(
-    // file-extension -> (content-type, content-disposition-inline)
-    "bmp" -> ("image/bmp", true),
-    "doc" -> ("application/msword", false),
-    "docx" -> ("application/vnd.openxmlformats-officedocument.wordprocessingml.document", false),
-    "folder" -> ("application/zip", false),
-    "gif" -> ("image/gif", true),
-    "jpg" -> ("image/jpeg", true),
-    "jpeg" -> ("image/jpeg", true),
-    "pdf" -> ("application/pdf", true),
-    "ppt" -> ("application/vnd.ms-powerpoint", true),
-    "pptx" -> ("application/vnd.openxmlformats-officedocument.presentationml.presentation", true),
-    "svg" -> ("image/svg+xml", true),
-    "tif" -> ("image/tiff", true),
-    "tiff" -> ("image/tiff", true),
-    "txt" -> ("text/plain", true),
-    "xls" -> ("application/vnd.ms-excel", true),
-    "xlsx" -> ("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", true),
-    "xml" -> ("application/xml", false),
-    "zip" -> ("application/zip", false)
-  )
+//  private val contentDescriptions: Map[String, (String, Boolean)] = Map(
+//    // file-extension -> (content-type, content-disposition-inline)
+//    "bmp" -> ("image/bmp", true),
+//    "doc" -> ("application/msword", false),
+//    "docx" -> ("application/vnd.openxmlformats-officedocument.wordprocessingml.document", false),
+//    "folder" -> ("application/zip", false),
+//    "gif" -> ("image/gif", true),
+//    "jpg" -> ("image/jpeg", true),
+//    "jpeg" -> ("image/jpeg", true),
+//    "pdf" -> ("application/pdf", true),
+//    "ppt" -> ("application/vnd.ms-powerpoint", true),
+//    "pptx" -> ("application/vnd.openxmlformats-officedocument.presentationml.presentation", true),
+//    "svg" -> ("image/svg+xml", true),
+//    "tif" -> ("image/tiff", true),
+//    "tiff" -> ("image/tiff", true),
+//    "txt" -> ("text/plain", true),
+//    "xls" -> ("application/vnd.ms-excel", true),
+//    "xlsx" -> ("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", true),
+//    "xml" -> ("application/xml", false),
+//    "zip" -> ("application/zip", false)
+//  )
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     val parameters = getParameterMap(request)
@@ -43,7 +44,7 @@ class DocumentDownload extends HttpServlet with HttpUtils {
       val projectOid = documentRecord.project_id[ObjectId]
       if (!ProjectApi.exists(projectOid))
         throw new IllegalArgumentException(s"Bad project_id in document record: '$projectOid'")
-      val (timestamp: Long, version: Option[DynDoc]) = parameters.get("timestamp") match {
+      val (timestamp: Long, optVersion: Option[DynDoc]) = parameters.get("timestamp") match {
         case Some(ts) => (ts.toLong, documentRecord.versions[Many[Document]].find(_.timestamp == ts.toLong))
         case None =>
           val optVersion = documentRecord.versions[Many[Document]].reverse.headOption
@@ -53,26 +54,34 @@ class DocumentDownload extends HttpServlet with HttpUtils {
           }
           (timestamp, optVersion)
       }
-      version match {
+      val version: DynDoc = optVersion match {
         case None =>
           throw new IllegalArgumentException("No content for this document record")
-        case Some(v) => if (v.has("file_name")) {
-          val fileName = v.file_name[String]
+        case Some(theVersion) => if (theVersion.has("file_name")) {
+          val fileName = theVersion.file_name[String]
           val contentDisposition = s"""attachment; filename="$fileName""""
           response.setHeader("Content-Disposition", contentDisposition)
           val message = s"Set Content-Disposition='$contentDisposition'"
           BWLogger.log(getClass.getName, request.getMethod, message, request)
+          theVersion
         } else {
           val contentDisposition = s"""attachment; filename="unknown-name.tmp""""
           response.setHeader("Content-Disposition", contentDisposition)
           val message = s"Set Content-Disposition='$contentDisposition'"
           BWLogger.log(getClass.getName, request.getMethod, message, request)
+          theVersion
         }
       }
       response.setContentType("application/octet-stream")
-      val storageKey = f"$projectOid-$documentOid-$timestamp%x"
-      BWLogger.log(getClass.getName, request.getMethod, s"storage-key: $storageKey", request)
-      val inputStream: InputStream = GoogleDrive.getObject(storageKey)
+      val inputStream: InputStream = version.get[String]("link_url") match {
+        case Some(url) =>
+          BWLogger.log(getClass.getName, request.getMethod, s"Fetching URL: $url", request)
+          new URL(url).openStream()
+        case None =>
+        val storageKey = f"$projectOid-$documentOid-$timestamp%x"
+        BWLogger.log(getClass.getName, request.getMethod, s"Fetching Google-Drive storage-key: $storageKey", request)
+        GoogleDrive.getObject(storageKey)
+      }
       val outputStream = response.getOutputStream
       val buffer = new Array[Byte](4096)
       var len = inputStream.read(buffer)
---
+++ src/main/scala/com/buildwhiz/baf3/DocumentLink.scala
@@ -0,0 +1,63 @@
+package com.buildwhiz.baf3
+
+import com.buildwhiz.baf2.{DocumentApi, PersonApi}
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
+import com.buildwhiz.infra.DynDoc._
+import com.buildwhiz.utils.{BWLogger, HttpUtils}
+import org.bson.types.ObjectId
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+class DocumentLink extends HttpServlet with HttpUtils {
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
+    val parameters = getParameterMap(request)
+    try {
+      val t0 = System.currentTimeMillis()
+      val documentOid = new ObjectId(parameters("document_id"))
+      if (!DocumentApi.exists(documentOid))
+        throw new IllegalArgumentException(s"unknown document-id: $documentOid")
+
+      val user: DynDoc = getPersona(request)
+      val authorOid = parameters.get("author_id") match {
+        case Some(aId) =>
+          if (!PersonApi.isBuildWhizAdmin(Right(user)))
+            throw new IllegalArgumentException("Not permitted to provide author_id")
+          new ObjectId(aId)
+        case None => user._id[ObjectId]
+      }
+      if (!PersonApi.exists(authorOid))
+        throw new IllegalArgumentException(s"unknown author-id: ${authorOid.toString}")
+
+      val linkUrl = parameters("link_url")
+      val documentRecord: DynDoc = DocumentApi.documentById(documentOid)
+      val fileName = parameters.get("file_name") match {
+        case Some(fn) => fn
+        case None => s"""${documentRecord.name[String].replaceAll("\\s+", "-")}.${documentRecord.`type`[String]}}"""
+      }
+      val timestamp = System.currentTimeMillis
+
+      val versionComments = if (parameters.contains("comments")) parameters("comments") else "-"
+
+      val versionRecord = Map("comments" -> versionComments, "timestamp" -> timestamp, "author_person_id" -> authorOid,
+        "file_name" -> fileName, "size" -> -1, "link_url" -> linkUrl)
+      val updateResult = BWMongoDB3.document_master.
+        updateOne(Map("_id" -> documentOid), Map("$push" -> Map("versions" -> versionRecord)))
+      if (updateResult.getModifiedCount == 0)
+        throw new IllegalArgumentException(s"MongoDB update failed: $updateResult")
+
+      response.setStatus(HttpServletResponse.SC_OK)
+      val delay = System.currentTimeMillis() - t0
+      val message = s"time: $delay ms, Linked version to file ${documentRecord.name[String]}"
+      BWLogger.audit(getClass.getName, request.getMethod, message, request)
+      response.getWriter.print(successJson())
+      response.setContentType("application/json")
+    } catch {
+      case t: Throwable =>
+        BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)
+        //t.printStackTrace()
+        throw t
+    }
+  }
+}

dcf2b3bc37edf1b80eba998ae72f34575fa74067
Sanjay <sanjay.dasgupta@buildwhiz.com>
2024-01-31 17:50:33 +0530

    ProcessDelete: added elapsed time to log messages at exit

--- src/main/scala/com/buildwhiz/baf3/ProcessDelete.scala
+++ src/main/scala/com/buildwhiz/baf3/ProcessDelete.scala
@@ -1,9 +1,9 @@
 package com.buildwhiz.baf3
 
 import com.buildwhiz.baf2.ProcessApi
+import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.infra.BWMongoDB3._
 import com.buildwhiz.infra.DynDoc._
-import com.buildwhiz.infra.{BWMongoDB3, DynDoc}
 import com.buildwhiz.utils.{BWLogger, HttpUtils}
 import org.bson.types.ObjectId
 import org.bson.Document
@@ -23,6 +23,7 @@ class ProcessDelete extends HttpServlet with HttpUtils {
 
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     try {
+      val t0 = System.currentTimeMillis()
       BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
       val parameters = getParameterMap(request)
       val processOid = new ObjectId(parameters("process_id"))
@@ -30,10 +31,12 @@ class ProcessDelete extends HttpServlet with HttpUtils {
       processDelete(processOid) match {
         case Right(msg) =>
           response.getWriter.print(successJson())
-          BWLogger.audit(getClass.getName, request.getMethod, msg, request)
+          val delay = System.currentTimeMillis() - t0
+          BWLogger.audit(getClass.getName, request.getMethod, s"(time: $delay ms. $msg)", request)
         case Left(msg) =>
           response.getWriter.print(new Document("ok", 2).append("message", msg).toJson)
-          BWLogger.log(getClass.getName, request.getMethod, s"EXIT-ERROR: $msg", request)
+          val delay = System.currentTimeMillis() - t0
+          BWLogger.log(getClass.getName, request.getMethod, s"EXIT-ERROR: (time: $delay ms. $msg)", request)
       }
     } catch {
       case t: Throwable =>

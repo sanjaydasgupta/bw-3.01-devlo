b1427ff098cab344f4ad8878b87cd06ecef664e9
Sanjay <sanjay.dasgupta@buildwhiz.com>
2024-02-24 14:08:46 +0530

    DocumentDownload: added new boolean parameter 'display' to control content-disposition of file

--- src/main/scala/com/buildwhiz/baf3/DocumentDownload.scala
+++ src/main/scala/com/buildwhiz/baf3/DocumentDownload.scala
@@ -13,33 +13,35 @@ import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class DocumentDownload extends HttpServlet with HttpUtils {
 
-//  private val contentDescriptions: Map[String, (String, Boolean)] = Map(
-//    // file-extension -> (content-type, content-disposition-inline)
-//    "bmp" -> ("image/bmp", true),
-//    "doc" -> ("application/msword", false),
-//    "docx" -> ("application/vnd.openxmlformats-officedocument.wordprocessingml.document", false),
-//    "folder" -> ("application/zip", false),
-//    "gif" -> ("image/gif", true),
-//    "jpg" -> ("image/jpeg", true),
-//    "jpeg" -> ("image/jpeg", true),
-//    "pdf" -> ("application/pdf", true),
-//    "ppt" -> ("application/vnd.ms-powerpoint", true),
-//    "pptx" -> ("application/vnd.openxmlformats-officedocument.presentationml.presentation", true),
-//    "svg" -> ("image/svg+xml", true),
-//    "tif" -> ("image/tiff", true),
-//    "tiff" -> ("image/tiff", true),
-//    "txt" -> ("text/plain", true),
-//    "xls" -> ("application/vnd.ms-excel", true),
-//    "xlsx" -> ("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", true),
-//    "xml" -> ("application/xml", false),
-//    "zip" -> ("application/zip", false)
-//  )
+  private val contentDescriptions: Map[String, (String, Boolean)] = Map(
+    // file-extension -> (content-type, content-disposition-inline)
+    "bmp" -> ("image/bmp", true),
+    "doc" -> ("application/msword", false),
+    "docx" -> ("application/vnd.openxmlformats-officedocument.wordprocessingml.document", true),
+    "folder" -> ("application/zip", false),
+    "gif" -> ("image/gif", true),
+    "html" -> ("text/html", true),
+    "jpg" -> ("image/jpeg", true),
+    "jpeg" -> ("image/jpeg", true),
+    "pdf" -> ("application/pdf", true),
+    "ppt" -> ("application/vnd.ms-powerpoint", true),
+    "pptx" -> ("application/vnd.openxmlformats-officedocument.presentationml.presentation", true),
+    "svg" -> ("image/svg+xml", true),
+    "tif" -> ("image/tiff", true),
+    "tiff" -> ("image/tiff", true),
+    "txt" -> ("text/plain", true),
+    "xls" -> ("application/vnd.ms-excel", true),
+    "xlsx" -> ("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", true),
+    "xml" -> ("application/xml", true),
+    "zip" -> ("application/zip", false)
+  )
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
     val parameters = getParameterMap(request)
     BWLogger.log(getClass.getName, request.getMethod, "ENTRY", request)
     try {
       val documentOid = new ObjectId(parameters("document_id"))
+      val display = parameters.getOrElse("display", "false").toBoolean
       val documentRecord: DynDoc = DocumentApi.documentById(documentOid)
       val projectOid = documentRecord.project_id[ObjectId]
       if (!ProjectApi.exists(projectOid))
@@ -57,22 +59,42 @@ class DocumentDownload extends HttpServlet with HttpUtils {
       val version: DynDoc = optVersion match {
         case None =>
           throw new IllegalArgumentException("No content for this document record")
-        case Some(theVersion) => if (theVersion.has("file_name")) {
-          val fileName = theVersion.file_name[String]
-          val contentDisposition = s"""attachment; filename="$fileName""""
-          response.setHeader("Content-Disposition", contentDisposition)
-          val message = s"Set Content-Disposition='$contentDisposition'"
-          BWLogger.log(getClass.getName, request.getMethod, message, request)
+        case Some(theVersion) =>
+          if (theVersion.has("file_name")) {
+            val fileName = theVersion.file_name[String]
+            val fileType = fileName.split("\\.").last.toLowerCase
+            if (contentDescriptions.contains(fileType)) {
+              val contentDescription = contentDescriptions(fileType)
+              response.setContentType(contentDescription._1)
+              val contentDisposition = if (contentDescription._2 && display) {
+                "inline"
+              } else {
+                s"""attachment; filename="$fileName""""
+              }
+              response.setHeader("Content-Disposition", contentDisposition)
+              val message = s"Set Content-Type='${contentDescription._1}', Content-Disposition='$contentDisposition'"
+              BWLogger.log(getClass.getName, request.getMethod, message, request)
+            } else {
+              if (display) {
+                BWLogger.log(getClass.getName, request.getMethod, s"WARN: No content-type for file-type: $fileType", request)
+              }
+              val contentDisposition = s"""attachment; filename="$fileName""""
+              response.setHeader("Content-Disposition", contentDisposition)
+              response.setContentType("application/octet-stream")
+              BWLogger.log(getClass.getName, request.getMethod, s"WARN: No Content-Type for: $fileType", request)
+            }
           theVersion
         } else {
+          if (display) {
+            BWLogger.log(getClass.getName, request.getMethod, s"WARN: No content-type for document: $documentOid", request)
+          }
           val contentDisposition = s"""attachment; filename="unknown-name.tmp""""
           response.setHeader("Content-Disposition", contentDisposition)
-          val message = s"Set Content-Disposition='$contentDisposition'"
-          BWLogger.log(getClass.getName, request.getMethod, message, request)
+          response.setContentType("application/octet-stream")
+          BWLogger.log(getClass.getName, request.getMethod, s"WARN: No Filename for document: $documentOid", request)
           theVersion
         }
       }
-      response.setContentType("application/octet-stream")
       val inputStream: InputStream = version.get[String]("link_url") match {
         case Some(url) =>
           BWLogger.log(getClass.getName, request.getMethod, s"Fetching URL: $url", request)

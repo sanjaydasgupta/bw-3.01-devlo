2200195e7ade5760f334aa82bb9e67f63c0cc3b7
Sanjay <sanjay.dasgupta@buildwhiz.com>
2024-03-30 16:21:40 +0530

    DocCategoriesFromGoogle: added count of folders to exit log message

--- src/main/scala/com/buildwhiz/baf3/DocCategoriesFromGoogle.scala
+++ src/main/scala/com/buildwhiz/baf3/DocCategoriesFromGoogle.scala
@@ -12,9 +12,16 @@ import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class DocCategoriesFromGoogle extends HttpServlet with HttpUtils with MailUtils {
 
-  private def getFolderCategories(folderId: String): Many[Document] = {
+  private def getFolderCategories(folderId: String): (Many[Document], Int) = {
     val folders = GoogleFolderAdapter.listObjects(folderId).filter(_.mimeType == "application/vnd.google-apps.folder")
-    folders.map(f => new Document("name", f.key).append("children", getFolderCategories(f.id))).asJava
+    if (folders.nonEmpty) {
+      val childNames = folders.map(_.key)
+      val descendants = folders.map(folder => getFolderCategories(folder.id))
+      val count = folders.length + descendants.map(_._2).sum
+      (childNames.zip(descendants.map(_._1)).map(nd => new Document("name", nd._1).append("children", nd._2)).asJava, count)
+    } else {
+      (Seq.empty[Document].asJava, 0)
+    }
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
@@ -23,12 +30,12 @@ class DocCategoriesFromGoogle extends HttpServlet with HttpUtils with MailUtils 
     try {
       val t0 = System.currentTimeMillis()
       val googleFolderId = parameters("google_folder_id")
-      val returnJson = new Document("ok", 1).append("folders", getFolderCategories(googleFolderId)).toJson
-      BWLogger.log(getClass.getName, request.getMethod, "EXIT-OK", request)
+      val (categories, count) = getFolderCategories(googleFolderId)
+      val returnJson = new Document("ok", 1).append("folders", categories).toJson
       response.getWriter.print(returnJson)
       response.setContentType("application/json")
       val delay = System.currentTimeMillis() - t0
-      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (time: $delay ms)", request)
+      BWLogger.log(getClass.getName, request.getMethod, s"EXIT-OK (time: $delay ms, count: $count)", request)
     } catch {
       case t: Throwable =>
         BWLogger.log(getClass.getName, request.getMethod, s"ERROR: ${t.getClass.getSimpleName}(${t.getMessage})", request)

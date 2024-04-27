f4fc1f26190dae108e867241f27a691c6d8244ce
Sanjay <sanjay.dasgupta@buildwhiz.com>
2024-04-27 16:47:37 +0530

    DocCategoriesSave: even more intermediate commits

--- src/main/scala/com/buildwhiz/baf3/DocCategoriesSave.scala
+++ src/main/scala/com/buildwhiz/baf3/DocCategoriesSave.scala
@@ -11,21 +11,17 @@ import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class DocCategoriesSave extends HttpServlet with HttpUtils with MailUtils {
 
-  private def getCategoryKey(rec: DynDoc): String = rec.asDoc.keySet().asScala.filter(_.matches("L[0-9]+")).toSeq.
-      sorted.mkString("/")
-
-  private def getExistingCategoryKeys(teamRec: DynDoc): Set[String] = {
-    val ownCategories = teamRec.own_doc_categories[Many[Document]]
-    ownCategories.map(getCategoryKey).toSet
-  }
+  private def getCategoryKey(rec: DynDoc): String = rec.asDoc.entrySet().asScala.filter(_.getKey.matches("L[0-9]+")).
+      toSeq.sortBy(_.getKey).map(_.getValue).mkString("/")
 
   private def accessGrantRecords(teamOid: ObjectId, projectOid: ObjectId): Seq[DynDoc] = {
-    val teamOidMatcher = Map($or -> Seq(Map("owner_team_id" -> teamOid), Map("granted_team_id" -> teamOid)))
+    //val teamOidMatcher = Map($or -> Seq(Map("owner_team_id" -> teamOid), Map("granted_team_id" -> teamOid)))
+    val teamOidMatcher = Map("owner_team_id" -> teamOid)
     val aggrPipe: Seq[Document] = Seq(
       Map("$match" -> Map($and -> Seq(teamOidMatcher, Map("project_id" -> projectOid)))),
       Map("$lookup" -> Map("from" -> "teams", "localField" -> "owner_team_id", "foreignField" -> "_id", "as" -> "team")),
       Map("$unwind" -> Map("path" -> "$team", "preserveNullAndEmptyArrays" -> true)),
-      Map("$project" -> Map("doc_category_id" -> true, "granted_team_id" -> true, "owner_team_id" -> true,
+      Map("$project" -> Map("doc_category_id" -> true, "granted_team_id" -> true,
           "own_doc_categories" -> "$team.own_doc_categories"))
     )
     val grantInfos: Seq[DynDoc] = BWMongoDB3.folder_access_grants.aggregate(aggrPipe.asJava)
@@ -67,11 +63,49 @@ class DocCategoriesSave extends HttpServlet with HttpUtils with MailUtils {
       val googleFolderId = postData.google_folder_id[String]
       val folders: Seq[DynDoc] = postData.folders[Many[Document]]
 
+      def saveCategories(categories: Many[Document], optSameNames: Option[Int] = None): Unit = {
+        val updateResult = BWMongoDB3.teams.updateOne(Map("_id" -> teamOid, "project_id" -> projectOid),
+          Map($set -> Map("google_folder_id" -> googleFolderId, "own_doc_categories" -> categories)))
+        val delay = System.currentTimeMillis() - t0
+        if (updateResult.getModifiedCount == 1) {
+          val statusMessage = optSameNames match {
+            case None => s"Stored ${categories.length} categories from " +
+              s"Google folder '$googleFolderId' with team '$teamOid'"
+            case Some(dupCount) => s"Stored ${categories.length} categories ($dupCount unchanged)" +
+              s"from Google folder '$googleFolderId' with team '$teamOid'"
+          }
+          BWLogger.audit(getClass.getName, request.getMethod, "(time: $delay ms) " + statusMessage, request)
+          response.getWriter.print(successJson(message = statusMessage))
+        } else {
+          val (isError, statusMessage) = optSameNames match {
+            case None => (true, s"MongoDB error updateResult: $updateResult")
+            case Some(dupCount) =>
+              if (dupCount == categories.length) {
+                (false, s"All ${categories.length} categories unchanged " +
+                  s"in Google folder '$googleFolderId'")
+              } else {
+                (true, s"Stored ${categories.length} categories ($dupCount unchanged) " +
+                  s"from Google folder '$googleFolderId' with team '$teamOid'")
+              }
+
+          }
+          if (isError) {
+            BWLogger.log(getClass.getName, request.getMethod, s"EXIT-ERROR: (time: $delay ms) $statusMessage", request)
+            val errorJson = new Document("ok", 0).append("message", statusMessage).toJson
+            response.getWriter.print(errorJson)
+          } else {
+            BWLogger.audit(getClass.getName, request.getMethod, s"EXIT-WARN: (time: $delay ms) $statusMessage", request)
+            val returnJson = new Document("ok", 0).append("message", statusMessage).toJson
+            response.getWriter.print(returnJson)
+          }
+        }
+      }
+
       val teamRecord: DynDoc = BWMongoDB3.teams.find(Map("_id" -> teamOid)).head
       val optExistingGoogleFolderId: Option[String] = teamRecord.get[String]("google_folder_id")
       val existingAccessGrants: Seq[DynDoc] = accessGrantRecords(teamOid, projectOid)
-      val categories: Many[Document] = foldersToRecords(folders).map(_.path[Seq[String]]).map(path => {
-        val indexedPath = path.zipWithIndex
+      val googleFolderCategories: Many[Document] = foldersToRecords(folders).map(record => {
+        val indexedPath = record.path[Seq[String]].zipWithIndex
         val categoryDoc = new Document("_id", new ObjectId()).append("editable", false)
         for (pc <- indexedPath) {
           categoryDoc.append(s"L${pc._2 + 1}", pc._1)
@@ -79,58 +113,48 @@ class DocCategoriesSave extends HttpServlet with HttpUtils with MailUtils {
         categoryDoc
       }).asJava
 
-      optExistingGoogleFolderId match {
-        case None =>
-          if (existingAccessGrants.nonEmpty) {
-            val existingCategories = existingAccessGrants.map(ag => {
-              val grantedTeamOid = ag.granted_team_id[ObjectId]
-              val grantedTeamName = TeamApi.teamById(grantedTeamOid).team_name[String]
-              val grantedCategoryName = ag.category_key[String]
-              s"[category: '$grantedCategoryName', team: '$grantedTeamName']"
-            }).mkString(", ")
-            val errorMessage = s"Access grants exist for: $existingCategories"
-            BWLogger.log(getClass.getName, request.getMethod, s"EXIT-ERROR: $errorMessage", request)
-            val rv = new Document("ok", 0).append("message", errorMessage)
-            response.getWriter.print(rv.toJson)
-          } else {
-            val updateResult = BWMongoDB3.teams.updateOne(Map("_id" -> teamOid, "project_id" -> projectOid),
-              Map($set -> Map("google_folder_id" -> googleFolderId, "own_doc_categories" -> categories)))
-            if (updateResult.getModifiedCount == 1) {
-              val delay = System.currentTimeMillis() - t0
-              val logMessage = s"(time: $delay ms) Stored ${categories.length} categories from Google folder '$googleFolderId' with team '$teamOid'"
-              BWLogger.audit(getClass.getName, request.getMethod, logMessage, request)
-              response.getWriter.print(successJson())
-            } else {
-              val errorMessage = s"MongoDB error updateResult: $updateResult"
-              BWLogger.log(getClass.getName, request.getMethod, s"EXIT-ERROR: $errorMessage", request)
-              val rv = new Document("ok", 0).append("message", errorMessage)
-              response.getWriter.print(rv.toJson)
-            }
-          }
-        case Some(existingGoogleFolderId) =>
-          if (existingGoogleFolderId != googleFolderId) {
-            val errorMessage = s"Given folder-id ($googleFolderId) differs from existing folder-id ($existingGoogleFolderId)"
-            BWLogger.log(getClass.getName, request.getMethod, s"EXIT-ERROR: $errorMessage", request)
-            val rv = new Document("ok", 0).append("message", errorMessage)
-            response.getWriter.print(rv.toJson)
-          } else {
-            val existingCategoryKeys = getExistingCategoryKeys(teamRecord)
-            val newCategories = categories.filterNot(cat => existingCategoryKeys.contains(getCategoryKey(cat)))
-            val updateResult = BWMongoDB3.teams.updateOne(Map("_id" -> teamOid, "project_id" -> projectOid),
-              Map($push -> Map("own_doc_categories" -> Map($each -> newCategories)),
-                $set -> Map("google_folder_id" -> googleFolderId)))
-            if (updateResult.getModifiedCount != 1) {
-              val errorMessage = s"MongoDB error updateResult: $updateResult"
+      if (googleFolderCategories.isEmpty) {
+        BWLogger.log(getClass.getName, request.getMethod, "EXIT-WARN: No categories found", request)
+        response.getWriter.print(successJson())
+      } else {
+        optExistingGoogleFolderId match {
+          case None =>
+            if (existingAccessGrants.nonEmpty) {
+              val existingCategories = existingAccessGrants.map(ag => {
+                val grantedTeamOid = ag.granted_team_id[ObjectId]
+                val grantedTeamName = TeamApi.teamById(grantedTeamOid).team_name[String]
+                val grantedCategoryName = ag.category_key[String]
+                s"[category: '$grantedCategoryName', team: '$grantedTeamName']"
+              }).mkString(", ")
+              val errorMessage = s"Access grants exist for: $existingCategories"
               BWLogger.log(getClass.getName, request.getMethod, s"EXIT-ERROR: $errorMessage", request)
               val rv = new Document("ok", 0).append("message", errorMessage)
               response.getWriter.print(rv.toJson)
             } else {
-              val delay = System.currentTimeMillis() - t0
-              val logMessage = s"(time: $delay ms) Stored ${categories.length} categories from Google folder '$googleFolderId' with team '$teamOid'"
-              BWLogger.audit(getClass.getName, request.getMethod, logMessage, request)
-              response.getWriter.print(successJson())
+              saveCategories(googleFolderCategories)
             }
-          }
+          case Some(existingGoogleFolderId) =>
+            if (existingGoogleFolderId != googleFolderId) {
+              val errorMessage = s"Given folder-id ($googleFolderId) differs from existing folder-id ($existingGoogleFolderId)"
+              BWLogger.log(getClass.getName, request.getMethod, s"EXIT-ERROR: $errorMessage", request)
+              val rv = new Document("ok", 0).append("message", errorMessage)
+              response.getWriter.print(rv.toJson)
+            } else {
+              val categoryKeyToIdMap = {
+                val ownCategories: Seq[DynDoc] = teamRecord.own_doc_categories[Many[Document]]
+                ownCategories.map(oc => (getCategoryKey(oc), oc._id[ObjectId])).toMap
+              }
+              var sameNames = 0
+              for (category <- googleFolderCategories) {
+                val categoryKey = getCategoryKey(category)
+                if (categoryKeyToIdMap.contains(categoryKey)) {
+                  category._id = categoryKeyToIdMap(categoryKey)
+                  sameNames += 1
+                }
+              }
+              saveCategories(googleFolderCategories, Some(sameNames))
+            }
+        }
       }
     } catch {
       case t: Throwable =>
